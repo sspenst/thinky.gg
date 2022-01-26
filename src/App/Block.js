b0358970c5bd0f468621dd94613c1ebf6be4caa4
Spencer Spenst <sspenst@Spencers-MacBook-Pro.local>
2022-01-25 23:07:10 -0800

    cleanup / early logic for locking blocks

--- src/App/Block.js
+++ src/App/Block.js
@@ -1,14 +1,10 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from 'react';
 import './index.css';
 
 export default function Block(props) {
   const blockRef = useRef();
 
   useEffect(() => {
-    // animation reference:
-    // https://css-tricks.com/controlling-css-animations-transitions-javascript/
-    // https://stackoverflow.com/questions/62570164/dynamic-css-animation-variable-with-react
-
     // move block to correct spot on window resize or position update
     blockRef.current.style.transform = `
       translateX(${props.squareSize * props.position.x}px)
---
+++ src/App/SquareType.js
@@ -0,0 +1,7 @@
+const SquareType = {
+  Default: 0,
+  Wall: 1,
+  End: 2,
+};
+
+export default SquareType;
