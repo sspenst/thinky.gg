15ba7b8a10fd1f9a9af1bb9fecb54532d6760641
Spencer Spenst <spencerspenst@gmail.com>
2022-01-31 16:43:16 -0800

    converted sirdavid's saved level data; removed duplicates and test levels

--- src/App/LevelDataType.js
+++ src/App/LevelDataType.js
@@ -1,9 +1,20 @@
 const LevelDataType = {
+  // PP1
   Default: '0',
   Wall: '1',
   Block: '2',
   End: '3',
   Start: '4',
+  // PP2
+  Hole: '5',
+  Left: '6',
+  Up: '7',
+  Right: '8',
+  Down: '9',
+  Upleft: 'A',
+  Upright: 'B',
+  Downright: 'C',
+  Downleft: 'D',
 };
 
 export default LevelDataType;
