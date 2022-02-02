4e9017cf7c690c9a39c4ff8753eb536e71480202
Spencer Spenst <spencerspenst@gmail.com>
2022-02-01 21:57:28 -0800

    update pp1 names to inclulde level number

---
+++ src/App/Color.js
@@ -0,0 +1,15 @@
+// this might be stupid
+// probably better to make some sort of skin css file
+
+const Color = {
+  Background: 'rgb(38, 38, 38)',
+  Block: 'rgb(110, 80, 60)',
+  Player: 'rgb(244, 114, 182)',
+  TextEndLose: 'rgb(205, 0, 0)',
+  TextEndWin: 'rgb(255, 255, 255)',
+  TextLevelSelect: 'rgb(255, 255, 255)',
+  TextMove: 'rgb(0, 0, 0)',
+  TextMoveOver: 'rgb(255, 50, 50)',
+};
+
+export default Color;
