2cba717d76eda4887e53a61df843a0eebf1b3a5f
Spencer Spenst <spencerspenst@gmail.com>
2022-02-24 23:47:51 -0800

    basic leaderboard component

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -26,6 +26,7 @@ export default function App() {
       top: Dimensions.MenuHeight,
     }}>
       <div><Link to='/catalog'>CATALOG</Link></div>
+      <div><Link to='/leaderboard'>LEADERBOARD</Link></div>
       {/* <div><Link to='/data'>DATA</Link></div>
       <div><Link to='/editor'>EDITOR</Link></div> */}
       <UserState/>
