37260adfbeb69db63ef39c177728555ee26bfc5d
Spencer Spenst <spencerspenst@gmail.com>
2022-03-08 02:02:54 -0800

    debug revalidate 500s in prod

--- pages/api/revalidate/index.js
+++ pages/api/revalidate/index.js
@@ -5,6 +5,6 @@ export default async function handler(req, res) {
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
-    return res.status(500).send('Error revalidating');
+    return res.status(500).send(err);
   }
 }
