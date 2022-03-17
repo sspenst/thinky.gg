db9efa394c27b0729b15d68b5d6f6d6d7339c6ba
Spencer Spenst <spencerspenst@gmail.com>
2022-03-16 23:07:40 -0700

    game layout improvements

--- components/level/player.tsx
+++ components/level/player.tsx
@@ -1,44 +1,42 @@
 import Color from '../../constants/color';
 import { GameState } from './game';
+import Movable from './movable';
 import React from 'react';
-import { useState } from 'react';
 
-interface BlockProps {
+interface PlayerProps {
   gameState: GameState;
   leastMoves: number;
+  padding: number;
   size: number;
 }
 
-export default function Player({ gameState, leastMoves, size }: BlockProps) {
-  // initialize the block at the starting position to avoid an animation from the top left
-  const [initPos] = useState(gameState.pos.clone());
+export default function Player({ gameState, leastMoves, padding, size }: PlayerProps) {
   const color = gameState.moveCount > leastMoves ? Color.TextEndLose :
     gameState.endText === undefined ? Color.TextMove :
     gameState.moveCount < leastMoves ? Color.TextEndRecord : Color.TextEndWin;
+  const innerSize = size - 2 * padding;
+  const fontSize = gameState.moveCount >= 1000 ? innerSize / 3 : innerSize / 2;
 
   return (
-    <div
-      className={'cursor-default select-none'}
-      style={{
-        backgroundColor: Color.Player,
-        color: color,
-        fontSize: size / 2,
-        height: size,
-        left: size * initPos.x,
-        lineHeight: size + 'px',
-        position: 'absolute',
-        textAlign: 'center',
-        top: size * initPos.y,
-        transform: `
-          translateX(${(gameState.pos.x - initPos.x) * 100}%)
-          translateY(${(gameState.pos.y - initPos.y) * 100}%)
-        `,
-        transition: 'transform 0.1s',
-        verticalAlign: 'middle',
-        width: size,
-      }}
+    <Movable
+      padding={padding}
+      position={gameState.pos}
+      size={size}
     >
-      {gameState.endText === undefined ? String(gameState.moveCount) : gameState.endText}
-    </div>
+      <div
+        style={{
+          backgroundColor: Color.Player,
+          color: color,
+          fontSize: fontSize,
+          height: innerSize,
+          lineHeight: innerSize + 'px',
+          textAlign: 'center',
+          verticalAlign: 'middle',
+          width: innerSize,
+        }}
+      >
+        {gameState.endText === undefined ? String(gameState.moveCount) : gameState.endText}
+      </div>
+    </Movable>
   );
 }
