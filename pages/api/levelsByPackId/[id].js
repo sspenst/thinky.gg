6b6f4d1c5e77ff9f4f17c4bae9b1c163566313fd
Spencer Spenst <spencerspenst@gmail.com>
2022-03-19 19:05:38 -0700

    try unstable_revalidate everywhere again

--- pages/api/levelsByPackId/[id].js
+++ pages/api/levelsByPackId/[id].js
@@ -20,5 +20,7 @@ export default async function handler(req, res) {
     });
   }
 
+  levels.sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1);
+
   res.status(200).json(levels);
 }
