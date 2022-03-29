d793e838eefb40caa1217b89e6e9dee0fb3f720c
Spencer Spenst <spencerspenst@gmail.com>
2022-03-28 23:41:17 -0700

    fix build errors

--- pages/level/[id].tsx
+++ pages/level/[id].tsx
@@ -92,7 +92,7 @@ interface LevelPageProps {
 function LevelPage({ creator, pack }: LevelPageProps) {
   const router = useRouter();
   const { id } = router.query;
-  const { level } = useLevel(id);
+  const { level } = useLevel(id as string);
 
   return (!level ? null :
     <Page
