5b6fe8501caa6dca6514703b189363228cbefadd
Spencer Spenst <spencerspenst@gmail.com>
2022-04-06 19:09:48 -0700

    forgot to update leaderboard page

--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -11,7 +11,7 @@ import useLeaderboard from '../../hooks/useLeaderboard';
 export async function getStaticProps() {
   await dbConnect();
   
-  const users = await UserModel.find<User>({}, 'name score').sort({ score: -1 });
+  const users = await UserModel.find<User>({ score: { $ne: 0 }}, 'name score').sort({ score: -1 });
 
   if (!users) {
     throw new Error('Error finding Users');
