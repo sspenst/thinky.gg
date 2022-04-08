7c3864d82b978d0f9fcbf9e2e38ce4a37218f11f
Spencer Spenst <spencerspenst@gmail.com>
2022-04-08 16:52:44 -0700

    legacy reviews

---
+++ models/db/review.ts
@@ -0,0 +1,12 @@
+import { Types } from 'mongoose';
+
+// represents a document from the pathology.reviews collection
+export default interface Review {
+  _id: Types.ObjectId;
+  levelId: Types.ObjectId;
+  psychopathId?: number;
+  score: number;
+  text: string;
+  ts: number;
+  userId: Types.ObjectId;
+}
