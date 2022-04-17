e0ac40318388f8e8ef2dad23ead4e58064a2e407
Spencer Spenst <spencerspenst@gmail.com>
2022-04-16 17:02:55 -0700

    authorNote

--- models/db/level.ts
+++ models/db/level.ts
@@ -3,11 +3,12 @@ import { Types } from 'mongoose';
 // represents a document from the pathology.levels collection
 export default interface Level {
   _id: Types.ObjectId;
+  authorNote?: string;
   data: string;
   height: number;
   leastMoves: number;
-  leastMovesTs: number;
-  leastMovesUserId: Types.ObjectId;
+  leastMovesTs?: number;
+  leastMovesUserId?: Types.ObjectId;
   name: string;
   officialUserId?: Types.ObjectId;
   packId: Types.ObjectId;
