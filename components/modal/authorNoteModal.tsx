a015427c3afb3661e8981e756787aad427e0533e
Spencer Spenst <spencerspenst@gmail.com>
2022-04-20 15:43:59 -0700

    remove html tags when displaying authorNote

--- components/modal/authorNoteModal.tsx
+++ components/modal/authorNoteModal.tsx
@@ -8,13 +8,15 @@ interface AuthorNoteModalProps {
 }
 
 export default function AuthorNoteModal({ authorNote, closeModal, isOpen }: AuthorNoteModalProps) {
+  const authorNoteWithoutTags = authorNote.replace(/<\/?[^>]+>/g, '');
+
   return (
     <Modal
       closeModal={closeModal}
       isOpen={isOpen}
       title={'Author Note'}
     >
-      <span style={{whiteSpace: 'pre-wrap'}}>{authorNote}</span>
+      <span style={{whiteSpace: 'pre-wrap'}}>{authorNoteWithoutTags}</span>
     </Modal>
   );
 }
