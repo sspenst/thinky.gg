3ddbf747b3408443e4e176ed98ad7dc07d020326
Spencer Spenst <spencerspenst@gmail.com>
2022-04-26 14:36:30 -0700

    update password (closes #7)

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -6,16 +6,21 @@ import useStats from '../../hooks/useStats';
 import useUser from '../../hooks/useUser';
 
 export default function Account() {
-  const [email, setEmail] = useState<string>();
-  const { mutateStats } = useStats();
-  const [name, setName] = useState<string>();
+  const [currentPassword, setCurrentPassword] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
   const { error, isLoading, mutateUser, user } = useUser();
+  const { mutateStats } = useStats();
+  const [name, setName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [password2, setPassword2] = useState<string>('');
   const router = useRouter();
   const { setIsLoading } = useContext(AppContext);
 
   useEffect(() => {
-    setEmail(user?.email);
-    setName(user?.name);
+    if (user) {
+      setEmail(user.email);
+      setName(user.name);
+    }
   }, [user]); 
 
   useEffect(() => {
@@ -24,16 +29,15 @@ export default function Account() {
     }
   }, [error, router]);
 
-  function updateName(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-
+  function updateUser(
+    body: string,
+    error: string,
+  ) {
     setIsLoading(true);
 
     fetch('/api/user', {
       method: 'PUT',
-      body: JSON.stringify({
-        name: name,
-      }),
+      body: body,
       credentials: 'include',
       headers: {
         'Content-Type': 'application/json'
@@ -42,36 +46,53 @@ export default function Account() {
       const { updated } = await res.json();
 
       if (!updated) {
-        alert('Error: username already exists');
+        alert(error);
       }
 
       mutateUser();
+    }).catch(err => {
+      console.error(err);
+      alert('Error updating user');
     });
+  }
+
+  function updateName(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    updateUser(
+      JSON.stringify({
+        name: name,
+      }),
+      'Error: username already exists',
+    );
   }
 
   function updateEmail(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
-    setIsLoading(true);
-
-    fetch('/api/user', {
-      method: 'PUT',
-      body: JSON.stringify({
+    updateUser(
+      JSON.stringify({
         email: email,
       }),
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    }).then(async res => {
-      const { updated } = await res.json();
+      'Error: email already exists',
+    );
+  }
 
-      if (!updated) {
-        alert('Error: email already exists');
-      }
+  function updatePassword(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
 
-      mutateUser();
-    });
+    if (password !== password2) {
+      alert('Password does not match');
+      return;
+    }
+
+    updateUser(
+      JSON.stringify({
+        currentPassword: currentPassword,
+        password: password,
+      }),
+      'Error updating password',
+    );
   }
 
   function deleteAccount() {
@@ -89,39 +110,89 @@ export default function Account() {
   return (error || isLoading ? null :
     <Page title={'Account'}>
       <>
-        <form onSubmit={updateName}>
-          <label htmlFor='name'>Username:</label>
+        <div
+          style={{
+            display: 'table',
+            margin: '0 auto',
+          }}
+        >
+          <form onSubmit={updateName}>
+            <label htmlFor='name'>Username:</label>
+            <br/>
+            <input
+              type='text'
+              name='name'
+              placeholder='Enter username'
+              value={name}
+              onChange={e => setName(e.target.value)}
+              style={{color: 'rgb(0, 0, 0)'}}
+              required
+            />
+            <br/>
+            <input className='cursor-pointer underline' type='submit' value='Update'></input>
+          </form>
           <br/>
-          <input
-            type='text'
-            name='name'
-            placeholder='Enter username'
-            value={name}
-            onChange={e => setName(e.target.value)}
-            style={{color: 'rgb(0, 0, 0)'}}
-            required
-          />
+          <form onSubmit={updateEmail}>
+            <label htmlFor='email'>Email:</label>
+            <br/>
+            <input
+              type='email'
+              name='email'
+              placeholder='Enter email'
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              style={{color: 'rgb(0, 0, 0)'}}
+              required
+            />
+            <br/>
+            <input className='cursor-pointer underline' type='submit' value='Update'></input>
+          </form>
           <br/>
-          <input className='cursor-pointer underline' type='submit' value='Update'></input>
-        </form>
-        <br/>
-        <form onSubmit={updateEmail}>
-          <label htmlFor='email'>Email:</label>
+          <form onSubmit={updatePassword}>
+            <label htmlFor='email'>Password:</label>
+            <br/>
+            <input
+              type='password'
+              name='currentPassword'
+              placeholder='Enter current password'
+              value={currentPassword}
+              onChange={e => setCurrentPassword(e.target.value)}
+              style={{color: 'rgb(0, 0, 0)'}}
+              required
+            />
+            <br/>
+            <input
+              type='password'
+              name='password'
+              placeholder='Enter new password'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              style={{color: 'rgb(0, 0, 0)'}}
+              required
+            />
+            <br/>
+            <input
+              type='password'
+              name='password2'
+              placeholder='Re-enter new password'
+              value={password2}
+              onChange={e => setPassword2(e.target.value)}
+              style={{color: 'rgb(0, 0, 0)'}}
+              required
+            />
+            <br/>
+            <input className='cursor-pointer underline' type='submit' value='Update'></input>
+          </form>
           <br/>
-          <input
-            type='email'
-            name='email'
-            placeholder='Enter email'
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            style={{color: 'rgb(0, 0, 0)'}}
-            required
-          />
-          <br/>
-          <input className='cursor-pointer underline' type='submit' value='Update'></input>
-        </form>
-        <br/>
-        <div><button onClick={deleteAccount}>DELETE ACCOUNT</button></div>
+        </div>
+        <div
+          style={{
+            display: 'table',
+            margin: '0 auto',
+          }}
+        >
+          <button onClick={deleteAccount}>DELETE ACCOUNT</button>
+        </div>
       </>
     </Page>
   );
