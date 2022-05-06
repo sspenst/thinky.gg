99a65bbd74fd4616851994d18f1fb9d687b3e1c8
Spencer Spenst <spencerspenst@gmail.com>
2022-05-06 16:32:48 -0700

    misc API polish (closes #43)

--- hooks/useLevelById.ts
+++ hooks/useLevelById.ts
@@ -2,7 +2,7 @@ import Level from '../models/db/level';
 import useSWRHelper from '../helpers/useSWRHelper';
 
 export default function useLevelById(id: string | string[] | undefined) {
-  const { data, error, isLoading, mutate } = useSWRHelper<Level>(`/api/level-by-id/${id}`);
+  const { data, error, isLoading, mutate } = useSWRHelper<Level>(id ? `/api/level-by-id/${id}` : null);
 
   return {
     error,
--- hooks/useLevelsByWorldId.ts
+++ hooks/useLevelsByWorldId.ts
@@ -2,7 +2,7 @@ import Level from '../models/db/level';
 import useSWRHelper from '../helpers/useSWRHelper';
 
 export default function useLevelsByWorldId(id: string | string[] | undefined) {
-  const { data, error, isLoading, mutate } = useSWRHelper<Level[]>(`/api/levels-by-world-id/${id}`);
+  const { data, error, isLoading, mutate } = useSWRHelper<Level[]>(id ? `/api/levels-by-world-id/${id}` : null);
 
   return {
     error,
