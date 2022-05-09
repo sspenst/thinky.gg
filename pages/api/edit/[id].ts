67bec500355ae0a3c9b989e8fe1852d5dbc8101d
Spencer Spenst <spencerspenst@gmail.com>
2022-05-08 19:22:15 -0700

    save level (closes #46)

---
+++ pages/api/edit/[id].ts
@@ -0,0 +1,30 @@
+import withAuth, { NextApiRequestWithAuth } from '../../../lib/withAuth';
+import { LevelModel } from '../../../models/mongoose';
+import type { NextApiResponse } from 'next';
+import dbConnect from '../../../lib/dbConnect';
+
+export default withAuth(async (req: NextApiRequestWithAuth, res: NextApiResponse) => {
+  if (req.method !== 'PUT') {
+    return res.status(405).json({
+      error: 'Method not allowed',
+    });
+  }
+  
+  const { id } = req.query;
+  const { level } = req.body;
+
+  await dbConnect();
+
+  await LevelModel.updateOne({
+    _id: id,
+    userId: req.userId,
+  }, {
+    $set: {
+      data: level.data,
+      height: level.height,
+      width: level.width,
+    },
+  });
+
+  res.status(200).json(level);
+});
