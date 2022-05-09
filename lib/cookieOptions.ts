f881d556235f312fe080e5209050aef63f40822f
Spencer Spenst <spencerspenst@gmail.com>
2022-05-09 15:39:06 -0700

    move to k2xl.com (closes #42)

--- lib/cookieOptions.ts
+++ lib/cookieOptions.ts
@@ -1,9 +1,9 @@
 import { CookieSerializeOptions } from 'cookie';
 
 // https://github.com/jshttp/cookie#options-1
-export default function cookieOptions(clear = false): CookieSerializeOptions {
+export default function cookieOptions(host: string | undefined, clear = false): CookieSerializeOptions {
   return {
-    domain: process.env.LOCAL ? 'localhost' : 'pathology.sspenst.com',
+    domain: process.env.LOCAL ? 'localhost' : host ?? 'pathology.k2xl.com',
     // browsers will delete cookies with a unix epoch expiration date
     expires: clear ? new Date(0) : undefined,
     httpOnly: true,
