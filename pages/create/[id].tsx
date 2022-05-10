9c8f01cb413a07735e09de4e45b767d294d6778f
Spencer Spenst <spencerspenst@gmail.com>
2022-05-09 21:34:18 -0700

    editor instructions (closes #51)

--- pages/create/[id].tsx
+++ pages/create/[id].tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import Dimensions from '../../constants/dimensions';
 import LevelTable from '../../components/levelTable';
 import LinkInfo from '../../models/linkInfo';
 import Page from '../../components/page';
@@ -27,7 +28,19 @@ export default function Create() {
       folders={[new LinkInfo('Create', '/create')]}
       title={world.name}
     >
-      <LevelTable worldId={id.toString()} />
+      <>
+        <div
+          style={{
+            marginLeft: Dimensions.TableMargin,
+            marginRight: Dimensions.TableMargin,
+            marginTop: Dimensions.TableMargin,
+            textAlign: 'center',
+          }}
+        >
+          Welcome to the level creation page! After creating a level, click on its name to start editing. Once you have finished desgining your level, click the &apos;Test&apos; button to set the level&apos;s least moves, then click publish to make your level available for everyone to play. You can unpublish or delete a level at any time.
+        </div>
+        <LevelTable worldId={id.toString()} />
+      </>
     </Page>
   );
 }
