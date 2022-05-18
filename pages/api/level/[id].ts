d5239eae4fb909b57949254ae5e40c5f949e2a17
Danny Miller <k2xl.com@gmail.com>
2022-05-18 12:15:54 -0400

    More tests

--- pages/api/level/[id].ts
+++ pages/api/level/[id].ts
@@ -102,8 +102,7 @@ export default withAuth(async (req: NextApiRequestWithAuth, res: NextApiResponse
       UserModel.updateMany({ _id: { $in: userIds } }, { $inc: { score: -1 } }),
       WorldModel.updateMany({ levels: level._id }, { $pull: { levels: level._id } }),
     ]);
-
-    await revalidateUniverse(req, res);
+    return await revalidateUniverse(req, res);
   } else {
     return res.status(405).json({
       error: 'Method not allowed',
--- tests/api/level/level.test.ts
+++ tests/api/level/level.test.ts
@@ -2,16 +2,19 @@ import { NextApiRequestWithAuth } from '../../../lib/withAuth';
 import { ObjectId } from 'bson';
 import createLevelHandler from '../../../pages/api/level/index';
 import { dbDisconnect } from '../../../lib/dbConnect';
-import getLevelHandler from '../../../pages/api/level/[id]';
+import modifyLevelHandler from '../../../pages/api/level/[id]';
+import getWorldHandler from '../../../pages/api/world-by-id/[id]';
 import { getTokenCookieValue } from '../../../lib/getTokenCookie';
 import { testApiHandler } from 'next-test-api-route-handler';
 
 const USER_ID_FOR_TESTING = '600000000000000000000000';
 const WORLD_ID_FOR_TESTING = '600000000000000000000001';
-
+var level_id_1: string;
+var level_id_2:string;
 afterAll(async () => {
   await dbDisconnect();
 });
+require('jest-fetch-mock').enableMocks()
 
 describe('pages/api/level/index.ts', () => {
   test('Sending nothing should return 401', async () => {
@@ -53,7 +56,7 @@ describe('pages/api/level/index.ts', () => {
   });
 
   test('Doing a POST with level data should be OK', async () => {
-    let level_id: string;
+    
     await testApiHandler({
       handler: async (_, res) => {
         const req: NextApiRequestWithAuth = {
@@ -77,10 +80,38 @@ describe('pages/api/level/index.ts', () => {
         const res = await fetch();
         const response = await res.json();
         expect(response.success).toBe(true);
-        level_id = response._id;
+        level_id_1 = response._id;
         expect(res.status).toBe(200);
       },
     });
+
+    await testApiHandler({
+        handler: async (_, res) => {
+          const req: NextApiRequestWithAuth = {
+            method: 'POST',
+            userId: USER_ID_FOR_TESTING,
+            cookies: {
+              token: getTokenCookieValue(USER_ID_FOR_TESTING),
+            },
+            body: {
+              authorNote: 'I\'m a mean little note.',
+              name: 'A Second Test Level',
+              worldId: WORLD_ID_FOR_TESTING,
+            },
+            headers: {
+              'content-type': 'application/json',
+            },
+          } as unknown as NextApiRequestWithAuth;
+          await createLevelHandler(req, res);
+        },
+        test: async ({ fetch }) => {
+          const res = await fetch();
+          const response = await res.json();
+          expect(response.success).toBe(true);
+          level_id_2 = response._id;
+          expect(res.status).toBe(200);
+        },
+      });
 
     // now we should be able to get the level
     await testApiHandler({
@@ -92,10 +123,10 @@ describe('pages/api/level/index.ts', () => {
             token: getTokenCookieValue(USER_ID_FOR_TESTING),
           },
           query: {
-            id: level_id,
+            id: level_id_1,
           },
         } as unknown as NextApiRequestWithAuth;
-        await getLevelHandler(req, res);
+        await modifyLevelHandler(req, res);
       },
       test: async ({ fetch }) => {
         const res = await fetch();
@@ -103,7 +134,7 @@ describe('pages/api/level/index.ts', () => {
         expect(response.authorNote).toBe('I\'m a nice little note.');
         expect(response.name).toBe('A Test Level');
         expect(response.worldId._id).toBe(WORLD_ID_FOR_TESTING);
-        expect(response._id).toBe(level_id);
+        expect(response._id).toBe(level_id_1);
         expect(res.status).toBe(200);
       },
     });
@@ -121,12 +152,111 @@ describe('pages/api/level/index.ts', () => {
             id: new ObjectId(), // shouldn't exist
           },
         } as unknown as NextApiRequestWithAuth;
-        await getLevelHandler(req, res);
+        await modifyLevelHandler(req, res);
       },
       test: async ({ fetch }) => {
         const res = await fetch();
         expect(res.status).toBe(404);
       },
+    });
+
+    // querying the world should show the level in the level_ids array
+    
+    await testApiHandler({
+        handler: async (_, res) => {
+            const req: NextApiRequestWithAuth = {
+            method: 'GET',
+            userId: USER_ID_FOR_TESTING,
+            cookies: {
+                token: getTokenCookieValue(USER_ID_FOR_TESTING),
+            },
+            query: {
+                id: WORLD_ID_FOR_TESTING,
+            },
+            } as unknown as NextApiRequestWithAuth;
+            await getWorldHandler(req, res);
+        },
+        test: async ({ fetch }) => {
+            const res = await fetch();
+            const response = await res.json();
+            expect(response.levels).toContain(level_id_1);
+            expect(response.levels).toContain(level_id_2);
+            expect(response.levels.length).toBe(2);
+            expect(res.status).toBe(200);
+        },
+    });
+
+    // Testing deleting a level //
+    // Deleting a level that doesn't exist should return a 404
+    await testApiHandler({
+        handler: async (_, res) => {
+          const req: NextApiRequestWithAuth = {
+            method: 'DELETE',
+            userId: USER_ID_FOR_TESTING,
+            cookies: {
+              token: getTokenCookieValue(USER_ID_FOR_TESTING),
+            },
+            headers: {
+                'content-type': 'application/json',
+            },
+            query: {
+              id: new ObjectId(), // shouldn't exist
+            },
+          } as unknown as NextApiRequestWithAuth;
+          await modifyLevelHandler(req, res);
+        },
+        test: async ({ fetch }) => {
+          const res = await fetch();
+          expect(res.status).toBe(404);
+        },
+      });
+    // Deleting a level that DOES exist should work
+    await testApiHandler({
+        handler: async (_, res) => {
+          const req: NextApiRequestWithAuth = {
+            method: 'DELETE',
+            userId: USER_ID_FOR_TESTING,
+            headers: {
+                'content-type': 'application/json',
+            },
+            cookies: {
+              token: getTokenCookieValue(USER_ID_FOR_TESTING),
+            },
+            query: {
+              id: level_id_1, // shouldn't exist
+            },
+          } as unknown as NextApiRequestWithAuth;
+          await modifyLevelHandler(req, res);
+        },
+        test: async ({ fetch }) => {
+          const res = await fetch();
+          const response = await res.json();
+          expect(response.updated).toBe(true);
+          expect(res.status).toBe(200);
+        },
+      });
+      // Now fetching the worlds should return the level_ids array without the deleted level
+      await testApiHandler({
+        handler: async (_, res) => {
+            const req: NextApiRequestWithAuth = {
+            method: 'GET',
+            userId: USER_ID_FOR_TESTING,
+            cookies: {
+                token: getTokenCookieValue(USER_ID_FOR_TESTING),
+            },
+            query: {
+                id: WORLD_ID_FOR_TESTING,
+            },
+            } as unknown as NextApiRequestWithAuth;
+            await getWorldHandler(req, res);
+        },
+        test: async ({ fetch }) => {
+            const res = await fetch();
+            const response = await res.json();
+            expect(response.levels).toContain(level_id_2);
+            expect(response.levels.length).toBe(1);
+            expect(res.status).toBe(200);
+        },
     });
   });
 });
