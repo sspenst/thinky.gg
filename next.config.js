545f41a77cb7a12b4ff9b8b5e72ef18c82b18a7b
Spencer Spenst <spencerspenst@gmail.com>
2022-05-18 22:55:50 -0700

    lint

--- next.config.js
+++ next.config.js
@@ -24,6 +24,7 @@ module.exports = {
   },
   eslint: {
     dirs: [
+      '',
       'components',
       'constants',
       'contexts',
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   content: [
-    "./components/**/*.{js,jsx,ts,tsx}",
-    "./pages/**/*.{js,jsx,ts,tsx}",
+    './components/**/*.{js,jsx,ts,tsx}',
+    './pages/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
