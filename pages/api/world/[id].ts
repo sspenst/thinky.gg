1a79c20963bd8afda814004d202a14194205f43a
Spencer Spenst <spencerspenst@gmail.com>
2022-05-19 19:09:12 -0700

    delete world should not delete levels (#121)

--- pages/api/world/[id].ts
+++ pages/api/world/[id].ts
@@ -1,9 +1,7 @@
-import { LevelModel, RecordModel, ReviewModel, StatModel, UserModel, WorldModel } from '../../../models/mongoose';
 import withAuth, { NextApiRequestWithAuth } from '../../../lib/withAuth';
-import Level from '../../../models/db/level';
 import type { NextApiResponse } from 'next';
-import Stat from '../../../models/db/stat';
 import World from '../../../models/db/world';
+import { WorldModel } from '../../../models/mongoose';
 import dbConnect from '../../../lib/dbConnect';
 import revalidateUniverse from '../../../helpers/revalidateUniverse';
 
@@ -42,34 +40,9 @@ export default withAuth(async (req: NextApiRequestWithAuth, res: NextApiResponse
       });
     }
 
-    const levelIds = (await LevelModel.find<Level>({ worldId: id })).map(level => level._id);
-    const stats = await StatModel.find<Stat>({ levelId: { $in: levelIds }});
+    await WorldModel.deleteOne({ _id: id });
 
-    // user scores that need to be adjusted after deleting levels
-    const scoreInc: {[userId: string]: number} = {};
-
-    for (let i = 0; i < stats.length; i++) {
-      if (stats[i].complete) {
-        const userId = stats[i].userId.toString();
-        scoreInc[userId] = (scoreInc[userId] || 0) - 1;
-      }
-    }
-
-    const promises: unknown[] = [
-      LevelModel.deleteMany({ worldId: id }),
-      RecordModel.deleteMany({ levelId: { $in: levelIds }}),
-      ReviewModel.deleteMany({ levelId: { $in: levelIds }}),
-      StatModel.deleteMany({ levelId: { $in: levelIds }}),
-      WorldModel.deleteOne({ _id: id }),
-    ];
-
-    for (const userId in scoreInc) {
-      promises.push(UserModel.updateOne({ _id: userId }, { $inc: { score: scoreInc[userId] }}));
-    }
-
-    await Promise.all(promises);
-
-    return res.status(200).json({ success: true });
+    return await revalidateUniverse(req, res);
   } else {
     return res.status(405).json({
       error: 'Method not allowed',
