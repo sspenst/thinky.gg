fde8ae7d2968ff4bc3be2a516d56ea62b6a25d0c
Danny Miller <k2xl.com@gmail.com>
2022-05-29 09:57:58 -0400

    reset api/level/index

--- pages/api/level/index.ts
+++ pages/api/level/index.ts
@@ -1,37 +1,24 @@
-import { LevelModel, RecordModel, ReviewModel, StatModel, UserModel, WorldModel } from '../../../models/mongoose';
+import { LevelModel, WorldModel } from '../../../models/mongoose';
 import withAuth, { NextApiRequestWithAuth } from '../../../lib/withAuth';
-import Level from '../../../models/db/level';
 import type { NextApiResponse } from 'next';
-import Stat from '../../../models/db/stat';
+import { ObjectId } from 'bson';
 import dbConnect from '../../../lib/dbConnect';
-import revalidateUniverse from '../../../helpers/revalidateUniverse';
+import getTs from '../../../helpers/getTs';
 
 export default withAuth(async (req: NextApiRequestWithAuth, res: NextApiResponse) => {
-  if (req.method === 'GET') {
-    const { id } = req.query;
-
-    await dbConnect();
-
-    const level = await LevelModel.findOne({
-      _id: id,
-      userId: req.userId,
+  if (req.method !== 'POST') {
+    return res.status(405).json({
+      error: 'Method not allowed',
     });
+  }
 
-    if (!level) {
-      return res.status(404).json({
-        error: 'Level not found',
-      });
-    }
-
-    return res.status(200).json(level);
-  } else if (req.method === 'PUT') {
+  try {
     if (!req.body) {
       return res.status(400).json({
         error: 'Missing required fields',
       });
     }
 
-    const { id } = req.query;
     const { authorNote, name, points, worldIds } = req.body;
 
     if (!name || points === undefined || !worldIds) {
@@ -48,73 +35,35 @@ export default withAuth(async (req: NextApiRequestWithAuth, res: NextApiResponse
 
     await dbConnect();
 
+    const levelId = new ObjectId();
+
     await Promise.all([
-      LevelModel.updateOne({
-        _id: id,
+      LevelModel.create({
+        _id: levelId,
+        authorNote: authorNote,
+        data: '4000000000\n0000000000\n0000000000\n0000000000\n0000000000\n0000000000\n0000000000\n0000000000\n0000000000\n0000000003',
+        height: 10,
+        isDraft: true,
+        leastMoves: 0,
+        name: name,
+        points: points,
+        ts: getTs(),
         userId: req.userId,
-      }, {
-        $set: {
-          authorNote: authorNote,
-          name: name,
-          points: points,
-        },
+        width: 10,
       }),
       WorldModel.updateMany({
         _id: { $in: worldIds },
       }, {
         $addToSet: {
-          levels: id,
-        },
-      }),
-      WorldModel.updateMany({
-        _id: { $nin: worldIds },
-        levels: id,
-      }, {
-        $pull: {
-          levels: id,
+          levels: levelId,
         },
       }),
     ]);
 
-    return await revalidateUniverse(req, res);
-  } else if (req.method === 'DELETE') {
-    const { id } = req.query;
-
-    const level = await LevelModel.findById<Level>(id);
-
-    if (!level) {
-      return res.status(404).json({
-        error: 'Level not found',
-      });
-    }
-
-    if (level.userId.toString() !== req.userId) {
-      return res.status(401).json({
-        error: 'Not authorized to delete this Level',
-      });
-    }
-
-    const stats = await StatModel.find<Stat>({ levelId: id });
-    const userIds = stats.filter(stat => stat.complete).map(stat => stat.userId);
-
-    await Promise.all([
-      LevelModel.deleteOne({ _id: id }),
-      RecordModel.deleteMany({ levelId: id }),
-      ReviewModel.deleteMany({ levelId: id }),
-      StatModel.deleteMany({ levelId: id }),
-      UserModel.updateMany({ _id: { $in: userIds } }, { $inc: { score: -1 } }),
-      WorldModel.updateMany({ levels: id }, { $pull: { levels: id } }),
-    ]);
-
-    // skip revalidation for draft levels
-    if (level.isDraft) {
-      return res.status(200).json({ updated: true });
-    }
-
-    return await revalidateUniverse(req, res);
-  } else {
-    return res.status(405).json({
-      error: 'Method not allowed',
+    return res.status(200).json({ success: true, _id: levelId });
+  } catch (err) {
+    return res.status(500).json({
+      error: 'Error creating level ' + err,
     });
   }
 });
