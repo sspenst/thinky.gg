a6feb6fb467983e86f332273c4b72621e3ce5feb
Danny Miller <k2xl.com@gmail.com>
2022-06-02 14:36:22 -0400

    Added test for image gen

---
+++ tests/pages/api/level/level.image.test.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest } from 'next';
+import { dbDisconnect } from '../../../../lib/dbConnect';
+import { enableFetchMocks } from 'jest-fetch-mock';
+import getLevelImageHandler from '../../../../pages/api/level/image/[id]';
+import { testApiHandler } from 'next-test-api-route-handler';
+
+const USER_ID_FOR_TESTING = '600000000000000000000000';
+const WORLD_ID_FOR_TESTING = '600000000000000000000001';
+const LEVEL_ID_FOR_TESTING = '600000000000000000000002';
+afterAll(async () => {
+  await dbDisconnect();
+});
+enableFetchMocks();
+
+describe('pages/api/level/image/[id]', () => {
+  test('Now we should be able to get the level image', async () => {
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequest = {
+          method: 'GET',
+          query: {
+            id: LEVEL_ID_FOR_TESTING,
+          },
+        } as unknown as NextApiRequest;
+        await getLevelImageHandler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        expect(res.status).toBe(200);
+        expect(res.body.read().length).toBeGreaterThan(1000);
+
+      },
+    });
+  });
+
+});
