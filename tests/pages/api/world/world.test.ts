0a9bc69931b296af6989bb7d4bb4d15c5aed52fc
Danny Miller <k2xl.com@gmail.com>
2022-06-05 11:02:44 -0400

    more test for error states in api for world

--- tests/pages/api/world/world.test.ts
+++ tests/pages/api/world/world.test.ts
@@ -79,7 +79,66 @@ describe('pages/api/world/index.ts', () => {
       },
     });
   });
+  test('Doing a non-POST HTTP method on the create world should fail', async () => {
 
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'GET',
+          userId: USER_ID_FOR_TESTING,
+          cookies: {
+            token: getTokenCookieValue(USER_ID_FOR_TESTING),
+          },
+          body: {
+            authorNote: 'I\'m a nice little world note.',
+            name: 'A Test World',
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await createWorldHandler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(response.error).toBe('Method not allowed');
+        expect(res.status).toBe(405);
+      },
+    });
+  });
+  test('Doing a POST but missing a field should fail', async () => {
+
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'POST',
+          userId: USER_ID_FOR_TESTING,
+          cookies: {
+            token: getTokenCookieValue(USER_ID_FOR_TESTING),
+          },
+          body: {
+            authorNote: 'I\'m a nice little world note.',
+            // name: 'A Test World',
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await createWorldHandler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(response.error).toBe('Missing required fields');
+        expect(res.status).toBe(400);
+      },
+    });
+  });
   test('Doing a POST with world data should be OK', async () => {
 
     await testApiHandler({
