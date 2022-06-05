7b6313625974d63fc8fc04ee51be715efda3ddc7
Danny Miller <k2xl.com@gmail.com>
2022-06-04 22:08:43 -0400

    added tests for setting level order in world levels

---
+++ tests/pages/api/world/world.update.test.ts
@@ -0,0 +1,210 @@
+import dbConnect, { dbDisconnect } from '../../../../lib/dbConnect';
+
+import { LevelModel } from '../../../../models/mongoose';
+import { NextApiRequestWithAuth } from '../../../../lib/withAuth';
+import { ObjectId } from 'bson';
+import { enableFetchMocks } from 'jest-fetch-mock';
+import { getTokenCookieValue } from '../../../../lib/getTokenCookie';
+import getTs from '../../../../helpers/getTs';
+import getWorldHandler from '../../../../pages/api/world-by-id/[id]';
+import { testApiHandler } from 'next-test-api-route-handler';
+import updateWorldHandler from '../../../../pages/api/world/[id]';
+
+afterAll(async() => {
+  await dbDisconnect();
+});
+
+const USER_ID_FOR_TESTING = '600000000000000000000000';
+const WORLD_ID_FOR_TESTING = '600000000000000000000001';
+const levels:ObjectId[] = [];
+
+enableFetchMocks();
+describe('Testing updating world data', () => {
+
+  test('Adding/publishing 10 levels manually should not error', async () => {
+    await dbConnect();
+
+    for (let i = 0; i < 10; i++) {
+      const ts = getTs();
+
+      levels[i] = new ObjectId();
+      const response = await LevelModel.create({
+        _id: levels[i],
+        authorNote: 'test level 1 author note',
+        data: '40010\n12000\n05000\n67890\nABCD3',
+        height: 5,
+        isDraft: false,
+        leastMoves: 20,
+        name: 'level ' + i,
+        points: 0,
+        ts: ts,
+        userId: USER_ID_FOR_TESTING,
+        width: 5,
+      });
+
+      expect(response).toBeDefined();
+    }
+  }, 30000);
+  test('querying for the world should return this world and the single level in it', async () => {
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'GET',
+          userId: USER_ID_FOR_TESTING,
+          cookies: {
+            token: getTokenCookieValue(USER_ID_FOR_TESTING),
+          },
+          query: {
+            id: WORLD_ID_FOR_TESTING, // shouldn't exist
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await getWorldHandler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+
+        expect(res.status).toBe(200);
+        const response = await res.json();
+
+        expect(response.levels).toBeDefined();
+        expect(response.levels.length).toBe(1);
+      },
+    });
+  });
+  test('now we SET the 10 created levels to the world', async () => {
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'PUT',
+          userId: USER_ID_FOR_TESTING,
+          cookies: {
+            token: getTokenCookieValue(USER_ID_FOR_TESTING),
+          },
+          query: {
+            id: WORLD_ID_FOR_TESTING, // shouldn't exist
+          },
+          body: {
+            levels: levels.map(levelId => levelId.toString()),
+            authorNote: 'added 100 levels',
+            name: 'the big world'
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await updateWorldHandler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(response.error).toBeUndefined();
+        expect(res.status).toBe(200);
+        expect(response.updated).toBe(true);
+      },
+    });
+  });
+  test('querying for the world after SETTING 10 levels should return this world and the 10 levels in it (without the one that was original there)', async () => {
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'GET',
+          userId: USER_ID_FOR_TESTING,
+          cookies: {
+            token: getTokenCookieValue(USER_ID_FOR_TESTING),
+          },
+          query: {
+            id: WORLD_ID_FOR_TESTING, // shouldn't exist
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await getWorldHandler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+
+        expect(res.status).toBe(200);
+        const response = await res.json();
+
+        expect(response.levels).toBeDefined();
+        expect(response.levels.length).toBe(10);
+
+        for (let i = 0; i < 10; i++) {
+          expect(response.levels[i]._id).toBe(levels[i].toString());
+        }
+      },
+    });
+  });
+  test('now we SET the 10 created levels AGAIN but this time we swap first and last item of levels', async () => {
+    const tmp = levels[0];
+
+    levels[0] = levels[levels.length - 1];
+    levels[levels.length - 1] = tmp;
+
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'PUT',
+          userId: USER_ID_FOR_TESTING,
+          cookies: {
+            token: getTokenCookieValue(USER_ID_FOR_TESTING),
+          },
+          query: {
+            id: WORLD_ID_FOR_TESTING, // shouldn't exist
+          },
+          body: {
+            levels: levels.map(levelId => levelId.toString()),
+            authorNote: 'added 100 levels',
+            name: 'the big world'
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await updateWorldHandler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(response.error).toBeUndefined();
+        expect(res.status).toBe(200);
+        expect(response.updated).toBe(true);
+      },
+    });
+  });
+  test('querying for the world after setting 10 levels (after the first and last level have been swapped) should return this world and the 10 levels in the new order', async () => {
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'GET',
+          userId: USER_ID_FOR_TESTING,
+          cookies: {
+            token: getTokenCookieValue(USER_ID_FOR_TESTING),
+          },
+          query: {
+            id: WORLD_ID_FOR_TESTING, // shouldn't exist
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await getWorldHandler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+
+        expect(res.status).toBe(200);
+        const response = await res.json();
+
+        expect(response.levels).toBeDefined();
+        expect(response.levels.length).toBe(10);
+
+        for (let i = 0; i < 10; i++) {
+          expect(response.levels[i]._id).toBe(levels[i].toString());
+        }
+      },
+    });
+  });
+});
