5fa51aafd9c93cc45c49c601df9ea3342010a706
Danny Miller <k2xl.com@gmail.com>
2022-06-05 09:54:34 -0400

    add more tests to cover world api 100%

--- tests/pages/api/world/world.delete.test.ts
+++ tests/pages/api/world/world.delete.test.ts
@@ -1,4 +1,5 @@
 import { NextApiRequestWithAuth } from '../../../../lib/withAuth';
+import { ObjectId } from 'bson';
 import { dbDisconnect } from '../../../../lib/dbConnect';
 import { enableFetchMocks } from 'jest-fetch-mock';
 import { getTokenCookieValue } from '../../../../lib/getTokenCookie';
@@ -11,10 +12,69 @@ afterAll(async() => {
 });
 const USER_ID_FOR_TESTING = '600000000000000000000000';
 const WORLD_ID_FOR_TESTING = '600000000000000000000001';
+const differentUser = '600000000000000000000006';
 
 enableFetchMocks();
 describe('pages/api/world/index.ts', () => {
-  test('Doing a DELETE for world should not error', async () => {
+  test('Doing a DELETE for unknown world should error', async () => {
+
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'DELETE',
+          cookies: {
+            token: getTokenCookieValue(differentUser),
+          },
+          query: {
+            id: new ObjectId().toString(), // unknown
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await modifyWorldHandler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(response.error).toBe('World not found');
+        expect(response.success).toBeUndefined();
+        expect(res.status).toBe(404);
+      },
+    });
+  });
+  test('Doing a DELETE when we don\'t own the world should not error', async () => {
+
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'DELETE',
+          cookies: {
+            token: getTokenCookieValue(differentUser),
+          },
+          query: {
+            id: WORLD_ID_FOR_TESTING,
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await modifyWorldHandler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(response.error).toBe('Not authorized to delete this World');
+        expect(response.success).toBeUndefined();
+        expect(res.status).toBe(401);
+      },
+    });
+  });
+  test('Doing a DELETE for valid world that i own should not error', async () => {
 
     await testApiHandler({
       handler: async (_, res) => {
@@ -38,7 +98,7 @@ describe('pages/api/world/index.ts', () => {
         const response = await res.json();
 
         expect(response.error).toBeUndefined();
-        expect(response.success).toBeUndefined();
+        expect(response.success).toBeUndefined(); /** @TODO: Probably should be defined */
         expect(res.status).toBe(200);
       },
     });
