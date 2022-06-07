2818e5fb2de0a0ff8dbb472a9f01bfffdf85d345
Danny Miller <k2xl.com@gmail.com>
2022-06-07 14:54:37 -0400

    check-token 100% code coverage

---
+++ tests/pages/api/check-token/check-token.test.ts
@@ -0,0 +1,77 @@
+import { NextApiRequestWithAuth } from '../../../../lib/withAuth';
+import checkTokenHandler from '../../../../pages/api/check-token/index';
+import { dbDisconnect } from '../../../../lib/dbConnect';
+import { enableFetchMocks } from 'jest-fetch-mock';
+import { getTokenCookieValue } from '../../../../lib/getTokenCookie';
+import { testApiHandler } from 'next-test-api-route-handler';
+
+const USER_ID_FOR_TESTING = '600000000000000000000000';
+const differentUser = '600000000000000000000006';
+
+const WORLD_ID_FOR_TESTING = '600000000000000000000001';
+let level_id_1: string;
+let level_id_2: string;
+let level_id_3: string;
+
+afterAll(async () => {
+  await dbDisconnect();
+});
+enableFetchMocks();
+
+describe('Testing check token handler', () => {
+  test('Calling with wrong http method should fail', async () => {
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'POST',
+          cookies: {
+            token: getTokenCookieValue(USER_ID_FOR_TESTING),
+          },
+          body: {
+
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await checkTokenHandler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(response.error).toBe('Method not allowed');
+        expect(res.status).toBe(405);
+      },
+    });
+  });
+  test('Calling with correct http method should be OK', async () => {
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'GET',
+          cookies: {
+            token: getTokenCookieValue(USER_ID_FOR_TESTING),
+          },
+          body: {
+
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await checkTokenHandler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(response.error).toBeUndefined();
+        expect(response.success).toBe(true);
+        expect(res.status).toBe(200);
+      },
+    });
+  });
+});
