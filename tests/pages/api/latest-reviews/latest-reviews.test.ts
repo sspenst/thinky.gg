4ee6aae1052620f9e79fef2956c4d22dfa74c5e6
Danny Miller <k2xl.com@gmail.com>
2022-06-07 15:32:27 -0400

    100% line coverage for latest-reviews

--- tests/pages/api/latest-reviews/latest-reviews.test.ts
+++ tests/pages/api/latest-reviews/latest-reviews.test.ts
@@ -1,14 +1,15 @@
-import { LevelModel } from '../../../../models/mongoose';
 import { NextApiRequestWithAuth } from '../../../../lib/withAuth';
 import { ObjectId } from 'bson';
+import { ReviewModel } from '../../../../models/mongoose';
 import { dbDisconnect } from '../../../../lib/dbConnect';
 import { enableFetchMocks } from 'jest-fetch-mock';
 import { getTokenCookieValue } from '../../../../lib/getTokenCookie';
 import getTs from '../../../../helpers/getTs';
-import latestLevelsHandler from '../../../../pages/api/latest-levels/index';
+import latestReviewsHandler from '../../../../pages/api/latest-reviews/index';
 import { testApiHandler } from 'next-test-api-route-handler';
 
 const USER_ID_FOR_TESTING = '600000000000000000000000';
+const LEVEL_ID_FOR_TESTING = '600000000000000000000002';
 
 afterEach(() => {
   jest.restoreAllMocks();
@@ -18,7 +19,7 @@ afterAll(async () => {
 });
 enableFetchMocks();
 
-describe('Testing latest levels api', () => {
+describe('Testing latest reviews api', () => {
   test('Calling with wrong http method should fail', async () => {
     await testApiHandler({
       handler: async (_, res) => {
@@ -35,7 +36,7 @@ describe('Testing latest levels api', () => {
           },
         } as unknown as NextApiRequestWithAuth;
 
-        await latestLevelsHandler(req, res);
+        await latestReviewsHandler(req, res);
       },
       test: async ({ fetch }) => {
         const res = await fetch();
@@ -62,7 +63,7 @@ describe('Testing latest levels api', () => {
           },
         } as unknown as NextApiRequestWithAuth;
 
-        await latestLevelsHandler(req, res);
+        await latestReviewsHandler(req, res);
       },
       test: async ({ fetch }) => {
         const res = await fetch();
@@ -76,20 +77,14 @@ describe('Testing latest levels api', () => {
   });
   test('Should always be limited to 10 levels and should only return drafts', async () => {
     for (let i = 0; i < 25; i++) {
-      await LevelModel.create({
+      await ReviewModel.create({
         _id: new ObjectId(),
-        authorNote: 'level ' + i + ' author note',
-        data: '40000\n12000\n05000\n67890\nABCD3',
-        height: 5,
-        isDraft: i % 2 === 0,
-        leastMoves: 20,
-        name: 'level ' + i,
-        points: 0,
+        levelId: LEVEL_ID_FOR_TESTING,
+        score: 5,
+        text: 'My review ' + i,
         ts: getTs(),
-        userId: USER_ID_FOR_TESTING,
-        width: 5,
+        userId: USER_ID_FOR_TESTING
       });
-
     }
 
     await testApiHandler({
@@ -107,7 +102,7 @@ describe('Testing latest levels api', () => {
           },
         } as unknown as NextApiRequestWithAuth;
 
-        await latestLevelsHandler(req, res);
+        await latestReviewsHandler(req, res);
       },
       test: async ({ fetch }) => {
         const res = await fetch();
@@ -117,20 +112,21 @@ describe('Testing latest levels api', () => {
         expect(response.length).toBe(10);
         expect(res.status).toBe(200);
 
-        for (let i = 0; i < response.length; i++) {
-          expect(response[i].isDraft).toBe(false);
-        }
       },
     });
   }, 30000);
   test('If mongo query returns null we should fail gracefully', async () => {
 
-    jest.spyOn(LevelModel, 'find').mockReturnValueOnce({
+    jest.spyOn(ReviewModel, 'find').mockReturnValueOnce({
+
       populate: function() {
         return {
-          sort: function() {
-            return { limit: function() {
-              return null;
+          populate: function() {
+            return { sort: function() {
+              return { limit: function() {
+                return null;
+              }
+              };
             }
             };
           }
@@ -153,20 +149,20 @@ describe('Testing latest levels api', () => {
           },
         } as unknown as NextApiRequestWithAuth;
 
-        await latestLevelsHandler(req, res);
+        await latestReviewsHandler(req, res);
       },
       test: async ({ fetch }) => {
         const res = await fetch();
         const response = await res.json();
 
-        expect(response.error).toBe('Error finding Levels');
+        expect(response.error).toBe('Error finding Reviews');
         expect(res.status).toBe(500);
       },
     });
   });
   test('If mongo query throw exception we should fail gracefully', async () => {
 
-    jest.spyOn(LevelModel, 'find').mockReturnValueOnce({ 'thisobjectshouldthrowerror': true } as any);
+    jest.spyOn(ReviewModel, 'find').mockReturnValueOnce({ 'thisobjectshouldthrowerror': true } as any);
 
     await testApiHandler({
       handler: async (_, res) => {
@@ -183,13 +179,13 @@ describe('Testing latest levels api', () => {
           },
         } as unknown as NextApiRequestWithAuth;
 
-        await latestLevelsHandler(req, res);
+        await latestReviewsHandler(req, res);
       },
       test: async ({ fetch }) => {
         const res = await fetch();
         const response = await res.json();
 
-        expect(response.error).toBe('Error finding Levels');
+        expect(response.error).toBe('Error finding Reviews');
         expect(res.status).toBe(500);
       },
     });
