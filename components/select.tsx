37180c72a5f6b984eaa586c363c882a95249bdef
Danny Miller <k2xl.com@gmail.com>
2022-06-08 13:53:14 -0400

    100% logout api test line coverage

--- components/select.tsx
+++ components/select.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useEffect, useState } from 'react';
-
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import SelectCard from './selectCard';
