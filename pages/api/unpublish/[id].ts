2bf82be83be911655e33d2e42c395c57acee8aad
Spencer Spenst <spencerspenst@gmail.com>
2022-06-19 11:48:07 -0700

    decrement calc_records on unpublish and delete

--- pages/api/unpublish/[id].ts
+++ pages/api/unpublish/[id].ts
@@ -2,6 +2,7 @@ import { LevelModel, RecordModel, ReviewModel, StatModel, UserModel } from '../.
 import withAuth, { NextApiRequestWithAuth } from '../../../lib/withAuth';
 import Level from '../../../models/db/level';
 import type { NextApiResponse } from 'next';
+import Record from '../../../models/db/record';
 import Stat from '../../../models/db/stat';
 import revalidateUniverse from '../../../helpers/revalidateUniverse';
 
@@ -26,6 +27,14 @@ export default withAuth(async (req: NextApiRequestWithAuth, res: NextApiResponse
     return res.status(401).json({
       error: 'Not authorized to delete this Level',
     });
+  }
+
+  const record = await RecordModel.findOne<Record>({ levelId: id }).sort({ ts: -1 });
+
+  // update calc_records if the record was set by a different user
+  if (record && record.userId.toString() !== req.userId) {
+    // NB: await to avoid multiple user updates in parallel
+    await UserModel.updateOne({ _id: record.userId }, { $inc: { calc_records: -1 } });
   }
 
   const stats = await StatModel.find<Stat>({ levelId: id });
