a6c45b1b37dde697b892926d472abcfae6ab5540
Danny Miller <k2xl.com@gmail.com>
2022-06-20 17:41:54 -0400

    change default score to 0.00

--- models/schemas/levelSchema.ts
+++ models/schemas/levelSchema.ts
@@ -69,7 +69,7 @@ const LevelSchema = new mongoose.Schema<Level>(
     calc_reviews_score_avg: {
       type: Number,
       required: false,
-      default: 0.5
+      default: 0.00
     },
     calc_reviews_score_count: {
       type: Number,
@@ -79,7 +79,7 @@ const LevelSchema = new mongoose.Schema<Level>(
     calc_reviews_score_laplace: {
       type: Number,
       required: false,
-      default: 0.5
+      default: 0.00
     },
     calc_records_last_ts: {
       type: Number,
