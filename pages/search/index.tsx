dd7042ee302e28ddc4d5ef1ed25f4895bb231e9b
Danny Miller <k2xl.com@gmail.com>
2022-06-22 19:33:49 -0400

    types/debounce

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import DataTable, { Alignment } from 'react-data-table-component';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Level from '../../models/db/level';
