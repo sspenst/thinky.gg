eb4c015c3fc750c7a8e53dec4841aa7f5271be91
Danny Miller <k2xl.com@gmail.com>
2022-06-27 10:55:25 -0400

    should disable zoom for mobile

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+
 import React from 'react';
 
 class MyDocument extends Document {
@@ -17,6 +18,7 @@ class MyDocument extends Document {
           <link href='/favicon.png' rel='icon' />
           <meta name='theme-color' content='#000000' />
           <meta name='description' content='Pathology' />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"></meta>
         </Head>
         <body className='theme-modern'>
           <Main />
