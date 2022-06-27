03aba797f9b6ed4e44bd3583ba83e726385e42c0
Spencer Spenst <spencerspenst@gmail.com>
2022-06-27 14:38:45 -0700

    refactoring; css updates

--- components/modal/reviewsModal.tsx
+++ components/modal/reviewsModal.tsx
@@ -1,46 +1,10 @@
 import React, { useCallback, useEffect, useState } from 'react';
-
 import AddReviewModal from './addReviewModal';
 import DeleteReviewModal from './deleteReviewModal';
-import Link from 'next/link';
+import FormattedReview from '../formattedReview';
 import Modal from '.';
 import Review from '../../models/db/review';
-import getFormattedDate from '../../helpers/getFormattedDate';
 import useUser from '../../hooks/useUser';
-
-interface ReviewDivProps {
-  review: Review;
-}
-export function starComponent(stars:number) {
-  const starsArray = [];
-
-  for (let i = 0; i < 5; i++) {
-    if (stars > i) {
-      starsArray.push(<svg className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>);
-    } else {
-      starsArray.push(<svg className="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>);
-    }
-  }
-
-  return ( <div className='flex items-center'>{ starsArray }</div> );
-}
-export function ReviewDiv({ review }: ReviewDivProps) {
-  return (
-    <div className="block p-1 max-w-sm bg-gray rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <Link href={`/profile/${review.userId._id}`} passHref>
-        <a className='font-bold underline'>
-          {review.userId.name}
-        </a>
-
-      </Link>
-      { ' - '}<span className='italic'>{getFormattedDate(review.ts)}</span>
-      <div className='flex items-center' style={{ justifyContent: 'center' }}>
-        {review.score ? starComponent(review.score) : ''}
-      </div>
-      <span style={{ whiteSpace: 'pre-wrap' }}>{review.text}</span>
-    </div>
-  );
-}
 
 interface ReviewsModalProps {
   closeModal: () => void;
@@ -86,7 +50,12 @@ export default function ReviewsModal({ closeModal, isOpen, levelId }: ReviewsMod
 
       const review = reviews[i];
 
-      reviewDivs.push(<ReviewDiv key={i} review={review} />);
+      reviewDivs.push(
+        <FormattedReview
+          review={review}
+          user={review.userId}
+        />
+      );
 
       if (review.score) {
         reviewsWithScore++;
