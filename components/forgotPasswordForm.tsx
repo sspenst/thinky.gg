c9e1a9e9c95eb838755f65c8ae940e04d4b4ffe1
Danny Miller <k2xl.com@gmail.com>
2022-06-27 19:35:22 -0400

    forgot password form tailwinded

--- components/forgotPasswordForm.tsx
+++ components/forgotPasswordForm.tsx
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from 'react';
+
 import { AppContext } from '../contexts/appContext';
 import { PageContext } from '../contexts/pageContext';
+import toast from 'react-hot-toast';
 
 export default function ForgotPasswordForm() {
   const [email, setEmail] = useState<string>('');
@@ -12,6 +14,8 @@ export default function ForgotPasswordForm() {
     event.preventDefault();
 
     setIsLoading(true);
+    toast.dismiss();
+    toast.loading('Sending reset email...');
 
     fetch('/api/forgot-password', {
       method: 'POST',
@@ -23,61 +27,36 @@ export default function ForgotPasswordForm() {
       }
     }).then(res => {
       if (res.status === 200) {
+        toast.dismiss();
+        toast.success('Email sent');
         setIsSent(true);
       } else {
         throw res.text();
       }
     }).catch(err => {
       console.error(err);
-      alert('Error resetting password');
+      toast.dismiss();
+      toast.error('Error sending password reset email for this email address');
     });
   }
 
   return (
-    <>
-      <form
-        onSubmit={onSubmit}
-        style={{
-          width: windowSize.width,
-        }}
-      >
-        <div
-          style={{
-            display: 'table',
-            margin: '0 auto',
-          }}
-        >
-          <div>
-            <input
-              type='email'
-              name='email'
-              placeholder='Enter email'
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-              style={{ color: 'rgb(0, 0, 0)' }}
-              required
-            />
-          </div>
+
+    <div className="w-full max-w-xs mx-auto p-3 py-12">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4 ">
+          <label className="block text-gray-700 text-sm font-bold mb-2 " htmlFor="email">
+        Send a forgot email request
+          </label>
+          <input required onChange={e => setEmail(e.target.value)} value={email} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email"/>
         </div>
-        <div
-          style={{
-            display: 'table',
-            margin: '0 auto',
-          }}
-        >
-          <button className='underline' type='submit'>Reset password</button>
+        <div className="flex items-center justify-between">
+          <button disabled={isSent} onClick={onSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        Send
+          </button>
         </div>
       </form>
-      {isSent ?
-        <div
-          style={{
-            display: 'table',
-            margin: '0 auto',
-          }}
-        >
-          Email sent!
-        </div>
-        : null}
-    </>
+    </div>
+
   );
 }
--- components/loginForm.tsx
+++ components/loginForm.tsx
@@ -1,5 +1,8 @@
 import React, { useContext, useState } from 'react';
+
+import Link from 'next/link';
 import { PageContext } from '../contexts/pageContext';
+import toast from 'react-hot-toast';
 import { useRouter } from 'next/router';
 
 export default function LoginForm() {
@@ -7,8 +10,11 @@ export default function LoginForm() {
   const [password, setPassword] = useState<string>('');
   const router = useRouter();
   const { windowSize } = useContext(PageContext);
+  const [ error_message, setError_message ] = useState<string>('');
 
   function onSubmit(event: React.FormEvent) {
+    toast.dismiss();
+    toast.loading('Logging in');
     event.preventDefault();
     fetch('/api/login', {
       method: 'POST',
@@ -22,17 +28,52 @@ export default function LoginForm() {
       }
     }).then(res => {
       if (res.status === 200) {
+        toast.dismiss();
+        toast.success('Logged in');
         router.push('/');
       } else {
         throw res.text();
       }
     }).catch(err => {
-      console.error(err);
-      alert('Error logging in please try again');
+      toast.dismiss();
+      toast.error('Could not log in. Please try again');
     });
   }
 
+  // vertical center
   return (
+
+    <div className="w-full max-w-xs mx-auto p-3 py-12">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4 ">
+          <label className="block text-gray-700 text-sm font-bold mb-2 " htmlFor="username">
+        Username
+          </label>
+          <input onChange={e => setName(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+        Password
+          </label>
+          <input onChange={e => setPassword(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+          <p className="text-red-500 text-xs italic">{error_message}</p>
+        </div>
+        <div className="flex items-center justify-between">
+          <button onClick={onSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        Sign In
+          </button>
+          <Link href="/forgot-password">
+            <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">Forgot Password?</a>
+          </Link>
+        </div>
+      </form>
+      <p className="text-center text-gray-500 text-xs">
+        Hang out in our <Link href='https://discord.gg/NsN8SBEZGN'><a className='underline'>Discord server</a></Link>
+      </p>
+    </div>
+  );
+/*
+
     <form
       onSubmit={onSubmit}
       style={{
@@ -77,5 +118,6 @@ export default function LoginForm() {
         <button className='underline' type='submit'>Log In</button>
       </div>
     </form>
-  );
+  )
+  */
 }
