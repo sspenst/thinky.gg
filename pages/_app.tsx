12343a6db52d526ad911b5f7a9e17b89c8ee35b0
Spencer Spenst <spencerspenst@gmail.com>
2022-06-27 15:09:09 -0700

    viewport meta should not be in document (#241)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/global.css';
 import React, { useState } from 'react';
 import { AppContext } from '../contexts/appContext';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import ProgressBar from '../components/progressBar';
 import { Toaster } from 'react-hot-toast';
 
@@ -9,12 +10,17 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   const [isLoading, setIsLoading] = useState<boolean>();
 
   return (
-    <AppContext.Provider value={{
-      setIsLoading: setIsLoading,
-    }}>
-      <ProgressBar isLoading={isLoading} />
-      <Toaster toastOptions={{ duration: 1500 }}/>
-      <Component {...pageProps} />
-    </AppContext.Provider>
+    <>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
+      </Head>
+      <AppContext.Provider value={{
+        setIsLoading: setIsLoading,
+      }}>
+        <ProgressBar isLoading={isLoading} />
+        <Toaster toastOptions={{ duration: 1500 }}/>
+        <Component {...pageProps} />
+      </AppContext.Provider>
+    </>
   );
 }
