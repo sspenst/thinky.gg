4e8069df758481178e7dbd46397df8a1dca41acb
Spencer Spenst <spencerspenst@gmail.com>
2022-07-08 14:32:13 -0700

    Revert "next v12.2.1 (#274)" (#275)
    This reverts commit 8da035f3120e14c5db8640fcf0c0559902a8c59c.

--- models/schemas/userSchema.ts
+++ models/schemas/userSchema.ts
@@ -1,6 +1,7 @@
 import { LevelModel } from '../mongoose';
 import bcrypt from 'bcrypt';
 import generateSlug from '../../helpers/generateSlug';
+import getTs from '../../helpers/getTs';
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
