98357d0c1ce141b60811d49cc2ce0c006ddb712a
Spencer Spenst <spencerspenst@gmail.com>
2022-07-08 14:01:45 -0700

    Revert "Svg controls (#270)"
    This reverts commit bdb8458f1f9ceff4500899e41542de564339ecde.

--- models/control.ts
+++ models/control.ts
@@ -1,18 +1,18 @@
 export default class Control {
   action: () => void;
   disabled: boolean;
-  element: JSX.Element;
   id: string;
+  text: string;
 
   constructor(
     id: string,
     action: () => void,
-    element: JSX.Element,
+    text: string,
     disabled = false,
   ) {
     this.action = action;
     this.disabled = disabled;
-    this.element = element;
     this.id = id;
+    this.text = text;
   }
 }
