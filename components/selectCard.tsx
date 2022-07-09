2249f4529d793bd78450c924faf88bc917e30264
Danny Miller <k2xl.com@gmail.com>
2022-07-09 17:44:31 -0400

    reorganized

--- components/selectCard.tsx
+++ components/selectCard.tsx
@@ -1,8 +1,9 @@
 import { DropTargetMonitor, useDrag, useDrop } from 'react-dnd';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Link from 'next/link';
 import SelectOption from '../models/selectOption';
 import classNames from 'classnames';
+import { relative } from 'path';
 
 interface SelectCardProps {
   draggable?: boolean;
@@ -69,72 +70,106 @@ export default function SelectCard({
         display: 'inline-block',
         padding: padding,
         verticalAlign: 'middle',
+        position: 'relative',
+        overflow: 'hidden',
       }}
     >
-      {option.href ?
-        <Link href={(option.disabled) ? '' : option.href} passHref prefetch={prefetch}>
-          <a
-            className={classNames(
-              'border-2 rounded-md',
-              { 'pointer-events-none': (option.disabled || option.draggable) },
-              { 'scale': !option.disabled },
-              { 'text-xl': !option.stats },
-            )}
-            style={{
-              backgroundColor: spec.isOver ? '#141' : '',
-              borderColor: color,
-              color: color,
-              display: 'table',
-              height: option.height,
-              padding: 10,
-              textAlign: 'center',
-              width: optionWidth,
-            }}
-          >
-            <span style={{
-              display: 'table-cell',
-              verticalAlign: 'middle',
-            }}>
-              {option.text}
-              {option.author ?
-                <>
-                  <br/>
-                  <span className=''>
-                    {option.author}
-                  </span>
-                </>
-                : null}
-              {option.points !== undefined ?
-                <>
-                  <br/>
-                  <span className='italic'>
-              Difficulty: {option.points}
-                  </span>
-                </>
-                : null}
-              <br/>
-              {option.stats ?
-                <>
-                  {option.stats.getText()}
-                  <br/>
-                </>
-                : null}
-            </span>
-          </a>
-        </Link>
-        :
-        <div
-          className={'text-xl'}
+      <div className="wrapper"
+        style={{
+          width: optionWidth,
+          height: option.height,
+          overflow: 'hidden',
+          position: 'relative',
+        }}
+      >
+        <div className='background rounded-md'
           style={{
-            height: option.height,
-            lineHeight: option.height + 'px',
-            textAlign: 'center',
-            verticalAlign: 'middle',
+            backgroundImage: 'url(/api/level/image/' + option.id + ')',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
             width: optionWidth,
-          }}>
-          {option.text}
-        </div>
-      }
+            height: option.height,
+            position: 'absolute',
+            opacity: 0.25,
+            transform: 'scale(1.6)',
+
+          }}
+        />
+        {option.href ?
+          <Link href={(option.disabled) ? '' : option.href} passHref prefetch={prefetch}>
+
+            <a
+              className={classNames(
+                'border-2 rounded-md',
+                { 'pointer-events-none': (option.disabled || option.draggable) },
+                { 'scale': !option.disabled },
+                { 'text-xl': !option.stats },
+              )}
+              style={{
+                backgroundColor: spec.isOver ? '#141' : '',
+                borderColor: color,
+                color: spec.isOver ? 'black' : color,
+                display: 'table',
+                height: option.height,
+
+                textAlign: 'center',
+                width: optionWidth,
+              }}
+            >
+
+              <span className={option.text.length < 25 ? 'font-bold display-block text-center' : 'font-bold display-block text-xs text-center'}
+                style={{
+                  //transform: 'translateX(calc(' + optionWidth + 'px - 100%));'
+                  display: 'table-cell',
+                  verticalAlign: 'middle',
+                }}
+              >{option.text}
+
+                <span className='text-center'>
+                  {option.author ?
+                    <>
+                      <br/>
+                      <span className=''>
+                        {option.author}
+                      </span>
+                    </>
+                    : null}
+                  {option.points !== undefined ?
+                    <>
+                      <br/>
+                      <span className='italic text-sm'>
+                Difficulty: {option.points}
+                      </span>
+                    </>
+                    : null}
+                  <br/>
+                  <span className='italic text-sm'>
+                    {option.stats ?
+                      <>
+                        {option.stats.getText()}
+                        <br/>
+                      </>
+                      : null}
+                  </span>
+                </span>
+
+              </span>
+            </a>
+          </Link>
+          :
+          <div
+            className={'text-xl'}
+            style={{
+              height: option.height,
+              lineHeight: option.height + 'px',
+              textAlign: 'center',
+              verticalAlign: 'middle',
+              width: optionWidth,
+            }}>
+            {option.text}
+          </div>
+        }
+      </div>
     </div>
   );
 }
