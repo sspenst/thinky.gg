ea457d6fcd069e5290043006ed5e66418695eb12
Danny Miller <k2xl.com@gmail.com>
2022-07-11 15:07:57 -0400

    api refactor and test

---
+++ pages/api/play-attempt/index.ts
@@ -0,0 +1,93 @@
+import { LevelModel, PlayAttemptModel, StatModel } from '../../../models/mongoose';
+import withAuth, { NextApiRequestWithAuth } from '../../../lib/withAuth';
+
+import { NextApiResponse } from 'next';
+import { ObjectId } from 'bson';
+import PlayAttempt from '../../../models/schemas/playAttemptSchema';
+import dbConnect from '../../../lib/dbConnect';
+
+export default withAuth(async (req: NextApiRequestWithAuth, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({
+      error: 'Method not allowed',
+    });
+  }
+
+  if (!req.body) {
+    return res.status(400).json({
+      error: 'Missing required parameters',
+    });
+  }
+
+  const { levelId } = req.body;
+
+  if (!levelId) {
+    return res.status(400).json({
+      error: 'Missing required parameters',
+    });
+  }
+
+  await dbConnect();
+
+  // first don't do anything if user has already beaten this level
+  const [level, playAttempt] = await Promise.all([
+    LevelModel.findById(levelId),
+    PlayAttemptModel.findOne({
+      userId: req.user._id,
+      levelId: levelId,
+    }, {
+      sort: { endTime: -1 }
+    })
+  ]);
+
+  if (!level) {
+    return res.status(404).json({
+      error: 'Level not found',
+    });
+  }
+
+  const now = new Date().getTime();
+
+  if (playAttempt) {
+    if (playAttempt.didWin) {
+      return res.status(412).json({
+        error: 'You have already beaten this level',
+      // 412 to tell the app to stop sending requests to this endpoint. Technically there is an edge case where if someone has the level already open and have already beaten the level and while it is open another player beats the record, there current play wouldnt be logged.
+      });
+    }
+
+    // Check if the endtime is within 15 minutes of now, if so update it to current time
+
+    const endTime = playAttempt.endTime;
+    const timeDiff = now - endTime;
+    const diffMinutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+
+    if (diffMinutes < 15) {
+      playAttempt.endTime = now;
+      playAttempt.save();
+
+      return res.status(200).json({
+        message: 'updated',
+        playAttempt: playAttempt,
+      });
+    }
+  }
+
+  // if it has been more than 15 minutes OR if we have no play attempt record create a new play attempt
+  const newPlayAttempt = new PlayAttemptModel({
+    _id: new ObjectId(),
+    userId: req.user._id,
+    levelId: levelId,
+    startTime: now,
+    endTime: now,
+    didWin: false,
+  });
+
+  await newPlayAttempt.save();
+
+  return res.status(200).json({
+    message: 'created',
+    playAttempt: newPlayAttempt,
+  });
+
+});
---
+++ tests/pages/api/play-attempt/play-attempt.test.ts
@@ -0,0 +1,278 @@
+import { LevelModel } from '../../../../models/mongoose';
+import { NextApiRequestWithAuth } from '../../../../lib/withAuth';
+import { ObjectId } from 'bson';
+import { dbDisconnect } from '../../../../lib/dbConnect';
+import { enableFetchMocks } from 'jest-fetch-mock';
+import { getTokenCookieValue } from '../../../../lib/getTokenCookie';
+import handler from '../../../../pages/api/play-attempt/index';
+import { testApiHandler } from 'next-test-api-route-handler';
+
+const USER_ID_FOR_TESTING = '600000000000000000000000';
+const LEVEL_ID_FOR_TESTING = '600000000000000000000002';
+const differentUser = '600000000000000000000006';
+
+afterAll(async () => {
+  await dbDisconnect();
+});
+enableFetchMocks();
+
+describe('Testing stats api', () => {
+  test('Wrong HTTP method should fail', async () => {
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'PATCH',
+          cookies: {
+            token: getTokenCookieValue(USER_ID_FOR_TESTING),
+          },
+          body: {
+
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await handler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(response.error).toBe('Method not allowed');
+        expect(res.status).toBe(405);
+      },
+    });
+  });
+  test('Doing a POST with no body should error', async () => {
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'POST',
+          cookies: {
+            token: getTokenCookieValue(USER_ID_FOR_TESTING),
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await handler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(response.error).toBe('Missing required parameters');
+        expect(res.status).toBe(400);
+      },
+    });
+  });
+  test('Doing a POST with a body but no params should error', async () => {
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'POST',
+          cookies: {
+            token: getTokenCookieValue(USER_ID_FOR_TESTING),
+          },
+          body: {
+
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await handler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(response.error).toBe('Missing required parameters');
+        expect(res.status).toBe(400);
+      },
+    });
+  });
+  test('Doing a POST with an invalid level should error', async () => {
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'POST',
+          cookies: {
+            token: getTokenCookieValue(USER_ID_FOR_TESTING),
+          },
+          body: {
+            levelId: new ObjectId()
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await handler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(response.error).toBe('Level not found');
+        expect(res.status).toBe(404);
+      },
+    });
+  });
+  test('Doing a POST should work', async () => {
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'POST',
+          cookies: {
+            token: getTokenCookieValue(USER_ID_FOR_TESTING),
+          },
+          body: {
+            levelId: LEVEL_ID_FOR_TESTING
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await handler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(res.status).toBe(200);
+        expect(response.message).toBe('created');
+      },
+    });
+  });
+  test('Doing a second POST should work by update', async () => {
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'POST',
+          cookies: {
+            token: getTokenCookieValue(USER_ID_FOR_TESTING),
+          },
+          body: {
+            levelId: LEVEL_ID_FOR_TESTING
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await handler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(res.status).toBe(200);
+        expect(response.message).toBe('updated');
+      },
+    });
+  });
+  test('Doing a GET should return a stats object', async () => {
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'GET',
+          cookies: {
+            token: getTokenCookieValue(USER_ID_FOR_TESTING),
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await handler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(response.error).toBeUndefined();
+        expect(response.length).toBe(1);
+        expect(response[0].attempts).toBe(1);
+        expect(response[0].complete).toBe(true);
+        expect(response[0].userId).toBe(USER_ID_FOR_TESTING);
+        expect(response[0].moves).toBe(14);
+        expect(res.status).toBe(200);
+
+        const lvl = await LevelModel.findById(LEVEL_ID_FOR_TESTING);
+
+        expect(lvl.calc_stats_players_beaten).toBe(1);
+      },
+    });
+  });
+  test('Doing a PUT with a DIFFERENT user with a level solution (that is long) should be OK', async () => {
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'PUT',
+          cookies: {
+            token: getTokenCookieValue(differentUser),
+          },
+          body: {
+            codes: ['ArrowRight', 'ArrowRight', 'ArrowDown', 'ArrowRight', 'ArrowUp', 'ArrowLeft', 'ArrowLeft', 'ArrowDown', 'ArrowDown', 'ArrowRight', 'ArrowRight', 'ArrowRight', 'ArrowDown', 'ArrowDown'],
+            levelId: LEVEL_ID_FOR_TESTING
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await handler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(response.error).toBeUndefined();
+        expect(response.success).toBe(true);
+        expect(res.status).toBe(200);
+        const lvl = await LevelModel.findById(LEVEL_ID_FOR_TESTING);
+
+        expect(lvl.leastMoves).toBe(14);
+        expect(lvl.calc_stats_players_beaten).toBe(2);
+      },
+    });
+  });
+  test('Doing a PUT with a different user with correct minimum level solution should be OK', async () => {
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          method: 'PUT',
+          cookies: {
+            token: getTokenCookieValue(differentUser),
+          },
+          body: {
+            codes: [ 'ArrowRight', 'ArrowDown', 'ArrowRight', 'ArrowRight', 'ArrowRight', 'ArrowDown', 'ArrowDown', 'ArrowDown'],
+            levelId: LEVEL_ID_FOR_TESTING
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        } as unknown as NextApiRequestWithAuth;
+
+        await handler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(response.error).toBeUndefined();
+        expect(response.success).toBe(true);
+        expect(res.status).toBe(200);
+        const lvl = await LevelModel.findById(LEVEL_ID_FOR_TESTING);
+
+        expect(lvl.leastMoves).toBe(8);
+        expect(lvl.calc_stats_players_beaten).toBe(1);
+      },
+    });
+  });
+});
