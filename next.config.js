1c66fa94f7848279c3cae281806a45a185ca3e04
Danny Miller <k2xl.com@gmail.com>
2022-07-15 08:25:34 -0400

    reduce cpus to 1 for build

--- next.config.js
+++ next.config.js
@@ -29,4 +29,11 @@ module.exports = {
       'tests',
     ],
   },
+  experimental: {
+    // This is experimental but can
+    // be enabled to allow parallel threads
+    // with nextjs automatic static generation
+    workerThreads: false,
+    cpus: 1
+  },
 };
