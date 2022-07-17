b2bfa1f237912e7a842e44380cb696cc302311c9
Spencer Spenst <spencerspenst@gmail.com>
2022-07-17 14:13:30 -0700

    rename levelimages to images (#309)

--- models/db/image.ts
+++ models/db/image.ts
@@ -1,12 +1,12 @@
 import Level from './level';
 import { Types } from 'mongoose';
 
-// represents a document from the pathology.levelimages collection
-interface LevelImage {
+// represents a document from the pathology.images collection
+interface Image {
   _id: Types.ObjectId;
   image: Buffer;
   levelId: Types.ObjectId & Level;
   ts: number;
 }
 
-export default LevelImage;
+export default Image;
