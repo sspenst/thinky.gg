a03a34a709cb71167cea3cf0472ccdf424326642
Danny Miller <k2xl.com@gmail.com>
2022-07-25 19:11:08 -0400

    still tryin

--- components/formattedLevelReviews.tsx
+++ components/formattedLevelReviews.tsx
@@ -82,7 +82,7 @@ export default function FormattedLevelReviews({ levelId }: FormattedLevelReviews
           {levelContext.level && user ?
             <>
               <div>
-                <ReviewForm userReview={userReview} plevel={levelContext.level} />
+                <ReviewForm onUpdate={()=>levelContext?.getReviews()} userReview={userReview} plevel={levelContext.level} />
               </div>
               <br/>
             </>
