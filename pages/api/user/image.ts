4662f7eecd78248fe44f422c0766061984863eb1
Spencer Spenst <spencerspenst@gmail.com>
2022-07-25 14:40:12 -0700

    validate file type on server

--- pages/api/user/image.ts
+++ pages/api/user/image.ts
@@ -1,3 +1,4 @@
+import { MAGIC_MIME_TYPE, Magic } from 'mmmagic';
 import withAuth, { NextApiRequestWithAuth } from '../../../lib/withAuth';
 import { ImageModel } from '../../../models/mongoose';
 import { NextApiResponse } from 'next';
@@ -24,20 +25,37 @@ export default withAuth(async (req: NextApiRequestWithAuth, res: NextApiResponse
     await dbConnect();
 
     const imageBuffer = Buffer.from(image, 'binary');
-    const imageModel = await ImageModel.findOne({ documentId: req.userId });
 
-    if (!imageModel) {
-      await ImageModel.create({
-        _id: new ObjectId(),
-        documentId: req.userId,
-        image: imageBuffer,
-        ts: getTs(),
-      });
-    } else {
-      await ImageModel.updateOne({ documentId: req.userId }, { $set: { image: imageBuffer } });
-    }
+    const magic = new Magic(MAGIC_MIME_TYPE);
 
-    return res.status(200).send({ updated: true });
+    magic.detect(imageBuffer, async function(err, result) {
+      if (err) {
+        return res.status(500).json({
+          error: 'Error inspecting file',
+        });
+      }
+
+      if (!['image/png', 'image/jpeg', 'image/gif', 'image/bmp'].includes(result as string)) {
+        return res.status(400).json({
+          error: 'Invalid file type',
+        });
+      }
+
+      const imageModel = await ImageModel.findOne({ documentId: req.userId });
+
+      if (!imageModel) {
+        await ImageModel.create({
+          _id: new ObjectId(),
+          documentId: req.userId,
+          image: imageBuffer,
+          ts: getTs(),
+        });
+      } else {
+        await ImageModel.updateOne({ documentId: req.userId }, { $set: { image: imageBuffer } });
+      }
+
+      return res.status(200).send({ updated: true });
+    });
   } else {
     return res.status(405).json({
       error: 'Method not allowed',
