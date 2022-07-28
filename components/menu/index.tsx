3f13001387d7353e099f5170ebaf69622bb12eb1
Spencer Spenst <spencerspenst@gmail.com>
2022-07-28 16:53:21 -0700

    simplify menu logo

--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -2,7 +2,6 @@ import React, { useContext, useEffect, useState } from 'react';
 import Dimensions from '../../constants/dimensions';
 import Directory from './directory';
 import Dropdown from './dropdown';
-import Image from 'next/image';
 import Link from 'next/link';
 import LinkInfo from '../../models/linkInfo';
 import { PageContext } from '../../contexts/pageContext';
@@ -86,17 +85,14 @@ export default function Menu({
             className={'font-bold text-3xl'}
             style={{
               lineHeight: Dimensions.MenuHeight + 'px',
-              width: 20,
             }}
           >
-            <Image alt='Pathology' height={32} width={32} src='/logo.svg'/>
-            {/* <svg
-              version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink' x='0px' y='0px' className='h-5 w-5' viewBox='0 0 32 32' xmlSpace='preserve'>
-              <rect x='1' y='1' style={{ 'fill': 'var(--level-player)' }} width='14' height='14'/>
-              <rect x='17' y='1' style={{ 'fill': 'var(--level-grid)' }} width='14' height='14'/>
-              <rect x='17' y='17' style={{ 'fill': 'var(--level-grid)' }} width='14' height='14'/>
-              <rect x='1' y='17' style={{ 'fill': 'var(--level-grid)' }} width='14' height='14'/>
-            </svg> */}
+            <svg xmlns='http://www.w3.org/2000/svg' version='1.1' className='h-6 w-6' viewBox='0 0 32 32'>
+              <rect x='1' y='1' fill='var(--level-player)' width='14' height='14'/>
+              <rect x='17' y='1' fill='var(--level-grid)' width='14' height='14'/>
+              <rect x='17' y='17' fill='rgb(14, 168, 117)' width='14' height='14'/>
+              <rect x='1' y='17' fill='rgb(14, 168, 117)' width='14' height='14'/>
+            </svg>
           </a>
         </Link>
       </div>
