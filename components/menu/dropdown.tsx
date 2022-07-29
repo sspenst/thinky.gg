ee6258c1cfccbd0aa022a3601d3a7bac616f10e1
Danny Miller <k2xl.com@gmail.com>
2022-07-28 20:00:54 -0400

    dropdown tweaks

--- components/menu/dropdown.tsx
+++ components/menu/dropdown.tsx
@@ -18,18 +18,26 @@ import useStats from '../../hooks/useStats';
 import useUser from '../../hooks/useUser';
 
 interface SettingProps {
+  onClick: () => void;
+  icon?: JSX.Element;
   children: JSX.Element;
 }
 
-function Setting({ children }: SettingProps) {
+function Setting({ icon, children, onClick }: SettingProps) {
   return (
-    <div
+    <div onClick={onClick} className="hover:bg-gray-700 flex flex-cols-2 items-center"
       style={{
         padding: Dimensions.MenuPadding,
         textAlign: 'center',
+        cursor: 'pointer',
       }}
     >
-      {children}
+      <div className='mr-3'>
+        {icon}
+      </div>
+      <div className='text-center p-1'>
+        {children}
+      </div>
     </div>
   );
 }
@@ -157,48 +165,42 @@ export default function Dropdown() {
                 borderStyle: 'solid',
                 borderWidth: '0 1px 1px 1px',
                 color: 'var(--color)',
-                minWidth: 160,
+
                 position: 'absolute',
                 right: Dimensions.MenuPadding,
-                top: Dimensions.MenuHeight - 1,
+                top: Dimensions.MenuHeight,
               }}
             >
               {levelContext?.level && !hasSidebar ?
                 <>
                   {levelContext.level.authorNote ?
-                    <Setting>
-                      <button onClick={() => setOpenModal(Modal.AuthorNote)}>
+                    <Setting onClick={() => setOpenModal(Modal.AuthorNote)}>
+                      <button>
                         Author Note
                       </button>
                     </Setting>
                     : null}
-                  <Setting>
-                    <button onClick={() => setOpenModal(Modal.LevelInfo)}>
+                  <Setting onClick={() => setOpenModal(Modal.LevelInfo)}>
+                    <button >
                       Level Info
                     </button>
                   </Setting>
-                  <Setting>
-                    <button onClick={() => setOpenModal(Modal.Reviews)}>
+                  <Setting onClick={() => setOpenModal(Modal.Reviews)}>
+                    <button >
                       Reviews
                     </button>
                   </Setting>
                 </>
                 : null}
-              <Setting>
-                <button onClick={() => setOpenModal(Modal.Theme)}>
-                  Theme
-                </button>
-              </Setting>
-              <Setting>
-                <button onClick={() => setOpenModal(Modal.Help)}>
-                  Help
-                </button>
-              </Setting>
-              <Setting>
-                <button onClick={() => setOpenModal(Modal.About)}>
+
+              <Setting onClick={() => setOpenModal(Modal.About)} icon={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-info-lg" viewBox="0 0 16 16">
+                <path d="m9.708 6.075-3.024.379-.108.502.595.108c.387.093.464.232.38.619l-.975 4.577c-.255 1.183.14 1.74 1.067 1.74.72 0 1.554-.332 1.933-.789l.116-.549c-.263.232-.65.325-.905.325-.363 0-.494-.255-.402-.704l1.323-6.208Zm.091-2.755a1.32 1.32 0 1 1-2.64 0 1.32 1.32 0 0 1 2.64 0Z"/>
+              </svg>}>
+                <button>
                   About
                 </button>
               </Setting>
+<<<<<<< Updated upstream
               <Setting>
                 <button onClick={() => {
                   window.open('https://k2xl.com/privacy_policy', '_blank');
@@ -211,22 +213,53 @@ export default function Dropdown() {
                   window.open('https://docs.google.com/forms/d/e/1FAIpQLSdUWWfDYTzHGJs2YGkFIRogR1cu_EXOPi-TCezpoQ-Iid9FBg/viewform?usp=sf_link', '_blank');
                 }}>
                   Contact
+=======
+              <Setting onClick={() => {
+                console.log('a');
+              }
+              } icon={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+              </svg>}>
+                <button>
+                  Add to...
+                </button>
+              </Setting>
+              <Setting onClick={() => setOpenModal(Modal.Theme)} icon={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eyeglasses" viewBox="0 0 16 16">
+                <path d="M4 6a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm2.625.547a3 3 0 0 0-5.584.953H.5a.5.5 0 0 0 0 1h.541A3 3 0 0 0 7 8a1 1 0 0 1 2 0 3 3 0 0 0 5.959.5h.541a.5.5 0 0 0 0-1h-.541a3 3 0 0 0-5.584-.953A1.993 1.993 0 0 0 8 6c-.532 0-1.016.208-1.375.547zM14 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
+              </svg>}>
+                <button >
+                  Theme
+>>>>>>> Stashed changes
                 </button>
               </Setting>
               {!isLoading && user ?
                 <>
-                  <Setting>
-                    <Link href={`/profile/${user._id}`}>
+                  <Setting onClick={()=>{
+                    window.location.href = `/profile/${user._id}`;
+                  }} icon={
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
+                      <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                    </svg>
+                  }>
+                    <button>
                       Profile
-                    </Link>
+                    </button>
                   </Setting>
-                  <Setting>
-                    <Link href='/settings'>
+                  <Setting onClick={()=>{
+                    window.location.href = '/settings';
+                  }} icon={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-gear" viewBox="0 0 16 16">
+                    <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
+                    <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
+                  </svg>}>
+                    <button>
                       Settings
-                    </Link>
+                    </button>
                   </Setting>
-                  <Setting>
-                    <button onClick={logOut}>
+                  <Setting onClick={logOut} icon={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"/>
+                    <path fillRule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
+                  </svg>}>
+                    <button>
                       Log Out
                     </button>
                   </Setting>
