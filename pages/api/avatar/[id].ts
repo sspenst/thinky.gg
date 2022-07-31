65fd407f243f5200c233b86e2680ad7b6e3de0da
Danny Miller <k2xl.com@gmail.com>
2022-07-31 00:17:14 -0400

    turn lean to false for image

--- pages/api/avatar/[id].ts
+++ pages/api/avatar/[id].ts
@@ -47,7 +47,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
   }
 
-  const image = await ImageModel.findOne({ documentId: userId }, {}, { lean: true });
+  const image = await ImageModel.findOne({ documentId: userId }, {}, { lean: false });
 
   if (image) {
     res.setHeader('Content-Type', 'image/png');
