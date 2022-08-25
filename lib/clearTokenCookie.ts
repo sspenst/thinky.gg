75fec1393c3bebed4d3e9385408d765e1eeb4e5c
Spencer Spenst <spencerspenst@gmail.com>
2022-08-24 17:25:56 -0700

    remove cookieLegacy (#448)

--- lib/clearTokenCookie.ts
+++ lib/clearTokenCookie.ts
@@ -1,6 +1,6 @@
 import { serialize } from 'cookie';
 import cookieOptions from './cookieOptions';
 
-export default function clearTokenCookie(host: string | undefined, path = '/') {
-  return serialize('token', '', cookieOptions(host, true, path));
+export default function clearTokenCookie(host: string | undefined) {
+  return serialize('token', '', cookieOptions(host, true));
 }
