d51374d4dd37b48b3144929b1f18916b2e382123
Danny Miller <k2xl.com@gmail.com>
2022-08-31 23:18:59 -0400

    more formatting tweaks, but we should be good already

--- helpers/apiWrapper.ts
+++ helpers/apiWrapper.ts
@@ -6,7 +6,7 @@ import { logger } from './logger';
 export default function apiWrapper(handler: (req: NextApiRequest, res: NextApiResponse) => Promise<unknown>) {
   return async (req: NextApiRequest, res: NextApiResponse): Promise<unknown> => {
     return handler(req, res).catch((error: Error) => {
-      logger.error(error.message, error);
+      logger.error('API Handler Error Caught', error);
 
       return res.status(500).send(error.message || error);
     });
@@ -16,7 +16,7 @@ export default function apiWrapper(handler: (req: NextApiRequest, res: NextApiRe
 export function apiWrapperAuth(handler: (req: NextApiRequestWithAuth, res: NextApiResponse) => Promise<unknown>) {
   return async (req: NextApiRequestWithAuth, res: NextApiResponse): Promise<unknown> => {
     return handler(req, res).catch((error: Error) => {
-      logger.error(error.message, error);
+      logger.error('API Auth Handler Error Caught', error);
 
       return res.status(500).send(error.message || error);
     });
