9a2a773e214bdddb83061486d879b894ac0bafec
Danny Miller <k2xl.com@gmail.com>
2022-09-01 14:14:24 -0400

    validators

--- helpers/apiWrapper.ts
+++ helpers/apiWrapper.ts
@@ -1,9 +1,57 @@
+import { ObjectId } from 'bson';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextApiRequestWithAuth } from '../lib/withAuth';
 import { logger } from './logger';
 
 export interface ReqValidator {
   methods?: string[];
+  expected?: ReqExpected;
+}
+export interface ReqExpected {
+  body?: { [key: string]: (value: any) => boolean };
+  query?: { [key: string]: (value: any) => boolean };
+}
+
+export function ValidType(type: string, mustExist?: boolean) {
+  return (value?: unknown) => {
+    if (!mustExist && !value) {
+      return true;
+    }
+
+    return typeof value === type;
+  };
+}
+
+export function ValidNumber(mustExist?: boolean, min?: number, max?: number) {
+  return (value?: unknown) => {
+    if (!mustExist && !value) {
+      return true;
+    }
+
+    if (typeof value !== 'number') {
+      return false;
+    }
+
+    if (min !== undefined && value < min) {
+      return false;
+    }
+
+    if (max !== undefined && value > max) {
+      return false;
+    }
+
+    return true;
+  };
+}
+
+export function ValidObjectId(mustExist?: boolean) {
+  return (value?: unknown) => {
+    if (!mustExist && !value) {
+      return true;
+    }
+
+    return ObjectId.isValid(value as string);
+  };
 }
 
 export function parseReq(validator: ReqValidator, req: NextApiRequest | NextApiRequestWithAuth): {statusCode: number, error: string} | null {
@@ -12,6 +60,33 @@ export function parseReq(validator: ReqValidator, req: NextApiRequest | NextApiR
       statusCode: 405,
       error: 'Method not allowed',
     };
+  }
+
+  if (validator.expected !== undefined) {
+    const badKeys = [];
+
+    if (validator.expected.body !== undefined) {
+      for (const [key, validatorFn] of Object.entries(validator.expected.body)) {
+        if (!validatorFn(req.body[key])) {
+          badKeys.push('body.' + key);
+        }
+      }
+    }
+
+    if (validator.expected.query !== undefined) {
+      for (const [key, validatorFn] of Object.entries(validator.expected.query)) {
+        if (!validatorFn(req.query[key])) {
+          badKeys.push('query.' + key);
+        }
+      }
+    }
+
+    if (badKeys.length > 0) {
+      return {
+        statusCode: 400,
+        error: 'Invalid ' + badKeys.sort().join(', ')
+      };
+    }
   }
 
   return null;
