cffb9d468d7260dd90119f423e209c649521b6ee
Danny Miller <k2xl.com@gmail.com>
2022-09-01 19:44:43 -0400

    controls...

--- components/level/game.tsx
+++ components/level/game.tsx
@@ -572,7 +572,7 @@ export default function Game({
       const dx: number = clientX - touchXDown.current;
       const dy: number = clientY - touchYDown.current;
 
-      if (Math.abs(dx) <= 0.5 && Math.abs(dy) <= 0.5 && timeSince < 200) {
+      if (Math.abs(dx) <= 0.5 && Math.abs(dy) <= 0.5) {
         // disable tap
         // get player
         const player = document.getElementById('player');
