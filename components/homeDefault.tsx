79de3a792cfc4836d6682bc9ad0f721cfba58982
Danny Miller <k2xl.com@gmail.com>
2022-09-09 22:50:37 -0400

    home page formatting

--- components/homeDefault.tsx
+++ components/homeDefault.tsx
@@ -33,7 +33,7 @@ export default function HomeDefault() {
         <div className='flex-auto sm:w-32 p-3'>
           <span className='font-bold text-4xl'>An active community</span>
           <div className='p-3'>has helped build <span className='italic'>thousands</span> of levels over multiple decades. A level and collection editor allows you to build your own challenging levels for the world of Pathology players.</div>
-          <div className='p-6 w-full'>
+          <div className='p-3 w-full'>
             <div className='grid grid-cols-2'>
               <figure>
                 <Image width={1200} height={630} alt='The Tower by Raszlo' src='/api/level/image/61fe3c372cdc920ef6f80190.png' />
@@ -45,7 +45,7 @@ export default function HomeDefault() {
               </figure>
             </div>
           </div>
-          <div>Join the <Link passHref href='https://discord.gg/NsN8SBEZGN'>
+          <div className='text-center'>Join the <Link passHref href='https://discord.gg/NsN8SBEZGN'>
             <a className={classNames('px-1 inline-flex justify-center items-center gap-2 rounded-md border font-medium align-middle focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-600 transition-all text-sm', discordClassNames)}>
               <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='bi bi-discord' viewBox='0 0 16 16'>
                 <path d='M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z' />
@@ -54,7 +54,8 @@ export default function HomeDefault() {
           </Link>
           </div>
           <div>
-            <LatestLevelsTable levels={levels} />
+            <h2 className='font-bold text-lg pt-4 text-center'>Latest Levels</h2>
+            <LatestLevelsTable levels={levels || []} />
           </div>
         </div>
       </div>
