fefa72afcfb92e7fd36aa05e3ab98be7a563b4d3
Spencer Spenst <spencerspenst@gmail.com>
2022-10-04 18:11:55 -0700

    lots of formatting

--- server/jobs/email-digest.tsx
+++ server/jobs/email-digest.tsx
@@ -16,10 +16,10 @@ import { getLevelOfDay } from '../../pages/api/level-of-day';
 dotenv.config();
 
 interface GroupedNotificationsByUser {
-    [key: string]: {
-        userId: User;
-        notifications: Notification[];
-    };
+  [key: string]: {
+    userId: User;
+    notifications: Notification[];
+  };
 }
 
 async function getUsersWithUnreadNotificationsPast24(): Promise<GroupedNotificationsByUser> {
@@ -88,92 +88,111 @@ async function start() {
   logger.info('There are ' + Object.keys(users).length + ' users with unread notifications in the past 24 hours');
   //console.log(users);
   const levelOfDay = await getLevelOfDay();
-  let levelOfDayHTML = null;
-
-  if (levelOfDay) {
-    levelOfDayHTML = (
-      <div>
-        <h2>Check out the level of the day</h2>
-        <div style={{
-          justifyContent: 'center',
-          textAlign: 'center',
-          alignItems: 'center',
-          flexDirection: 'column',
-        }}>
-          <a href={`https://pathology.gg/level/${levelOfDay.slug}`}>
-            {levelOfDay.name}
-          </a>
-          {' by '}
-          <a href={`https://pathology.gg/profile/${encodeURI(levelOfDay.userId.name)}`}>
-            {levelOfDay.userId.name}
-          </a>
-          <a href={`https://pathology.gg/level/${levelOfDay.slug}`}>
-            <img src={`https://pathology.gg/api/level/image/${levelOfDay._id}.png`} width='100%' alt={levelOfDay.name} />
-          </a>
-        </div>
-      </div>
-    );
-  }
 
   for (const group of Object.values(users)) {
     const { userId, notifications } = group;
-
-    const notificationListsHTML = '';
-
     const subject = 'You have ' + notifications.length + ' new notifications';
-
     const element = (
-      <html style={{
-        maxWidth: '70ch',
-        /* larger spacing on larger screens, very small spacing on tiny screens */
-        padding: 'calc(1vmin + .5rem)',
-        /* shorthand for margin-left/margin-right */
-        marginInline: 'auto',
-        /* fluid sizing: https://frontaid.io/blog/fluid-typography-2d-css-locks-clamp/ */
-        fontSize: 'clamp(1em, 0.909em + 0.45vmin, 1.25em)',
-        /* use system font stack: https://developer.mozilla.org/en-US/docs/Web/CSS/font-family */
-        fontFamily: 'system-ui',
-      }}>
-        <head>
-          <style>
-            {
-              `a {
-                color: #337ab7;
-                text-decoration: none;
-              }
-              p {
-                margin: 0 0 10px;
-              }
-              button {
-                border-radius: 0.25rem;
-                pointer: cursor;
-              }`
-            }
-          </style>
-        </head>
-        <body className='email'>
-          <h1>Hi {userId.name}</h1>
-          <p>You have <a href='https://pathology.gg/notifications?source=email-digest'>{notifications.length} unread notifications</a></p>
-          <div>
-            {notificationListsHTML ? notificationListsHTML : ''}
-          </div>
-          {levelOfDayHTML}
-          <div style={{
-            justifyContent: 'center',
-            textAlign: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
+      <html>
+        <body>
+          <table role='presentation' cellPadding='0' cellSpacing='0' style={{
+            backgroundColor: '#000',
+            borderCollapse: 'separate',
+            fontFamily: 'sans-serif',
+            width: '100%',
           }}>
-            <p>Thanks for using <a href='https://pathology.gg'>Pathology</a>!</p>
-          </div>
-          <div id='footer' style={{
-            fontSize: '10px',
-            color: '#999',
-            textAlign: 'center',
-          }}>
-            <p>Join the <a href='https://discord.gg/kpfdRBt43v'>Pathology Discord</a> to chat with other players and the developers!</p>
-            <p><a href='https://pathology.gg/settings'>Manage your email notification settings</a></p>
-          </div>
+            <tr>
+              <td align='center' style={{
+                display: 'block',
+                padding: 20,
+                verticalAlign: 'top',
+              }}>
+                <table role='presentation' cellPadding='0' cellSpacing='0' style={{
+                  color: '#FFF',
+                  maxWidth: 580,
+                }}>
+                  <tr>
+                    <td>
+                      <div style={{
+                        backgroundColor: 'rgb(38, 38, 38)',
+                        borderColor: 'rgb(130, 130, 130)',
+                        borderRadius: 20,
+                        borderStyle: 'solid',
+                        borderWidth: 1,
+                        padding: 20,
+                      }}>
+                        <a href='https://pathology.gg'>
+                          {/* eslint-disable-next-line @next/next/no-img-element */}
+                          <img src='https://i.imgur.com/fD1SUrZ.png' alt='Pathology' />
+                        </a>
+                        <h1>Hi {userId.name},</h1>
+                        <p>You have <a href='https://pathology.gg/notifications?source=email-digest' style={{
+                          color: '#337ab7',
+                          textDecoration: 'none',
+                        }}>{notifications.length} unread notifications</a></p>
+                        {levelOfDay &&
+                          <div>
+                            <h2>Check out the level of the day:</h2>
+                            <div style={{
+                              textAlign: 'center',
+                            }}>
+                              <a href={`https://pathology.gg/level/${levelOfDay.slug}`} style={{
+                                color: '#337ab7',
+                                textDecoration: 'none',
+                              }}>
+                                {levelOfDay.name}
+                              </a>
+                              {' by '}
+                              <a href={`https://pathology.gg/profile/${encodeURI(levelOfDay.userId.name)}`} style={{
+                                color: '#337ab7',
+                                textDecoration: 'none',
+                              }}>
+                                {levelOfDay.userId.name}
+                              </a>
+                              <div style={{
+                                padding: 20,
+                              }}>
+                                <a href={`https://pathology.gg/level/${levelOfDay.slug}`} style={{
+                                  color: '#337ab7',
+                                  textDecoration: 'none',
+                                }}>
+                                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                                  <img src={`https://pathology.gg/api/level/image/${levelOfDay._id}.png`} width='100%' alt={levelOfDay.name} />
+                                </a>
+                              </div>
+                            </div>
+                          </div>
+                        }
+                        <div style={{
+                          padding: 10,
+                          textAlign: 'center',
+                        }}>
+                          Thanks for using <a href='https://pathology.gg' style={{
+                            color: '#337ab7',
+                            textDecoration: 'none',
+                          }}>Pathology</a>!
+                        </div>
+                        <div id='footer' style={{
+                          fontSize: '10px',
+                          color: '#999',
+                          textAlign: 'center',
+                        }}>
+                          <p>Join the <a href='https://discord.gg/kpfdRBt43v' style={{
+                            color: '#337ab7',
+                            textDecoration: 'none',
+                          }}>Pathology Discord</a> to chat with other players and the developers!</p>
+                          <p><a href='https://pathology.gg/settings' style={{
+                            color: '#337ab7',
+                            textDecoration: 'none',
+                          }}>Manage your email notification settings</a></p>
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
         </body>
       </html>
     );
