02a83cfbfd2f4caa7f8bb6e6cb3566d09af8657d
k2xl <k2xl.com@gmail.com>
2022-10-09 10:52:32 -0400

    move digest to own api (#616)
    * move digest to own api
    
    * fix test
    
    * 100% coverage for jobs email
    
    * add INTERNAL_JOB_TOKEN_SECRET_EMAILDIGEST=cocomelon to node.js.yml

--- constants/emailDigest.ts
+++ constants/emailDigest.ts
@@ -2,10 +2,8 @@ export enum EmailKVTypes {
   LAST_TS_EMAIL_DIGEST = 'EMAIL_DIGEST.LAST_TS.',
   LAST_TS_EMAIL_MARKETING = 'EMAIL_MARKETING.LAST_TS.',
 }
-enum EmailDigest {
+export enum EmailDigestSettingTypes {
   DAILY = 'DAILY',
   ONLY_NOTIFICATIONS = 'ONLY_NOTIFICATIONS',
   NONE = 'NONE',
 }
-
-export default EmailDigest;
