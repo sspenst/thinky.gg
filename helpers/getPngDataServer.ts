f7df9400bb95d49e7fc0212ef10cef3932dd934b
Spencer Spenst <spencerspenst@gmail.com>
2022-10-30 17:51:12 -0700

    image stream fix

--- helpers/getPngDataServer.ts
+++ helpers/getPngDataServer.ts
@@ -5,14 +5,21 @@ import Dimensions from '../constants/dimensions';
 import Level from '../models/db/level';
 import generateLevelCanvas from './generateLevelCanvas';
 
-export default async function getPngDataServer(level: Level) {
+export default async function getPngDataServer(level: Level): Promise<Buffer> {
   let canvas = PImage.make(Dimensions.LevelCanvasWidth, Dimensions.LevelCanvasHeight, {});
 
   canvas = generateLevelCanvas(canvas, level) as Bitmap;
 
   const stream = new PassThrough();
+  const chunks: Uint8Array[] = [];
+  const encodePromise = PImage.encodePNGToStream(canvas, stream);
 
-  await PImage.encodePNGToStream(canvas, stream);
-
-  return await stream.read();
+  return await new Promise((resolve, reject) => {
+    stream.on('data', chunk => chunks.push(chunk));
+    stream.on('error', err => reject(err));
+    stream.on('end', async () => {
+      await encodePromise;
+      resolve(Buffer.concat(chunks));
+    });
+  });
 }
