61069a3e3d5de7d5ac188834e13d9c7dac9f1d97
Danny Miller <k2xl.com@gmail.com>
2022-11-05 13:20:15 -0400

    is this the issue

--- tests/pages/api/level-of-the-day/level-of-the-day.test.ts
+++ tests/pages/api/level-of-the-day/level-of-the-day.test.ts
@@ -37,7 +37,9 @@ describe('GET /api/level-of-day', () => {
   test('should return 200', async () => {
     // Artifically increase calc_playattempts_duration_sum to make it more likely to be selected
     MockDate.set(MOCK_DATE);
-    console.log('first test', Date.now(), 'should be ', MOCK_DATE.getTime());
+    console.log('first test', Date.now(), 'should be ', MOCK_DATE.getDay());
+    expect(MOCK_DATE.getDay()).toBe(0); // sunday
+    expect(new Date().getDay()).toBe(0); // sunday
     const updated = await LevelModel.updateOne({
       _id: TestId.LEVEL_3,
     }, {
