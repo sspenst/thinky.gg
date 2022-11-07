0243dfa46a7df7ae848f848f4382d273187cff5e
Spencer Spenst <spencerspenst@gmail.com>
2022-11-06 20:41:16 -0800

    no focus on modal x button

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -76,16 +76,16 @@ export default function Modal({
                 color: 'var(--color)',
               }}
             >
-              <div className="absolute top-1 right-1">
+              <div className='absolute top-1 right-1'>
                 <button
-                  type="button"
-                  className="inline-flex justify-center px-2 py-1 text-sm font-medium text-gray-200  border border-transparent rounded hover:bg-gray-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                  className='inline-flex justify-center px-2 py-1 text-sm font-medium  border border-transparent rounded'
                   onClick={closeModal}
+                  tabIndex={-1}
+                  type='button'
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16">
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+                  <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='bi bi-x-circle-fill' viewBox='0 0 16 16'>
+                    <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z' />
                   </svg>
-
                 </button>
               </div>
               <Dialog.Title
