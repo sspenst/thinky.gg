a906b98cef0887038c8ad4043753b47991ae8e4f
Spencer Spenst <spencerspenst@gmail.com>
2022-11-07 19:50:58 -0800

    reverse

--- pages/profile/[name]/[[...tab]]/index.tsx
+++ pages/profile/[name]/[[...tab]]/index.tsx
@@ -406,7 +406,7 @@ export default function ProfilePage({
                       {getFormattedDifficulty(Number(rank))}
                     </div>
                   );
-                })}
+                }).reverse()}
               </div>
             }
           </div>
