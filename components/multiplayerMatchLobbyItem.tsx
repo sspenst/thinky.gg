16fa91ea28bb68c2ca5cd9f4cf4abc24c8ec843a
Danny Miller <k2xl.com@gmail.com>
2022-11-17 18:55:15 -0500

    hooks make it clean

--- components/multiplayerMatchLobbyItem.tsx
+++ components/multiplayerMatchLobbyItem.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { toast } from 'react-hot-toast';
 import { PageContext } from '../contexts/pageContext';
 import MultiplayerMatch from '../models/db/multiplayerMatch';
 import { MultiplayerMatchState } from '../models/MultiplayerEnums';
@@ -7,21 +8,71 @@ import FormattedUser from './formattedUser';
 export default function MultiplayerMatchLobbyItem({ match, onJoinClick, onLeaveClick }: {match: MultiplayerMatch, onJoinClick: (matchId: string) => void, onLeaveClick: (matchId: string) => void}) {
   const { user } = useContext(PageContext);
 
+  const btnLeaveMatch = async (matchId: string) => {
+    toast.dismiss();
+    toast.loading('Leaving Match...');
+    const res = await fetch(`/api/match/${matchId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        action: 'quit',
+      }),
+    });
+    const data = await res.json();
+
+    if (!res.ok) {
+      toast.dismiss();
+      toast.error(data.error || 'Failed to leave match');
+    } else {
+      toast.dismiss();
+      toast.success('Left Match');
+    }
+
+    onLeaveClick(matchId);
+  };
+  const btnJoinMatch = async (matchId: string) => {
+    toast.dismiss();
+    toast.loading('Joining Match...');
+    const res = await fetch(`/api/match/${matchId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        action: 'join',
+      }),
+    });
+    const data = await res.json();
+
+    if (!res.ok) {
+      toast.dismiss();
+      toast.error(data.error || 'Failed to join match');
+    } else {
+      toast.dismiss();
+      toast.success('Joined Match');
+    }
+
+    onJoinClick(matchId);
+  };
+
   return (
     <div key={match._id.toString()} className='p-3 bg-gray-700 rounded flex flex-row gap-20'>
+      <h2>{match.state}</h2>
       {match.players.map((player) => (
         <div key={player._id.toString()}>
           <FormattedUser user={player} />
           {user?._id.toString() !== player._id.toString() && match.state === MultiplayerMatchState.OPEN && (
             <button
-              onClick={() => onJoinClick(match.matchId)}
+              onClick={() => btnJoinMatch(match.matchId)}
               className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
         Join
             </button>
           ) }
           {user?._id.toString() === player._id.toString() && (
             <button
-              onClick={() => onLeaveClick(match.matchId)}
+              onClick={() => btnLeaveMatch(match.matchId)}
               className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
           Leave
             </button>
--- pages/match/[matchId].tsx
+++ pages/match/[matchId].tsx
@@ -1,10 +1,70 @@
+import { GetServerSidePropsContext, NextApiRequest } from 'next';
+import { useRouter } from 'next/router';
 import React from 'react';
+import { toast } from 'react-hot-toast';
+import MultiplayerMatchLobbyItem from '../../components/multiplayerMatchLobbyItem';
 import Page from '../../components/page';
+import SkeletonPage from '../../components/skeletonPage';
+import useMatch from '../../hooks/useMatch';
+import { getUserFromToken } from '../../lib/withAuth';
+import MultiplayerMatch from '../../models/db/multiplayerMatch';
+import { ReqUser } from '../../models/db/user';
+import { MultiplayerMatchModel } from '../../models/mongoose';
+import { MultiplayerMatchState } from '../../models/MultiplayerEnums';
 
-export default function MatchGame() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const token = context.req?.cookies?.token;
+  const reqUser = token ? await getUserFromToken(token, context.req as NextApiRequest) : null;
+
+  if (!reqUser) {
+    // redirect to login page
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+  const { matchId } = context.query;
+
+  return {
+    props: {
+      matchId: matchId,
+    },
+  };
+}
+
+export default function MatchGame({ matchId }: {user: ReqUser, matchId: string}) {
+  const { match } = useMatch(matchId);
+  // use router
+  const router = useRouter();
+
+  if (!match) {
+    return <div>Cannot find match</div>;
+  }
+
+  if (match.state === MultiplayerMatchState.ABORTED) {
+    toast.error('Match has been aborted');
+    // redirect user
+    router.push('/match');
+  }
+
   return (
     <Page title='Multiplayer Match'>
-      <h1>Match Game</h1>
+      <div className='flex flex-col items-center justify-center p-3'>
+        <h1 className='text-4xl font-bold'>Match</h1>
+
+        <h1>Match Game</h1>
+        <MultiplayerMatchLobbyItem match={match}
+          onJoinClick={() => {
+            console.log('joining');
+          }}
+          onLeaveClick={() => {
+            console.log('leaving');
+          }}
+        />
+      </div>
     </Page>
   );
 }
