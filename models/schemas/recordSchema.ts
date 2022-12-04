1f6d2795d9bfa7834377dceed8564b8395ff200b
Spencer Spenst <spencerspenst@gmail.com>
2022-12-04 11:46:14 -0800

    RecordSchema unique moves index

--- models/schemas/recordSchema.ts
+++ models/schemas/recordSchema.ts
@@ -27,5 +27,6 @@ const RecordSchema = new mongoose.Schema<Record>({
 });
 
 RecordSchema.index({ levelId: 1 });
+RecordSchema.index({ levelId: 1, moves: 1 }, { unique: true });
 
 export default RecordSchema;
