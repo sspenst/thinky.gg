815fc735827b90a871905884afc2e94ebc44d460
Danny Miller <k2xl.com@gmail.com>
2022-12-04 20:39:23 -0500

    socket states

--- pages/api/health/index.ts
+++ pages/api/health/index.ts
@@ -1,14 +1,33 @@
 /* istanbul ignore file */
 
 import { NextApiRequest, NextApiResponse } from 'next';
+import { Socket } from 'socket.io';
 import apiWrapper from '../../../helpers/apiWrapper';
 
 export default apiWrapper({ GET: {} }, async (req: NextApiRequest, res: NextApiResponse) => {
   const { statusCode } = req.query;
 
+  if (process.env.HEALTH_CHECK_SECRET && process.env.HEALTH_CHECK_SECRET !== req.query['secret']) {
+    return res.status(401).json({
+      error: 'Invalid secret',
+    });
+  }
+
   if (statusCode as string === '500') {
     throw new Error('500 error');
   }
 
-  return res.status(parseInt(statusCode as string) || 200).json({ status: 'OK' });
+  const sockets: { [url: string]: boolean } = {};
+
+  for (const url in global.appSocketToWebSocketServer) {
+    sockets[url] = global.appSocketToWebSocketServer[url].connected;
+  }
+
+  return res.status(parseInt(statusCode as string) || 200).json(
+    {
+      status: 'OK',
+      socketStates: sockets,
+    },
+
+  );
 });
