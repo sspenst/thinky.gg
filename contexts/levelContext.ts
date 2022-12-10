accbe4fd5f170009a2f1779de0e34aafdf5d30fb
Danny Miller <k2xl.com@gmail.com>
2022-12-10 15:18:15 -0500

    don't grab collections unless you open the dialog to add to a collection

--- contexts/levelContext.ts
+++ contexts/levelContext.ts
@@ -1,12 +1,10 @@
 import { createContext } from 'react';
-import Collection from '../models/db/collection';
 import { EnrichedLevel } from '../models/db/level';
 import Record from '../models/db/record';
 import Review from '../models/db/review';
 import Stat from '../models/db/stat';
 
 interface LevelContextInterface {
-  collections: Collection[] | undefined;
   completions: Stat[] | undefined;
   getCompletions: (all: boolean) => void;
   getReviews: () => void;
