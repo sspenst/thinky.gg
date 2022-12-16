45efe7d64dd0cf1eda31f2680e53eb8a66b18050
Spencer Spenst <spencerspenst@gmail.com>
2022-12-15 21:25:38 -0800

    floor formatted date

--- helpers/getFormattedDate.ts
+++ helpers/getFormattedDate.ts
@@ -3,5 +3,7 @@ import moment from 'moment';
 export default function getFormattedDate(ts: number) {
   const date = new Date(ts * 1000);
 
+  moment.relativeTimeRounding(Math.floor);
+
   return moment(date).fromNow();
 }
