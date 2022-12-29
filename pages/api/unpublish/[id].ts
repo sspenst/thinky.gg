sspenst/thinky.gg

commit 3eb65765f6cfb970e1f240d6ed3a9e5b938f1447

kind of hard to repro, but i believe this fixes #780 (#790)
* kind of hard to repro, but i believe this fixes #780

* level management cleanup

Co-authored-by: Spencer Spenst <spencerspenst@gmail.com>

pages/api/unpublish/[id].ts
@@ -82,7 +82,6 @@ export default withAuth({ POST: {
         UserModel.updateMany({ _id: { $in: userIds } }, { $inc: { score: -1 } }, { session: session }),
         CollectionModel.updateMany({ levels: id }, { $pull: { levels: id } }, { session: session }),
         clearNotifications(undefined, undefined, level._id, undefined, { session: session }),
-
         MultiplayerMatchModel.updateMany({
           state: MultiplayerMatchState.ACTIVE,
           levels: id,
@@ -98,13 +97,14 @@ export default withAuth({ POST: {
         }, {
           session: session,
         }),
-
+        LevelModel.insertMany([levelClone], { session: session }),
       ]);
-      await LevelModel.insertMany([levelClone], { session: session });
+
       await Promise.all([
         queueRefreshIndexCalcs(levelClone._id, { session: session }),
-        queueCalcPlayAttempts(levelClone._id, { session: session })
+        queueCalcPlayAttempts(levelClone._id, { session: session }),
       ]);
+
       newId = levelClone._id;
     });
 

pages/api/level/[id].ts
@@ -113,10 +113,12 @@ export default withAuth({
       queueRefreshIndexCalcs(new ObjectId(id as string))
     ]);
 
-    // revalidate the old endpoint
+    // revalidate the old and new endpoints
     if (level) {
-      // without this if statement we error when adding a level to our favorites that we don't own
-      await revalidateLevel(res, level.slug);
+      await Promise.all([
+        revalidateLevel(res, level.slug),
+        revalidateLevel(res, slug),
+      ]);
     }
 
     return res.status(200).json({ updated: true });

pages/api/publish/[id].ts
@@ -104,9 +104,9 @@ export default withAuth({ POST: {
         throw new Error('Level not found [RC]');
       }
 
-      await queueRefreshIndexCalcs(level._id, { session: session });
-      await queueCalcPlayAttempts(level._id, { session: session });
       await Promise.all([
+        queueRefreshIndexCalcs(level._id, { session: session }),
+        queueCalcPlayAttempts(level._id, { session: session }),
         createNewLevelNotifications(new ObjectId(req.userId), level._id, undefined, { session: session }),
         queueDiscordWebhook(Discord.LevelsId, `**${user?.name}** published a new level: [${level.name}](${req.headers.origin}/level/${level.slug}?ts=${ts})`, { session: session }),
       ]);

components/levelTable.tsx
@@ -155,12 +155,10 @@ export default function LevelTable({ getLevels, levels }: LevelTableProps) {
           onPublish={() => getLevels()}
         />
         <UnpublishLevelModal
-          closeModal={() => {
-            setIsUnpublishLevelOpen(false);
-            getLevels();
-          }}
+          closeModal={() => setIsUnpublishLevelOpen(false)}
           isOpen={isUnpublishLevelOpen}
           level={levelToModify}
+          onUnpublish={() => getLevels()}
         />
         <DeleteLevelModal
           closeModal={() => {

components/modal/addLevelModal.tsx
@@ -20,10 +20,15 @@ export default function AddLevelModal({ closeModal, isOpen, level }: AddLevelMod
   const [authorNote, setAuthorNote] = useState<string>();
   const [collections, setCollections] = useState<Collection[]>();
   const [collectionIds, setCollectionIds] = useState<string[]>([]);
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const [name, setName] = useState<string>();
   const router = useRouter();
   const { setIsLoading, shouldAttemptAuth } = useContext(AppContext);
   const { user } = useContext(PageContext);
+
+  useEffect(() => {
+    setIsLoading(isSubmitting);
+  }, [isSubmitting, setIsLoading]);
 
   const getCollections = useCallback(() => {
     if (isOpen && shouldAttemptAuth) {
@@ -94,7 +99,7 @@ export default function AddLevelModal({ closeModal, isOpen, level }: AddLevelMod
       return;
     }
 
-    setIsLoading(true);
+    setIsSubmitting(true);
     toast.loading(level ? 'Updating level...' : 'Adding level...');
 
     fetch(level ? `/api/level/${level._id}` : '/api/level', {
@@ -127,7 +132,7 @@ export default function AddLevelModal({ closeModal, isOpen, level }: AddLevelMod
       toast.dismiss();
       toast.error(JSON.parse(await err)?.error);
     }).finally(() => {
-      setIsLoading(false);
+      setIsSubmitting(false);
     });
   }
 
@@ -153,6 +158,7 @@ export default function AddLevelModal({ closeModal, isOpen, level }: AddLevelMod
   return (
     <Modal
       closeModal={closeModal}
+      disabled={isSubmitting}
       isOpen={isOpen}
       onSubmit={onSubmit}
       title={!isUsersLevel ? 'Add to...' : `${level ? 'Edit' : 'New'} Level`}

components/modal/index.tsx
@@ -4,14 +4,16 @@ import React, { Fragment } from 'react';
 import { rubik } from '../../pages/_app';
 
 interface ModalButtonProps {
+  disabled?: boolean;
   onClick: (event?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   text: string;
 }
 
-function ModalButton({ onClick, text }: ModalButtonProps) {
+function ModalButton({ disabled, onClick, text }: ModalButtonProps) {
   return (
     <button
       className={classNames('inline-flex justify-center px-4 py-2 text-sm font-medium border border-transparent rounded-md bg-button')}
+      disabled={disabled}
       onClick={onClick}
       style={{
         marginRight: 10,
@@ -26,6 +28,7 @@ function ModalButton({ onClick, text }: ModalButtonProps) {
 interface ModalProps {
   children: JSX.Element;
   closeModal: () => void;
+  disabled?: boolean;
   isOpen: boolean;
   onConfirm?: () => void;
   onSubmit?: () => void;
@@ -35,6 +38,7 @@ interface ModalProps {
 export default function Modal({
   children,
   closeModal,
+  disabled,
   isOpen,
   onConfirm,
   onSubmit,
@@ -109,16 +113,16 @@ export default function Modal({
               >
                 {onConfirm ?
                   <>
-                    <ModalButton onClick={onConfirm} text={'OK'} />
-                    <ModalButton onClick={closeModal} text={'Cancel'} />
+                    <ModalButton disabled={disabled} onClick={onConfirm} text={'OK'} />
+                    <ModalButton disabled={disabled} onClick={closeModal} text={'Cancel'} />
                   </>
                   : onSubmit ?
                     <>
-                      <ModalButton onClick={onSubmit} text={'Submit'} />
-                      <ModalButton onClick={closeModal} text={'Cancel'} />
+                      <ModalButton disabled={disabled} onClick={onSubmit} text={'Submit'} />
+                      <ModalButton disabled={disabled} onClick={closeModal} text={'Cancel'} />
                     </>
                     :
-                    <ModalButton onClick={closeModal} text={'Close'} />
+                    <ModalButton disabled={disabled} onClick={closeModal} text={'Close'} />
                 }
               </div>
             </Dialog.Panel>

components/modal/publishLevelModal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { AppContext } from '../../contexts/appContext';
 import { PageContext } from '../../contexts/pageContext';
@@ -13,19 +13,18 @@ interface PublishLevelModalProps {
   onPublish: () => void;
 }
 
-export default function PublishLevelModal({
-  closeModal,
-  isOpen,
-  level,
-  onPublish,
-}: PublishLevelModalProps) {
+export default function PublishLevelModal({ closeModal, isOpen, level, onPublish }: PublishLevelModalProps) {
+  const [isPublishing, setIsPublishing] = useState(false);
   const { mutateUser } = useContext(PageContext);
   const { setIsLoading } = useContext(AppContext);
 
+  useEffect(() => {
+    setIsLoading(isPublishing);
+  }, [isPublishing, setIsLoading]);
+
   function onConfirm() {
-    closeModal();
+    setIsPublishing(true);
     toast.loading('Publishing level...');
-    setIsLoading(true);
 
     fetch(`/api/publish/${level._id}`, {
       method: 'POST',
@@ -35,6 +34,7 @@ export default function PublishLevelModal({
       },
     }).then(async res => {
       if (res.status === 200) {
+        closeModal();
         onPublish();
         mutateUser();
 
@@ -51,13 +51,14 @@ export default function PublishLevelModal({
       toast.dismiss();
       toast.error('Error publishing level');
     }).finally(() => {
-      setIsLoading(false);
+      setIsPublishing(false);
     });
   }
 
   return (
     <Modal
       closeModal={closeModal}
+      disabled={isPublishing}
       isOpen={isOpen}
       onConfirm={onConfirm}
       title={'Publish Level'}

components/modal/unpublishLevelModal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { AppContext } from '../../contexts/appContext';
 import { PageContext } from '../../contexts/pageContext';
@@ -9,22 +9,34 @@ interface UnpublishLevelModalProps {
   closeModal: () => void;
   isOpen: boolean;
   level: Level;
+  onUnpublish: () => void;
+
 }
 
-export default function UnpublishLevelModal({ closeModal, isOpen, level }: UnpublishLevelModalProps) {
+export default function UnpublishLevelModal({ closeModal, isOpen, level, onUnpublish }: UnpublishLevelModalProps) {
+  const [isUnpublishing, setIsUnpublishing] = useState(false);
   const { mutateUser } = useContext(PageContext);
   const { setIsLoading } = useContext(AppContext);
 
+  useEffect(() => {
+    setIsLoading(isUnpublishing);
+  }, [isUnpublishing, setIsLoading]);
+
   function onConfirm() {
-    closeModal();
-    setIsLoading(true);
+    setIsUnpublishing(true);
     toast.loading('Unpublishing...');
+
     fetch(`/api/unpublish/${level._id}`, {
       method: 'POST',
       credentials: 'include',
     }).then(res => {
       if (res.status === 200) {
+        closeModal();
+        onUnpublish();
         mutateUser();
+
+        toast.dismiss();
+        toast.success('Unpublished');
       } else {
         throw res.text();
       }
@@ -33,15 +45,14 @@ export default function UnpublishLevelModal({ closeModal, isOpen, level }: Unpub
       toast.dismiss();
       toast.error('Error unpublishing level');
     }).finally(() => {
-      toast.dismiss();
-      toast.success('Unpublished');
-      setIsLoading(false);
+      setIsUnpublishing(false);
     });
   }
 
   return (
     <Modal
       closeModal={closeModal}
+      disabled={isUnpublishing}
       isOpen={isOpen}
       onConfirm={onConfirm}
       title={'Unpublish Level'}

commit 871dd5f05f678fdec99e4bd5a3cabcb575317f18

fix bug introduced by #790

pages/api/unpublish/[id].ts
@@ -97,9 +97,12 @@ export default withAuth({ POST: {
         }, {
           session: session,
         }),
-        LevelModel.insertMany([levelClone], { session: session }),
       ]);
 
+      // need to wait for the level to get deleted before we insert the new one (otherwise we get a duplicate key error)
+      await LevelModel.insertMany([levelClone], { session: session });
+
+      // need to wait for the level to get inserted before we update the stats
       await Promise.all([
         queueRefreshIndexCalcs(levelClone._id, { session: session }),
         queueCalcPlayAttempts(levelClone._id, { session: session }),