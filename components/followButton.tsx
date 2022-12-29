sspenst/thinky.gg

commit 3eb65765f6cfb970e1f240d6ed3a9e5b938f1447

kind of hard to repro, but i believe this fixes #780 (#790)
* kind of hard to repro, but i believe this fixes #780

* level management cleanup

Co-authored-by: Spencer Spenst <spencerspenst@gmail.com>

pages/api/unpublish/[id].ts
@@ -82,7 +82,6 @@ export default withAuth({ POST: {
         UserModel.updateMany({ _id: { $in: userIds } }, { $inc: { score: -1 } }, { session: session }),
         CollectionModel.updateMany({ levels: id }, { $pull: { levels: id } }, { session: session }),
         clearNotifications(undefined, undefined, level._id, undefined, { session: session }),
-
         MultiplayerMatchModel.updateMany({
           state: MultiplayerMatchState.ACTIVE,
           levels: id,
@@ -98,13 +97,14 @@ export default withAuth({ POST: {
         }, {
           session: session,
         }),
-
+        LevelModel.insertMany([levelClone], { session: session }),
       ]);
-      await LevelModel.insertMany([levelClone], { session: session });
+
       await Promise.all([
         queueRefreshIndexCalcs(levelClone._id, { session: session }),
-        queueCalcPlayAttempts(levelClone._id, { session: session })
+        queueCalcPlayAttempts(levelClone._id, { session: session }),
       ]);
+
       newId = levelClone._id;
     });
 

components/modal/index.tsx
@@ -4,14 +4,16 @@ import React, { Fragment } from 'react';
 import { rubik } from '../../pages/_app';
 
 interface ModalButtonProps {
+  disabled?: boolean;
   onClick: (event?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   text: string;
 }
 
-function ModalButton({ onClick, text }: ModalButtonProps) {
+function ModalButton({ disabled, onClick, text }: ModalButtonProps) {
   return (
     <button
       className={classNames('inline-flex justify-center px-4 py-2 text-sm font-medium border border-transparent rounded-md bg-button')}
+      disabled={disabled}
       onClick={onClick}
       style={{
         marginRight: 10,
@@ -26,6 +28,7 @@ function ModalButton({ onClick, text }: ModalButtonProps) {
 interface ModalProps {
   children: JSX.Element;
   closeModal: () => void;
+  disabled?: boolean;
   isOpen: boolean;
   onConfirm?: () => void;
   onSubmit?: () => void;
@@ -35,6 +38,7 @@ interface ModalProps {
 export default function Modal({
   children,
   closeModal,
+  disabled,
   isOpen,
   onConfirm,
   onSubmit,
@@ -109,16 +113,16 @@ export default function Modal({
               >
                 {onConfirm ?
                   <>
-                    <ModalButton onClick={onConfirm} text={'OK'} />
-                    <ModalButton onClick={closeModal} text={'Cancel'} />
+                    <ModalButton disabled={disabled} onClick={onConfirm} text={'OK'} />
+                    <ModalButton disabled={disabled} onClick={closeModal} text={'Cancel'} />
                   </>
                   : onSubmit ?
                     <>
-                      <ModalButton onClick={onSubmit} text={'Submit'} />
-                      <ModalButton onClick={closeModal} text={'Cancel'} />
+                      <ModalButton disabled={disabled} onClick={onSubmit} text={'Submit'} />
+                      <ModalButton disabled={disabled} onClick={closeModal} text={'Cancel'} />
                     </>
                     :
-                    <ModalButton onClick={closeModal} text={'Close'} />
+                    <ModalButton disabled={disabled} onClick={closeModal} text={'Close'} />
                 }
               </div>
             </Dialog.Panel>

pages/api/level/[id].ts
@@ -113,10 +113,12 @@ export default withAuth({
       queueRefreshIndexCalcs(new ObjectId(id as string))
     ]);
 
-    // revalidate the old endpoint
+    // revalidate the old and new endpoints
     if (level) {
-      // without this if statement we error when adding a level to our favorites that we don't own
-      await revalidateLevel(res, level.slug);
+      await Promise.all([
+        revalidateLevel(res, level.slug),
+        revalidateLevel(res, slug),
+      ]);
     }
 
     return res.status(200).json({ updated: true });

pages/api/publish/[id].ts
@@ -104,9 +104,9 @@ export default withAuth({ POST: {
         throw new Error('Level not found [RC]');
       }
 
-      await queueRefreshIndexCalcs(level._id, { session: session });
-      await queueCalcPlayAttempts(level._id, { session: session });
       await Promise.all([
+        queueRefreshIndexCalcs(level._id, { session: session }),
+        queueCalcPlayAttempts(level._id, { session: session }),
         createNewLevelNotifications(new ObjectId(req.userId), level._id, undefined, { session: session }),
         queueDiscordWebhook(Discord.LevelsId, `**${user?.name}** published a new level: [${level.name}](${req.headers.origin}/level/${level.slug}?ts=${ts})`, { session: session }),
       ]);

components/levelTable.tsx
@@ -155,12 +155,10 @@ export default function LevelTable({ getLevels, levels }: LevelTableProps) {
           onPublish={() => getLevels()}
         />
         <UnpublishLevelModal
-          closeModal={() => {
-            setIsUnpublishLevelOpen(false);
-            getLevels();
-          }}
+          closeModal={() => setIsUnpublishLevelOpen(false)}
           isOpen={isUnpublishLevelOpen}
           level={levelToModify}
+          onUnpublish={() => getLevels()}
         />
         <DeleteLevelModal
           closeModal={() => {

components/modal/addLevelModal.tsx
@@ -20,10 +20,15 @@ export default function AddLevelModal({ closeModal, isOpen, level }: AddLevelMod
   const [authorNote, setAuthorNote] = useState<string>();
   const [collections, setCollections] = useState<Collection[]>();
   const [collectionIds, setCollectionIds] = useState<string[]>([]);
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const [name, setName] = useState<string>();
   const router = useRouter();
   const { setIsLoading, shouldAttemptAuth } = useContext(AppContext);
   const { user } = useContext(PageContext);
+
+  useEffect(() => {
+    setIsLoading(isSubmitting);
+  }, [isSubmitting, setIsLoading]);
 
   const getCollections = useCallback(() => {
     if (isOpen && shouldAttemptAuth) {
@@ -94,7 +99,7 @@ export default function AddLevelModal({ closeModal, isOpen, level }: AddLevelMod
       return;
     }
 
-    setIsLoading(true);
+    setIsSubmitting(true);
     toast.loading(level ? 'Updating level...' : 'Adding level...');
 
     fetch(level ? `/api/level/${level._id}` : '/api/level', {
@@ -127,7 +132,7 @@ export default function AddLevelModal({ closeModal, isOpen, level }: AddLevelMod
       toast.dismiss();
       toast.error(JSON.parse(await err)?.error);
     }).finally(() => {
-      setIsLoading(false);
+      setIsSubmitting(false);
     });
   }
 
@@ -153,6 +158,7 @@ export default function AddLevelModal({ closeModal, isOpen, level }: AddLevelMod
   return (
     <Modal
       closeModal={closeModal}
+      disabled={isSubmitting}
       isOpen={isOpen}
       onSubmit={onSubmit}
       title={!isUsersLevel ? 'Add to...' : `${level ? 'Edit' : 'New'} Level`}

components/modal/publishLevelModal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { AppContext } from '../../contexts/appContext';
 import { PageContext } from '../../contexts/pageContext';
@@ -13,19 +13,18 @@ interface PublishLevelModalProps {
   onPublish: () => void;
 }
 
-export default function PublishLevelModal({
-  closeModal,
-  isOpen,
-  level,
-  onPublish,
-}: PublishLevelModalProps) {
+export default function PublishLevelModal({ closeModal, isOpen, level, onPublish }: PublishLevelModalProps) {
+  const [isPublishing, setIsPublishing] = useState(false);
   const { mutateUser } = useContext(PageContext);
   const { setIsLoading } = useContext(AppContext);
 
+  useEffect(() => {
+    setIsLoading(isPublishing);
+  }, [isPublishing, setIsLoading]);
+
   function onConfirm() {
-    closeModal();
+    setIsPublishing(true);
     toast.loading('Publishing level...');
-    setIsLoading(true);
 
     fetch(`/api/publish/${level._id}`, {
       method: 'POST',
@@ -35,6 +34,7 @@ export default function PublishLevelModal({
       },
     }).then(async res => {
       if (res.status === 200) {
+        closeModal();
         onPublish();
         mutateUser();
 
@@ -51,13 +51,14 @@ export default function PublishLevelModal({
       toast.dismiss();
       toast.error('Error publishing level');
     }).finally(() => {
-      setIsLoading(false);
+      setIsPublishing(false);
     });
   }
 
   return (
     <Modal
       closeModal={closeModal}
+      disabled={isPublishing}
       isOpen={isOpen}
       onConfirm={onConfirm}
       title={'Publish Level'}

components/modal/unpublishLevelModal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { AppContext } from '../../contexts/appContext';
 import { PageContext } from '../../contexts/pageContext';
@@ -9,22 +9,34 @@ interface UnpublishLevelModalProps {
   closeModal: () => void;
   isOpen: boolean;
   level: Level;
+  onUnpublish: () => void;
+
 }
 
-export default function UnpublishLevelModal({ closeModal, isOpen, level }: UnpublishLevelModalProps) {
+export default function UnpublishLevelModal({ closeModal, isOpen, level, onUnpublish }: UnpublishLevelModalProps) {
+  const [isUnpublishing, setIsUnpublishing] = useState(false);
   const { mutateUser } = useContext(PageContext);
   const { setIsLoading } = useContext(AppContext);
 
+  useEffect(() => {
+    setIsLoading(isUnpublishing);
+  }, [isUnpublishing, setIsLoading]);
+
   function onConfirm() {
-    closeModal();
-    setIsLoading(true);
+    setIsUnpublishing(true);
     toast.loading('Unpublishing...');
+
     fetch(`/api/unpublish/${level._id}`, {
       method: 'POST',
       credentials: 'include',
     }).then(res => {
       if (res.status === 200) {
+        closeModal();
+        onUnpublish();
         mutateUser();
+
+        toast.dismiss();
+        toast.success('Unpublished');
       } else {
         throw res.text();
       }
@@ -33,15 +45,14 @@ export default function UnpublishLevelModal({ closeModal, isOpen, level }: Unpub
       toast.dismiss();
       toast.error('Error unpublishing level');
     }).finally(() => {
-      toast.dismiss();
-      toast.success('Unpublished');
-      setIsLoading(false);
+      setIsUnpublishing(false);
     });
   }
 
   return (
     <Modal
       closeModal={closeModal}
+      disabled={isUnpublishing}
       isOpen={isOpen}
       onConfirm={onConfirm}
       title={'Unpublish Level'}

commit cfb93d05ea9b6f82f2b35c8d6ccb7e41d4ae856e

Removeuneededuseeffects (#796)
* don't think useeffect is needed here

* this setAllCompletions(false) useEffect doesnt seem to do anything

* cleanup on unmounting for fullscreen

* in Game remove use effects to stop onComplete being called a ton of times when player is on end cell and you refresh

* cleanup + game useEffect fix

* fix bug introduced by #790

Co-authored-by: Spencer Spenst <spencerspenst@gmail.com>

components/followButton.tsx
@@ -1,22 +1,18 @@
 import classNames from 'classnames';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { toast } from 'react-hot-toast';
 import GraphType from '../constants/graphType';
 import User from '../models/db/user';
 import { FollowData } from '../pages/api/follow';
 
 interface FollowButtonProps {
-  isFollowingInit: boolean;
+  isFollowing: boolean;
   onResponse?: (followData: FollowData) => void;
   user: User;
 }
 
-export default function FollowButton({ isFollowingInit, onResponse, user }: FollowButtonProps) {
-  const [isFollowing, setIsFollowing] = useState<boolean>();
-
-  useEffect(() => {
-    setIsFollowing(isFollowingInit);
-  }, [isFollowingInit]);
+export default function FollowButton({ isFollowing, onResponse, user }: FollowButtonProps) {
+  const [_isFollowing, setIsFollowing] = useState<boolean>(isFollowing);
 
   const onFollowButtonPress = async (ele: React.MouseEvent<HTMLButtonElement>) => {
     // disable button and make it opacity 0.5
@@ -26,7 +22,7 @@ export default function FollowButton({ isFollowingInit, onResponse, user }: Foll
     targ.style.opacity = '0.5';
 
     const res = await fetch('/api/follow', {
-      method: !isFollowing ? 'PUT' : 'DELETE',
+      method: !_isFollowing ? 'PUT' : 'DELETE',
       headers: {
         'Content-Type': 'application/json',
       },
@@ -56,16 +52,12 @@ export default function FollowButton({ isFollowingInit, onResponse, user }: Foll
     }
   };
 
-  if (isFollowing === undefined) {
-    return null;
-  }
-
   return (
     <button className={classNames(
       'font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer',
-      isFollowing ? 'bg-button' : 'bg-blue-500 hover:bg-blue-700 text-white',
+      _isFollowing ? 'bg-button' : 'bg-blue-500 hover:bg-blue-700 text-white',
     )} onClick={onFollowButtonPress}>
-      {!isFollowing ? 'Follow' : 'Unfollow'}
+      {!_isFollowing ? 'Follow' : 'Unfollow'}
     </button>
   );
 }

components/followingList.tsx
@@ -14,7 +14,7 @@ export default function FollowingList({ users }: FollowingListProps) {
         {users.map((user) => (
           <div className='grid grid-cols-2 gap-6 p-2' key={'following-list-row-' + user._id}>
             <FormattedUser user={user} />
-            <FollowButton user={user} isFollowingInit={true} />
+            <FollowButton isFollowing={true} user={user} />
           </div>
         ))}
       </div>

components/formattedLevelInfo.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import toast from 'react-hot-toast';
 import Dimensions from '../constants/dimensions';
 import { LevelContext } from '../contexts/levelContext';
@@ -19,10 +19,6 @@ export default function FormattedLevelInfo({ level }: FormattedLevelInfoProps) {
   const [collapsedAuthorNote, setCollapsedAuthorNote] = useState(true);
   const [hideStats, setHideStats] = useState(true);
   const levelContext = useContext(LevelContext);
-
-  useEffect(() => {
-    setAllCompletions(false);
-  }, [level]);
 
   const completionDivs = [];
   const maxCollapsedAuthorNote = 100;

components/level/game.tsx
@@ -261,13 +261,6 @@ export default function Game({
     });
   }, [disableServer, lastCodes, matchId, mutateLevel, mutateUser]);
 
-  useEffect(() => {
-    if (gameState.board[gameState.pos.y][gameState.pos.x].levelDataType === LevelDataType.End &&
-      gameState.moves.length <= level.leastMoves && onComplete) {
-      onComplete();
-    }
-  }, [gameState, level.leastMoves, onComplete]);
-
   const handleKeyDown = useCallback(code => {
     // boundary checks
     function isPositionValid(
@@ -331,175 +324,186 @@ export default function Game({
     }
 
     setGameState(prevGameState => {
-      // restart
-      if (code === 'KeyR') {
-        if (prevGameState.moveCount > 0) {
-          oldGameState.current = cloneGameState(prevGameState);
-        }
-
-        return initGameState(prevGameState.actionCount + 1);
-      }
-
-      // treat prevGameState as immutable
-      const blocks = prevGameState.blocks.map(block => block.clone());
-      const board = prevGameState.board.map(row => {
-        return row.map(square => square.clone());
-      });
-      const moves = prevGameState.moves.map(move => move.clone());
-
-      // undo
-      function undo() {
-        const prevMove = moves.pop();
-
-        // nothing to undo
-        if (prevMove === undefined) {
-          let returnState = undefined;
-
-          if (oldGameState.current) {
-            returnState = cloneGameState(oldGameState.current);
+      function getNewGameState() {
+        // restart
+        if (code === 'KeyR') {
+          if (prevGameState.moveCount > 0) {
+            oldGameState.current = cloneGameState(prevGameState);
           }
 
-          oldGameState.current = undefined;
-
-          return returnState || prevGameState;
+          return initGameState(prevGameState.actionCount + 1);
         }
 
-        // remove text only from the current position for smoother animations
-        const text = board[prevGameState.pos.y][prevGameState.pos.x].text;
+        // treat prevGameState as immutable
+        const blocks = prevGameState.blocks.map(block => block.clone());
+        const board = prevGameState.board.map(row => {
+          return row.map(square => square.clone());
+        });
+        const moves = prevGameState.moves.map(move => move.clone());
 
-        // the text may not exist since it is only added when moving away from a position
-        if (text[text.length - 1] === prevGameState.moveCount) {
-          text.pop();
-        }
+        // undo
+        function undo() {
+          const prevMove = moves.pop();
 
-        if (prevMove.block) {
-          const block = getBlockById(blocks, prevMove.block.id);
+          // nothing to undo
+          if (prevMove === undefined) {
+            let returnState = undefined;
 
-          if (block) {
-            block.pos = prevMove.block.pos.clone();
+            if (oldGameState.current) {
+              returnState = cloneGameState(oldGameState.current);
+            }
 
-            if (block.inHole) {
-              block.inHole = false;
+            oldGameState.current = undefined;
 
-              if (prevMove.holePos !== undefined) {
-                board[prevMove.holePos.y][prevMove.holePos.x].levelDataType = LevelDataType.Hole;
+            return returnState || prevGameState;
+          }
+
+          // remove text only from the current position for smoother animations
+          const text = board[prevGameState.pos.y][prevGameState.pos.x].text;
+
+          // the text may not exist since it is only added when moving away from a position
+          if (text[text.length - 1] === prevGameState.moveCount) {
+            text.pop();
+          }
+
+          if (prevMove.block) {
+            const block = getBlockById(blocks, prevMove.block.id);
+
+            if (block) {
+              block.pos = prevMove.block.pos.clone();
+
+              if (block.inHole) {
+                block.inHole = false;
+
+                if (prevMove.holePos !== undefined) {
+                  board[prevMove.holePos.y][prevMove.holePos.x].levelDataType = LevelDataType.Hole;
+                }
               }
             }
           }
+
+          return {
+            actionCount: prevGameState.actionCount + 1,
+            blocks: blocks,
+            board: board,
+            height: prevGameState.height,
+            moveCount: prevGameState.moveCount - 1,
+            moves: moves,
+            pos: prevMove.pos.clone(),
+            width: prevGameState.width,
+          };
         }
 
-        return {
-          actionCount: prevGameState.actionCount + 1,
-          blocks: blocks,
-          board: board,
-          height: prevGameState.height,
-          moveCount: prevGameState.moveCount - 1,
-          moves: moves,
-          pos: prevMove.pos.clone(),
-          width: prevGameState.width,
-        };
-      }
-
-      function makeMove(direction: Position) {
-        // if the position didn't change or the new position is invalid
-        if (!isPlayerPositionValid(board, prevGameState.height, pos, prevGameState.width)) {
-          return prevGameState;
-        }
-
-        const blockIndex = getBlockIndexAtPosition(blocks, pos);
-        const move = new Move(code, prevGameState.pos);
-
-        // if there is a block at the new position
-        if (blockIndex !== -1) {
-          const block = blocks[blockIndex];
-          const blockPos = block.pos.add(direction);
-
-          // if the block is not allowed to move this direction or the new position is invalid
-          if (!block.canMoveTo(blockPos) ||
-            !isBlockPositionValid(board, blocks, prevGameState.height, blockPos, prevGameState.width)) {
+        function makeMove(direction: Position) {
+          // if the position didn't change or the new position is invalid
+          if (!isPlayerPositionValid(board, prevGameState.height, pos, prevGameState.width)) {
             return prevGameState;
           }
 
-          move.block = block.clone();
-          block.pos = blockPos;
+          const blockIndex = getBlockIndexAtPosition(blocks, pos);
+          const move = new Move(code, prevGameState.pos);
 
-          // remove block if it is pushed onto a hole
-          if (board[blockPos.y][blockPos.x].levelDataType === LevelDataType.Hole) {
-            block.inHole = true;
-            board[blockPos.y][blockPos.x].levelDataType = LevelDataType.Default;
-            move.holePos = blockPos.clone();
+          // if there is a block at the new position
+          if (blockIndex !== -1) {
+            const block = blocks[blockIndex];
+            const blockPos = block.pos.add(direction);
+
+            // if the block is not allowed to move this direction or the new position is invalid
+            if (!block.canMoveTo(blockPos) ||
+              !isBlockPositionValid(board, blocks, prevGameState.height, blockPos, prevGameState.width)) {
+              return prevGameState;
+            }
+
+            move.block = block.clone();
+            block.pos = blockPos;
+
+            // remove block if it is pushed onto a hole
+            if (board[blockPos.y][blockPos.x].levelDataType === LevelDataType.Hole) {
+              block.inHole = true;
+              board[blockPos.y][blockPos.x].levelDataType = LevelDataType.Default;
+              move.holePos = blockPos.clone();
+            }
           }
+
+          const text = board[prevGameState.pos.y][prevGameState.pos.x].text;
+
+          // save text if it doesn't already exist (may exist due to undo)
+          if (text[text.length - 1] !== prevGameState.moveCount) {
+            text.push(prevGameState.moveCount);
+          }
+
+          // save history from this move
+          moves.push(move);
+
+          const moveCount = prevGameState.moveCount + 1;
+
+          if (board[pos.y][pos.x].levelDataType === LevelDataType.End) {
+            trackStats(moves.map(move => move.code), level._id.toString(), 3);
+          }
+
+          return {
+            actionCount: prevGameState.actionCount + 1,
+            blocks: blocks,
+            board: board,
+            height: prevGameState.height,
+            moveCount: moveCount,
+            moves: moves,
+            pos: pos,
+            width: prevGameState.width,
+          };
         }
 
-        const text = board[prevGameState.pos.y][prevGameState.pos.x].text;
-
-        // save text if it doesn't already exist (may exist due to undo)
-        if (text[text.length - 1] !== prevGameState.moveCount) {
-          text.push(prevGameState.moveCount);
+        // if explicitly asked to undo, undo
+        if (code === 'Backspace' || code === 'KeyU') {
+          return undo();
         }
 
-        // save history from this move
-        moves.push(move);
+        const direction = getDirectionFromCode(code);
 
-        const moveCount = prevGameState.moveCount + 1;
-
-        if (board[pos.y][pos.x].levelDataType === LevelDataType.End) {
-          trackStats(moves.map(move => move.code), level._id.toString(), 3);
+        // return if no valid direction was pressed
+        if (!direction) {
+          return prevGameState;
         }
 
-        return {
-          actionCount: prevGameState.actionCount + 1,
-          blocks: blocks,
-          board: board,
-          height: prevGameState.height,
-          moveCount: moveCount,
-          moves: moves,
-          pos: pos,
-          width: prevGameState.width,
-        };
-      }
+        // calculate the target tile to move to
+        const pos = prevGameState.pos.add(direction);
 
-      // if explicitly asked to undo, undo
-      if (code === 'Backspace' || code === 'KeyU') {
-        return undo();
-      }
+        // before making a move, check if undo is a better choice
+        function checkForFreeUndo() {
+          if (moves.length === 0) {
+            return false;
+          }
 
-      const direction = getDirectionFromCode(code);
+          // logic for valid free undo:
+          //  if the board state has not changed and you're backtracking
+          const lastMove = moves[moves.length - 1];
 
-      // return if no valid direction was pressed
-      if (!direction) {
-        return prevGameState;
-      }
-
-      // calculate the target tile to move to
-      const pos = prevGameState.pos.add(direction);
-
-      // before making a move, check if undo is a better choice
-      function checkForFreeUndo() {
-        if (moves.length === 0) {
-          return false;
+          return pos.equals(lastMove.pos) && !lastMove.block;
         }
 
-        // logic for valid free undo:
-        //  if the board state has not changed and you're backtracking
-        const lastMove = moves[moves.length - 1];
+        if (allowFreeUndo && checkForFreeUndo()) {
+          return undo();
+        }
 
-        return pos.equals(lastMove.pos) && !lastMove.block;
+        // lock movement once you reach the finish
+        if (prevGameState.board[prevGameState.pos.y][prevGameState.pos.x].levelDataType === LevelDataType.End) {
+          return prevGameState;
+        }
+
+        // if not, just make the move normally
+        return makeMove(direction);
       }
 
-      if (allowFreeUndo && checkForFreeUndo()) {
-        return undo();
+      const newGameState = getNewGameState();
+
+      if (newGameState.board[newGameState.pos.y][newGameState.pos.x].levelDataType === LevelDataType.End &&
+        newGameState.moves.length <= level.leastMoves && onComplete) {
+        onComplete();
       }
 
-      // lock movement once you reach the finish
-      if (prevGameState.board[prevGameState.pos.y][prevGameState.pos.x].levelDataType === LevelDataType.End) {
-        return prevGameState;
-      }
-
-      // if not, just make the move normally
-      return makeMove(direction);
+      return newGameState;
     });
-  }, [allowFreeUndo, initGameState, level._id, trackStats]);
+  }, [allowFreeUndo, initGameState, level._id, level.leastMoves, onComplete, trackStats]);
 
   const touchXDown = useRef<number>(0);
   const touchYDown = useRef<number>(0);

components/level/gameWrapper.tsx
@@ -18,7 +18,7 @@ interface GameWrapperProps {
 export default function GameWrapper({ collection, level, mutateLevel, onNext }: GameWrapperProps) {
   const { user, userLoading } = useContext(PageContext);
 
-  const signUpToast = useCallback(() => {
+  const signUpToast = () => {
     toast.dismiss();
     toast.success(
       <div className='flex flex-row'>
@@ -43,7 +43,7 @@ export default function GameWrapper({ collection, level, mutateLevel, onNext }: 
         duration: 10000,
         icon: '🎉',
       });
-  }, []);
+  };
 
   const addNextButtonHighlight = useCallback(() => {
     // find <button> with id 'btn-next'

components/page.tsx
@@ -46,6 +46,10 @@ export default function Page({
     } else {
       document.body.classList.remove('touch-none');
     }
+
+    return () => {
+      document.body.classList.remove('touch-none');
+    };
   }, [isFullScreen]);
 
   useEffect(() => {

pages/api/unpublish/[id].ts
@@ -97,9 +97,12 @@ export default withAuth({ POST: {
         }, {
           session: session,
         }),
-        LevelModel.insertMany([levelClone], { session: session }),
       ]);
 
+      // need to wait for the level to get deleted before we insert the new one (otherwise we get a duplicate key error)
+      await LevelModel.insertMany([levelClone], { session: session });
+
+      // need to wait for the level to get inserted before we update the stats
       await Promise.all([
         queueRefreshIndexCalcs(levelClone._id, { session: session }),
         queueCalcPlayAttempts(levelClone._id, { session: session }),

pages/profile/[name]/[[...tab]]/index.tsx
@@ -412,7 +412,7 @@ export default function ProfilePage({
         {reqUser && reqUserIsFollowing !== undefined && reqUser._id.toString() !== user._id.toString() && (
           <div className='m-4'>
             <FollowButton
-              isFollowingInit={reqUserIsFollowing}
+              isFollowing={reqUserIsFollowing}
               onResponse={followData => setFollowerCount(followData.followerCount)}
               user={user}
             />