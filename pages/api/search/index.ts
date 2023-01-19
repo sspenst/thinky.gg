59efca13e3cae5f33c8d2ab80b8f285f8e0f2219
Spencer Spenst <spencerspenst@gmail.com>
2023-01-19 10:33:57 -0800

    use Select cards instead of table

--- pages/api/search/index.ts
+++ pages/api/search/index.ts
@@ -31,10 +31,12 @@ export type SearchResult = {
 export async function doQuery(query: SearchQuery, userId?: ObjectId, projection: any = LEVEL_SEARCH_DEFAULT_PROJECTION) {
   await dbConnect();
 
-  const { block_filter, difficulty_filter, num_results, max_steps, min_steps, page, search, searchAuthor, searchAuthorId, show_filter, sort_by, sort_dir, time_range } = query;
+  const { block_filter, difficulty_filter, max_steps, min_steps, num_results, page, search, searchAuthor, searchAuthorId, show_filter, sort_by, sort_dir, time_range } = query;
+
+  // limit is between 1-20
+  const limit = Math.max(1, Math.min(parseInt(num_results as string) || 20, 20));
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const searchObj = { 'isDraft': false } as { [key: string]: any };
-  const limit = Math.max(0, Math.min(parseInt(num_results as string) || 20, 20));
 
   if (search && search.length > 0) {
     searchObj['name'] = {
@@ -110,7 +112,6 @@ export async function doQuery(query: SearchQuery, userId?: ObjectId, projection:
     else if (sort_by === 'calc_difficulty_estimate') {
       if (difficulty_filter === 'Pending') {
         // sort by unique users
-        projection['calc_playattempts_unique_users_count'] = { $size: '$calc_playattempts_unique_users' };
         sortObj.push(['calc_playattempts_unique_users_count', sort_direction * -1]);
       } else {
         sortObj.push(['calc_difficulty_estimate', sort_direction]);
@@ -228,13 +229,8 @@ export async function doQuery(query: SearchQuery, userId?: ObjectId, projection:
     const [levelsAgg] = await Promise.all([
       LevelModel.aggregate([
         { $match: searchObj },
-
-        { $project: {
-          ...projection,
-        },
-        },
+        { $project: { ...projection } },
         ...levelFilterStatLookupStage,
-
         { $sort: sortObj.reduce((acc, cur) => ({ ...acc, [cur[0]]: cur[1] }), {}) },
         { '$facet': {
           metadata: [ { $count: 'totalRows' } ],
@@ -266,9 +262,8 @@ export async function doQuery(query: SearchQuery, userId?: ObjectId, projection:
       ]),
     ]);
 
-    const totalRows = levelsAgg[0]?.metadata?.totalRows || 0;
-
     const levels = levelsAgg[0]?.data as EnrichedLevel[];
+    const totalRows = levelsAgg[0]?.metadata?.totalRows || 0;
 
     levels.forEach((level) => {
       cleanUser(level.userId);
