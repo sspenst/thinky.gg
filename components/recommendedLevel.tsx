b78a518dfdf0c7b5c27e0e6fc3b45ff4d71a4170
Danny Miller <k2xl.com@gmail.com>
2023-01-21 14:44:13 -0500

    forgot to add component

---
+++ components/recommendedLevel.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { EnrichedLevel } from '../models/db/level';
+import SelectOption from '../models/selectOption';
+import SelectOptionStats from '../models/selectOptionStats';
+import SelectCard from './selectCard';
+
+interface ContinuePlayingProps {
+  level: EnrichedLevel;
+}
+
+export default function RecommendedLevel({ level }: ContinuePlayingProps): JSX.Element {
+  return (
+    <div className='flex flex-col justify-center rounded-lg border'
+      style={{
+        backgroundColor: 'var(--bg-color-2)',
+        borderColor: 'var(--bg-color-3)',
+      }}
+    >
+      <h2 className='self-center px-4 pt-3 text-lg font-bold'>
+        Try this Level:
+      </h2>
+      <SelectCard
+        option={{
+          author: level.userId.name,
+          href: `/level/${level.slug}`,
+          id: level._id.toString(),
+          level: level,
+          stats: new SelectOptionStats(level.leastMoves, level.userMoves),
+          text: level.name,
+        } as SelectOption}
+      />
+    </div>
+  );
+}
