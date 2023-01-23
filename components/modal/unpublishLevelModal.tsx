e65f9b35b00ba0e774275eb815a90d03c923472c
Spencer Spenst <spencerspenst@gmail.com>
2023-01-23 14:19:09 -0800

    better modal for archive

--- components/modal/unpublishLevelModal.tsx
+++ components/modal/unpublishLevelModal.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { AppContext } from '../../contexts/appContext';
 import { PageContext } from '../../contexts/pageContext';
+import { TimerUtil } from '../../helpers/getTs';
 import Level from '../../models/db/level';
 import Modal from '.';
 
@@ -49,18 +50,23 @@ export default function UnpublishLevelModal({ closeModal, isOpen, level, onUnpub
     });
   }
 
+  const archive = level.ts < TimerUtil.getTs() - 24 * 60 * 60;
+
   return (
     <Modal
       closeModal={closeModal}
       disabled={isUnpublishing}
       isOpen={isOpen}
       onConfirm={onConfirm}
-      title={'Unpublish Level'}
+      title={`${archive ? 'Archive' : 'Unpublish'} Level`}
     >
       <div style={{ textAlign: 'center' }}>
-        {`Are you sure you want to unpublish your level '${level.name}'?`}
+        {`Are you sure you want to ${archive ? 'archive' : 'unpublish'} your level '${level.name}'?`}
         <br />
-        {'All stats and reviews for this level will be deleted.'}
+        {archive ?
+          'Your level will be moved to the archive account.' :
+          'All stats and reviews for this level will be deleted.'
+        }
       </div>
     </Modal>
   );
