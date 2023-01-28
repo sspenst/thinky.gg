a00665c60f946fa8f88acad4c232cbaaf7635d37
Danny Miller <k2xl.com@gmail.com>
2023-01-27 23:31:15 -0500

    stuff

--- pathology-react-native/App.tsx
+++ pathology-react-native/App.tsx
@@ -8,6 +8,8 @@ import AchievementInfo from '../constants/achievementInfo';
 import NotificationType from '../constants/notificationType';
 import { EnrichedLevel } from '../models/db/level';
 import Notification from '../models/db/notification';
+import notifee from '@notifee/react-native';
+
 import User from '../models/db/user';
 
 // TODO:
@@ -19,47 +21,53 @@ import User from '../models/db/user';
 export async function getNotificationString(username: string, notification: Notification) {
   const targetLevel = notification.target as EnrichedLevel;
   const targetUser = notification.target as User;
-
+  
   switch (notification.type) {
-  case NotificationType.NEW_ACHIEVEMENT:
+  case NotificationType.NEW_ACHIEVEMENT: {
     if (notification.source) {
       const achievement = notification.source;
 
-      return [`Achievement unlocked! ${AchievementInfo[achievement.type].description}`, 'https://pathology.gg/profile/' + username + '/achievements'];
+      return [`Achievement unlocked! ${AchievementInfo[achievement.type].description}`, 'https://pathology.gg/profile/' + username + '/achievements', undefined];
     }
 
-    return ['Unknown achievement', 'https://pathology.gg/profile/' + username + '/achievements'];
-
-  case NotificationType.NEW_FOLLOWER:
-    return [notification.source.name + ' started following you', 'https://pathology.gg/profile/' + notification.source.name];
-
-  case NotificationType.NEW_LEVEL:
-    return [notification.source.name + ` published a new level: ${targetLevel.name}`, `https://pathology.gg/level/${targetLevel.slug}`];
-
-  case NotificationType.NEW_RECORD_ON_A_LEVEL_YOU_BEAT:
-    return [notification.source.name + ` set a new record: ${targetLevel.name} - ${(notification.message)} moves', 'https://pathology.gg/level/${targetLevel.slug}`];
-
-  case NotificationType.NEW_REVIEW_ON_YOUR_LEVEL:
-    return [notification.source.name + ` wrote a ${isNaN(Number(notification.message)) ? notification.message : Number(notification.message) > 0 ? `${Number(notification.message)} stars` : undefined} review on your level ${targetLevel.name}`, `https://pathology.gg/level/${targetLevel.slug}`];
-
+    return ['Unknown achievement', 'https://pathology.gg/profile/' + username + '/achievements', undefined];
+  }
+  case NotificationType.NEW_FOLLOWER:{
+    const avatar = "/api/avatar/"+notification.source._id+".png"
+    return [notification.source.name + ' started following you', 'https://pathology.gg/profile/' + notification.source.name, avatar];
+  }
+  case NotificationType.NEW_LEVEL: {
+    const levelImage = "/api/level/image/"+targetLevel._id+".png"
+    return [notification.source.name + ` published a new level: ${targetLevel.name}`, `https://pathology.gg/level/${targetLevel.slug}`, levelImage];
+  }
+  case NotificationType.NEW_RECORD_ON_A_LEVEL_YOU_BEAT: {
+    const levelImage = "/api/level/image/"+targetLevel._id+".png"
+    return [notification.source.name + ` set a new record: ${targetLevel.name} - ${(notification.message)} moves', 'https://pathology.gg/level/${targetLevel.slug}`,levelImage];
+  }
+  case NotificationType.NEW_REVIEW_ON_YOUR_LEVEL: {
+    const levelImage = "/api/level/image/"+targetLevel._id+".png"
+    return [notification.source.name + ` wrote a ${isNaN(Number(notification.message)) ? notification.message : Number(notification.message) > 0 ? `${Number(notification.message)} stars` : undefined} review on your level ${targetLevel.name}`, `https://pathology.gg/level/${targetLevel.slug}`,levelImage];
+  }
   case NotificationType.NEW_WALL_POST: {
+    const avatar = "/api/avatar/"+notification.source._id+".png"
     const comment = notification.message ? JSON.parse(notification.message) : null;
 
     const shortenedText = comment ? (comment.text.length > 10 ? comment.text.substring(0, 10) + '...' : comment.text) : '';
 
-    return [notification.source.name + ` posted "${shortenedText}" on your profile.`, `https://pathology.gg/profile/${username}`];
+    return [notification.source.name + ` posted "${shortenedText}" on your profile.`, `https://pathology.gg/profile/${username}`, avatar];
   }
 
   case NotificationType.NEW_WALL_REPLY: {
+    const avatar = "/api/avatar/"+notification.source._id+".png"
     const comment = notification.message ? JSON.parse(notification.message) : null;
 
     const shortenedText = comment ? (comment.text.length > 10 ? comment.text.substring(0, 10) + '...' : comment.text) : '';
 
-    return [notification.source.name + ` replied "${shortenedText}" to your message on ${targetUser.name}'s profile.`, `https://pathology.gg/profile/${targetUser.name}`];
+    return [notification.source.name + ` replied "${shortenedText}" to your message on ${targetUser.name}'s profile.`, `https://pathology.gg/profile/${targetUser.name}`, avatar];
   }
 
   default:
-    return ['Unknown', 'https://pathology.gg/notifications'];
+    return ['Unknown', 'https://pathology.gg/notifications', undefined];
   }
 }
 
@@ -68,7 +76,11 @@ let lastNotificationTimestamp = 0;
 
 TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
   // make a network request to https://pathology.gg/api/user
-  const response = await fetch(`https://pathology.gg/api/notification?min_timestamp=${lastNotificationTimestamp}`, {
+
+  // temp
+  lastNotificationTimestamp = 0
+  console.log('fetching with lastNotificationTimestamp as ', lastNotificationTimestamp)  
+  const response = await fetch(`https://pathology.gg/api/notification?read=false&min_timestamp=${lastNotificationTimestamp}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -89,7 +101,7 @@ TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
   if (!data.notifications || !Array.isArray(data.notifications)) {
     return BackgroundFetch.BackgroundFetchResult.Failed;
   }
-
+  
   const notifications = data.notifications as Notification[];
   const unreadNotifications = notifications.filter(n => !n.read);
 
@@ -100,25 +112,30 @@ TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
   // if only one notification then write out more explicitly what the notif is
   let body = 'You have ' + unreadNotifications.length + ' unread notifications';
   let url = 'https://pathology.gg/notifications?filter=unread';
-
+  let img;
   const latestUnreadTs = new Date(unreadNotifications[0].createdAt).getTime();
 
   lastNotificationTimestamp = Math.max(latestUnreadTs, lastNotificationTimestamp);
-
+  
+  let imageUrl = undefined;
   if (unreadNotifications.length === 1) {
-    [body, url] = await getNotificationString(data.name, unreadNotifications[0]);
+    [body, url, img] = await getNotificationString(data.name, unreadNotifications[0]);
+    if (img) {
+      imageUrl ="https://pathology.gg"+img;
+    }
   }
-
+  console.log('lastNotificationTimestamp updated to ', lastNotificationTimestamp, imageUrl)  
   // create a notification, link to pathology.gg/notifications
-  await Notifications.scheduleNotificationAsync({
-    content: {
-      title: 'Notification',
+  await notifee.displayNotification({
+    
+      title: 'Pathology',
       body: body,
-      data: { url: url }
-    },
-    trigger: {
-      seconds: 1,
-    },
+      data: { url: url },
+      ios: {
+      // don't include if imageUrl is undefined
+      ...(imageUrl && { attachments: [{ url: imageUrl }] }),
+      
+    }
   });
 
   // Be sure to return the successful result type!
@@ -198,7 +215,7 @@ export default function BackgroundFetchScreen() {
 
   const webViewRef = useRef<any>();
   const [loading, setLoading] = useState(false);
-  const [url, setUrl] = useState('https://pathology.gg?platform=' + Platform.OS);
+  const [webViewUrl, setWebViewUrl] = useState('https://pathology.gg?platform=' + Platform.OS);
   const goBack = () => {
     if (webViewRef.current) {
       webViewRef.current.goBack();
@@ -226,21 +243,29 @@ export default function BackgroundFetchScreen() {
   const responseListener = useRef<any>();
 
   useEffect(() => {
-    responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
-      const { data } = response.notification.request.content;
-
+    console.log("Registering background event")
+    notifee.onForegroundEvent(async (event) => {
+      const {type } = event;
+      console.log("Type ", type)
+      const { data, id } = event.detail.notification;
+      await notifee.cancelNotification(id);
+      console.log("in background, data is ", data, "id is ", id)
       if (!data) return;
 
       const { url } = data;
-
+      console.log("onBackgroundEvent", url, webViewUrl)
       if (url) {
-        setUrl(url as string);
+        setWebViewUrl( (url as string)+"?"+Date.now() );
       }
+    
     });
-
+    notifee.onBackgroundEvent = notifee.onForegroundEvent;
+    
     return () => {
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
+      console.log("unregistering background event")
+      notifee.onForegroundEvent(null);
+      notifee.onBackgroundEvent = notifee.onForegroundEvent;
+    }
   }, []);
 
   const SCREEN_WIDTH = Dimensions.get('window').width;
@@ -282,7 +307,7 @@ export default function BackgroundFetchScreen() {
         onContentProcessDidTerminate={() => webViewRef.current.reload()}
         mediaPlaybackRequiresUserAction={true}
 
-        source={{ uri: url }}
+        source={{ uri: webViewUrl }}
       />
 
       <Button color={'white'} title={'Back'} onPress={goBack}
