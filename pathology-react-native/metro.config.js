8ef1ba595be34d1e0aecd496266247238f474c7d
Danny Miller <k2xl.com@gmail.com>
2023-02-06 12:56:46 -0500

    meh

--- pathology-react-native/metro.config.js
+++ pathology-react-native/metro.config.js
@@ -1,7 +1,31 @@
-// Learn more: https://docs.expo.io/guides/customizing-metro
+// https://docs.expo.io/guides/customizing-metro
+// https://dushyant37.medium.com/how-to-import-files-from-outside-of-root-directory-with-react-native-metro-bundler-18207a348427
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
-const config = getDefaultConfig(__dirname);
+const extraNodeModules = {
+  '..': path.resolve(__dirname + '/..'),
+};
 
-module.exports = config;
+const watchFolders = [
+  path.resolve(__dirname + '/..'),
+];
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  resolver: {
+    extraNodeModules: new Proxy(extraNodeModules, {
+      get: (target, name) =>
+        // redirects dependencies referenced from common/ to local node_modules
+        name in target ? target[name] : path.join(process.cwd(), `node_modules/${name}`),
+    }),
+  },
+  watchFolders,
+};
