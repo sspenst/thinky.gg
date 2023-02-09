9e52a0b592aeffbddd85b8b1042e71b35d3146ac
Spencer Spenst <spencerspenst@gmail.com>
2023-02-08 22:32:12 -0800

    hide reviews when in campaign

--- components/menu/dropdown.tsx
+++ components/menu/dropdown.tsx
@@ -59,13 +59,15 @@ export default function Dropdown() {
           </svg>
         </button>
       </div>
-      <div className='hidden sm:flex xl:hidden'>
-        <button onClick={() => setOpenModal(Modal.Reviews)}>
-          <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor' strokeWidth={2}>
-            <path strokeLinecap='round' strokeLinejoin='round' d='M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z' />
-          </svg>
-        </button>
-      </div>
+      {!levelContext?.hideReviews &&
+        <div className='hidden sm:flex xl:hidden'>
+          <button onClick={() => setOpenModal(Modal.Reviews)}>
+            <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor' strokeWidth={2}>
+              <path strokeLinecap='round' strokeLinejoin='round' d='M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z' />
+            </svg>
+          </button>
+        </div>
+      }
     </>}
     <Menu>
       <Menu.Button>
@@ -111,22 +113,24 @@ export default function Dropdown() {
                     </div>
                   )}
                 </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <div
-                      className='flex w-full items-center rounded-md cursor-pointer px-3 py-2 gap-3'
-                      onClick={() => setOpenModal(Modal.Reviews)}
-                      style={{
-                        backgroundColor: active ? 'var(--bg-color-3)' : undefined,
-                      }}
-                    >
-                      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='none' viewBox='0 0 24 24' stroke='currentColor' strokeWidth={2}>
-                        <path strokeLinecap='round' strokeLinejoin='round' d='M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z' />
-                      </svg>
-                      Reviews
-                    </div>
-                  )}
-                </Menu.Item>
+                {!levelContext?.hideReviews &&
+                  <Menu.Item>
+                    {({ active }) => (
+                      <div
+                        className='flex w-full items-center rounded-md cursor-pointer px-3 py-2 gap-3'
+                        onClick={() => setOpenModal(Modal.Reviews)}
+                        style={{
+                          backgroundColor: active ? 'var(--bg-color-3)' : undefined,
+                        }}
+                      >
+                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='none' viewBox='0 0 24 24' stroke='currentColor' strokeWidth={2}>
+                          <path strokeLinecap='round' strokeLinejoin='round' d='M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z' />
+                        </svg>
+                        Reviews
+                      </div>
+                    )}
+                  </Menu.Item>
+                }
               </div>
             }
             {!userLoading && user && levelContext?.level &&
