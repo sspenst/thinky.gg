fb53dce4a182f1409c8160d90964e08420c4639d
Spencer Spenst <spencerspenst@gmail.com>
2023-02-08 23:44:02 -0800

    misc UX updates

--- components/menu/dropdown.tsx
+++ components/menu/dropdown.tsx
@@ -59,7 +59,7 @@ export default function Dropdown() {
           </svg>
         </button>
       </div>
-      {!levelContext?.hideReviews &&
+      {!levelContext?.inCampaign &&
         <div className='hidden sm:flex xl:hidden'>
           <button onClick={() => setOpenModal(Modal.Reviews)}>
             <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor' strokeWidth={2}>
@@ -113,7 +113,7 @@ export default function Dropdown() {
                     </div>
                   )}
                 </Menu.Item>
-                {!levelContext?.hideReviews &&
+                {!levelContext?.inCampaign &&
                   <Menu.Item>
                     {({ active }) => (
                       <div
--- components/menu/userInfo.tsx
+++ components/menu/userInfo.tsx
@@ -21,7 +21,12 @@ export default function UserInfo() {
         </>
         :
         <>
-          <span className='font-bold'>{user.score}</span>
+          <div className='flex gap-1'>
+            <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6'>
+              <path strokeLinecap='round' strokeLinejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12' />
+            </svg>
+            <span className='font-bold'>{user.score}</span>
+          </div>
           <Notifications />
           <Link href={'/search'} passHref prefetch={false}>
             <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor' strokeWidth={2}>
