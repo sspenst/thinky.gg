dd79cacfe67dde516a66d43c51e3e1f60772ee97
Spencer Spenst <spencerspenst@gmail.com>
2023-02-13 15:47:41 -0800

    attempted to redo notifee stuff

---
+++ pathology-react-native/ios/NotifeeNotificationService/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  NotifeeNotificationService
+//
+//  Created by Spencer Spenst on 2023-02-13.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
