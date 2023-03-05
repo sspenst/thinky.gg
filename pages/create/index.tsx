ddadd70f160d0f1113a45fa8216dbef825815ffc
Spencer Spenst <spencerspenst@gmail.com>
2023-03-04 16:59:15 -0800

    upsertLevelImage on publish

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -58,7 +58,7 @@ export default function Create({ levels }: CreateProps) {
         </div>
         <div>
           <Link
-            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer'
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer block'
             href='/new'
           >
             New Level
