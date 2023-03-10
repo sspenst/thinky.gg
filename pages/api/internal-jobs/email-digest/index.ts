6199ffc220c285a779b47fe285b203261dc9fc16
Spencer Spenst <spencerspenst@gmail.com>
2023-03-09 22:04:18 -0800

    try to fix prod

--- pages/api/internal-jobs/email-digest/index.ts
+++ pages/api/internal-jobs/email-digest/index.ts
@@ -27,7 +27,8 @@ const transporter = isLocal() ? nodemailer.createTransport({
   rateLimit: 3,
   rateDelta: 10000,
 }) : nodemailer.createTransport({
-  host: process.env.EMAIL_SMTP_HOST || 'smtp.gmail.com',
+  // host: process.env.EMAIL_SMTP_HOST || 'smtp.gmail.com',
+  host: 'smtp.gmail.com',
   port: 465,
   secure: true,
   auth: {
