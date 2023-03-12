e6bfa74485cdd6b3574737cc157f6200559c43ca
Danny Miller <k2xl.com@gmail.com>
2023-03-11 20:31:28 -0500

    Move user to AppContext. Have PageContext simply use AppContext user

--- contexts/appContext.ts
+++ contexts/appContext.ts
@@ -1,13 +1,18 @@
 import { createContext } from 'react';
+import { ReqUser } from '../models/db/user';
 import { MultiplayerSocket } from '../pages/_app';
 
 interface AppContextInterface {
+  mutateUser: () => void;
+  user?: ReqUser;
+  userLoading: boolean;
   multiplayerSocket: MultiplayerSocket;
   setShouldAttemptAuth: React.Dispatch<React.SetStateAction<boolean>>;
   shouldAttemptAuth: boolean;
 }
 
 export const AppContext = createContext<AppContextInterface>({
+
   multiplayerSocket: {
     connectedPlayers: [],
     connectedPlayersCount: 0,
@@ -15,6 +20,9 @@ export const AppContext = createContext<AppContextInterface>({
     privateAndInvitedMatches: [],
     socket: undefined,
   },
+  mutateUser: () => { return; },
   setShouldAttemptAuth: () => { return; },
   shouldAttemptAuth: true,
+  userLoading: true,
+
 });
