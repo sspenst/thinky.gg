5e809c4485163415842aa8b9535ef33e0596165b
Danny Miller <k2xl.com@gmail.com>
2023-03-15 12:08:19 -0400

    Tabbed Settings page

--- components/formTemplate.tsx
+++ components/formTemplate.tsx
@@ -7,17 +7,12 @@ interface FormTemplateProps {
 
 export default function FormTemplate({ children, style }: FormTemplateProps) {
   return (
-    <div className='w-full max-w-sm mx-auto px-4 my-6'>
-      <div
-        className='shadow-md border rounded px-8 py-6'
-        style={{
-          background: 'var(--bg-color-2)',
-          borderColor: 'var(--bg-color-4)',
-          ...style,
-        }}
-      >
-        {children}
-      </div>
+    <div className='flex justify-center items-center p-3' style={{
+      ...style,
+    }}>
+
+      {children}
+
     </div>
   );
 }
--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,18 +1,109 @@
+import classNames from 'classnames';
 import { GetServerSidePropsContext } from 'next';
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useContext, useState } from 'react';
 import Page from '../../components/page';
 import SettingsForm from '../../components/settingsForm';
+import UploadImage from '../../components/uploadImage';
+import { AppContext } from '../../contexts/appContext';
 import redirectToLogin from '../../helpers/redirectToLogin';
+import styles from '../../pages/profile/[name]/[[...tab]]/ProfilePage.module.css';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   return await redirectToLogin(context);
 }
 
+function Tab({ label, value, activeTab, setActiveTab }: { label: string, value: string, activeTab: string, setActiveTab: (value: string) => void }) {
+  const cls = classNames(
+    'inline-block p-2 rounded-lg',
+    activeTab == value ? [styles['tab-active'], 'font-bold'] : styles.tab,
+  );
+
+  return (
+    <button
+      className={cls}
+      onClick={() => setActiveTab(value)}
+    >
+      {label}
+    </button>
+  );
+}
+
+function TabContent({ value, activeTab, children }: { value: string, activeTab: string, children: JSX.Element }) {
+  return activeTab === value ? <div className='p-4'>{children}</div> : null;
+}
+
 /* istanbul ignore next */
 export default function Settings() {
+  const [activeTab, setActiveTab] = useState('general');
+  const { forceUpdate, mutateUser, setShouldAttemptAuth } = useContext(AppContext);
+  const router = useRouter();
+
+  function deleteAccount() {
+    if (prompt('Are you sure you want to delete your account? Type DELETE to confirm.') === 'DELETE') {
+      fetch('/api/user', {
+        method: 'DELETE',
+      }).then(() => {
+        localStorage.clear();
+        sessionStorage.clear();
+        mutateUser(undefined);
+        setShouldAttemptAuth(false);
+        router.push('/');
+        forceUpdate();
+      });
+    }
+  }
+
+  // ...rest of the states and functions
+
   return (
-    <Page title={'Settings'}>
-      <SettingsForm />
+    <Page title='Settings'>
+      <div>
+        <div className="flex flex-wrap text-sm text-center gap-2 mt-2 justify-center">
+          <Tab
+            label="General"
+            value="general"
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+          <Tab
+            label="Account"
+            value="account"
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+          <Tab
+            label="Danger Zone"
+            value="danger"
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+        </div>
+        <TabContent value="general" activeTab={activeTab}>
+
+          <div className='flex flex-col gap-3 '>
+            <UploadImage />
+
+          </div>
+
+        </TabContent>
+        <TabContent value="account" activeTab={activeTab}>
+
+          <SettingsForm />
+
+        </TabContent>
+        <TabContent value="danger" activeTab={activeTab}>
+          <div className='flex flex-col gap-4 justify-center items-center'>
+            <div className='font-bold'>
+          Danger Zone
+            </div>
+            <button onClick={deleteAccount} className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-fit' type='button'>
+          Delete Account
+            </button>
+              This can not be undone!
+          </div>
+        </TabContent>
+      </div>
     </Page>
   );
 }
