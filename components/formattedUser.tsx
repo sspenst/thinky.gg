996aad8762e83c7f13d6dcf1d5ed06a5c3f38c01
Danny Miller <k2xl.com@gmail.com>
2023-03-17 19:24:07 -0400

    pro badge

--- components/formattedUser.tsx
+++ components/formattedUser.tsx
@@ -1,5 +1,7 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import Role from '../constants/role';
 import getProfileSlug from '../helpers/getProfileSlug';
 import User from '../models/db/user';
 import Avatar from './avatar';
@@ -9,6 +11,44 @@ interface FormattedUserProps {
   onClick?: () => void;
   size?: number;
   user?: User;
+}
+
+/** to do, move these functions to a helper file */
+export function getIconsForUser(user: User) {
+  return (
+    <>
+      {user.roles?.map((role: Role) => {
+        const { icon, description_short } = getRoleData(role);
+
+        return <span key={user._id + '-' + role} className='text-xs qtip tooltip' data-tooltip={description_short}>
+          {icon}
+        </span>;
+      })}
+    </>
+  );
+}
+
+export function getRoleData(role: Role) {
+  let icon;
+  let description_short = '';
+
+  if (role === Role.ADMIN) {
+    icon = '';
+    description_short = 'Admin';
+  } else if (role === Role.CURATOR) {
+    // moderator
+    icon = '';
+    description_short = 'Curator';
+  } else if (role === Role.PRO_SUBSCRIBER) {
+    // a star
+    icon = <Image alt='logo' src='/pro-logo.svg' width='32' height='32' className='h-6 w-6' />;
+    description_short = 'Pro Subscriber';
+  }
+
+  return {
+    icon: icon,
+    description_short: description_short,
+  };
 }
 
 export default function FormattedUser({ noLinks, onClick, size, user }: FormattedUserProps) {
@@ -29,9 +69,11 @@ export default function FormattedUser({ noLinks, onClick, size, user }: Formatte
         </>
         :
         <>
+
           <Link href={getProfileSlug(user)} passHref>
             <Avatar size={size} user={user} />
           </Link>
+          <div className='flex flex-col'>{getIconsForUser(user)}</div>
           <Link
             className='font-bold underline'
             href={getProfileSlug(user)}
@@ -40,6 +82,7 @@ export default function FormattedUser({ noLinks, onClick, size, user }: Formatte
           >
             <span>{user.name}</span>
           </Link>
+
         </>
       }
     </div>
