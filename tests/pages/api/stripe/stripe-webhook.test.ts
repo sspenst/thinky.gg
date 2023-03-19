c052e639da0efa5ffb53c3393b4e5274665ba4e9
Danny Miller <k2xl.com@gmail.com>
2023-03-19 13:08:17 -0400

    more test

--- tests/pages/api/stripe/stripe-webhook.test.ts
+++ tests/pages/api/stripe/stripe-webhook.test.ts
@@ -260,6 +260,56 @@ describe('pages/api/stripe-webhook/index.ts', () => {
       },
     });
   });
+  test('resubscribe again should error saying you are already a pro subscriber', async () => {
+    // Create a mock checkout.session.completed event
+    const mockEvent = createMockStripeEvent('checkout.session.completed', {
+      id: 'cs_test_123',
+      client_reference_id: TestId.USER,
+      customer: 'customer_id_123',
+      // Add any other required fields for your specific implementation
+    });
+
+    // Create a payload buffer and signature
+    const payload = JSON.stringify(mockEvent);
+    const signature = stripe.webhooks.generateTestHeaderString({ payload: payload, secret: stripe_secret as string });
+
+    // convert payload to a readable stream
+    const readablePayload = Buffer.from(payload);
+
+    jest.spyOn(StripeWebhookHelper, 'createStripeSigned').mockImplementation(async () => {
+      return mockEvent as unknown as Stripe.Event;
+    });
+
+    await testApiHandler({
+      handler: async (_, res) => {
+        const req: NextApiRequestWithAuth = {
+          ...DefaultReq,
+          headers: {
+            ...DefaultReq.headers,
+            'stripe-signature': signature,
+          },
+          body: readablePayload
+
+        } as unknown as NextApiRequestWithAuth;
+
+        await handler(req, res);
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        const response = await res.json();
+
+        expect(response.error).toBe('User with id ' + TestId.USER + ' is already a pro subscriber');
+
+        expect(res.status).toBe(400);
+
+        const [user, userConfig] = await Promise.all([UserModel.findById(TestId.USER), UserConfigModel.findOne({ userId: TestId.USER }, { stripeCustomerId: 1 })]);
+
+        expect(user.roles).toContain(Role.PRO_SUBSCRIBER);
+        expect(userConfig.stripeCustomerId).toBe('customer_id_123');
+        // Add any other assertions to validate the expected side effects of the subscription
+      },
+    });
+  });
   test('invoice payment failed but db error should cause user to stay on pro plan', async () => {
     // Create a mock invoice.payment_failed event
     const mockEvent = createMockStripeEvent('invoice.payment_failed', {
