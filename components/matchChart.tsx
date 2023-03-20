a8f5dce5a59b7e94ec3cb33b9601715386e1fe91
Spencer Spenst <spencerspenst@gmail.com>
2023-03-19 19:31:24 -0700

    responsive LineChart; space-before-blocks

--- components/matchChart.tsx
+++ components/matchChart.tsx
@@ -1,11 +1,11 @@
 import classNames from 'classnames';
 import moment from 'moment';
 import React from 'react';
-import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import Level from '../models/db/level';
 import MultiplayerMatch from '../models/db/multiplayerMatch';
 import { UserWithMultiplayerProfile } from '../models/db/user';
-import { MatchAction, MatchLogDataLevelComplete, MatchLogDataUserLeveId } from '../models/MultiplayerEnums';
+import { MatchAction, MatchLogDataLevelComplete, MatchLogDataUserLeveId, MultiplayerMatchTypeDurationMap } from '../models/MultiplayerEnums';
 
 interface MatchChartProps {
   match: MultiplayerMatch;
@@ -42,7 +42,7 @@ const CustomizedDot = (props: any) => {
 
 // https://recharts.org/en-US/examples/CustomizedDotLineChart
 export default function MatchChart({ match }: MatchChartProps) {
-  if (!match.matchLog) {
+  if (!match.matchLog || !match.players.length) {
     return null;
   }
 
@@ -71,9 +71,9 @@ export default function MatchChart({ match }: MatchChartProps) {
     }
 
     let completedBy = (log.data as MatchLogDataUserLeveId)?.userId?.toString();
-    const level = (log.data as MatchLogDataLevelComplete)?.levelId?.toString(); // For gamestart and gameend this will be undefined
+    const level = (log.data as MatchLogDataLevelComplete)?.levelId?.toString();
 
-    if (log.type === MatchAction.COMPLETE_LEVEL){
+    if (log.type === MatchAction.COMPLETE_LEVEL) {
       completedBy = (log.data as MatchLogDataLevelComplete).userId.toString();
       playerScore[completedBy] = playerScore[completedBy] ? playerScore[completedBy] + 1 : 1;
     }
@@ -93,67 +93,73 @@ export default function MatchChart({ match }: MatchChartProps) {
     });
   }
 
-  const maxScore = Object.values(playerScore).reduce((acc, score) => Math.max(acc, score), 0);
+  // add final scores to complete the line
+  for (const playerId in playerMap) {
+    chartData.push({
+      time: MultiplayerMatchTypeDurationMap[match.type],
+      type: MatchAction.GAME_END,
+      user: playerMap[playerId].name,
+      [playerMap[playerId].name]: playerScore[playerId],
+    });
+  }
 
-  return (<>
-    <LineChart width={400} height={400} data={chartData} >
-      <Line
-        connectNulls
-        dataKey={playerMap[match.players[0]._id.toString()].name}
-        dot={<CustomizedDot />}
-        isAnimationActive={false}
-        stroke='rgb(234 179 8)'
-        type='monotone'
-      />
-      <Line
-        connectNulls
-        dataKey={playerMap[match.players[1]._id.toString()].name}
-        dot={<CustomizedDot />}
-        isAnimationActive={false}
-        stroke='rgb(168 85 247)'
-        type='monotone'
-      />
-      <XAxis
-        dataKey='time'
-        scale='linear'
-        tickFormatter={timeStr => moment(timeStr).format('m:ss')}
-        ticks={[0, 30000, 60000, 90000, 120000, 150000, 180000]}
-        type='number'
-      />
-      <YAxis
-        interval={0}
-        tickCount={maxScore + 1}
-      />
-      <CartesianGrid
-        opacity={0.5}
-        stroke='var(--bg-color-4)'
-        strokeDasharray='1 9'
-        vertical={false}
-      />
-      <Legend />
-      <Tooltip
-        content={
-          ({ active, payload }) => {
-            if (active && payload && payload.length) {
-              const payloadObj = payload[0].payload;
+  return (
+    <ResponsiveContainer width='100%' height='100%'>
+      <LineChart width={400} height={400} data={chartData} margin={{ top: 10, right: 20, bottom: 0, left: -20 }}>
+        <Line
+          connectNulls
+          dataKey={playerMap[match.players[0]._id.toString()].name}
+          dot={<CustomizedDot />}
+          isAnimationActive={false}
+          stroke='rgb(234 179 8)'
+          type='monotone'
+        />
+        <Line
+          connectNulls
+          dataKey={playerMap[match.players[1]._id.toString()].name}
+          dot={<CustomizedDot />}
+          isAnimationActive={false}
+          stroke='rgb(168 85 247)'
+          type='monotone'
+        />
+        <XAxis
+          dataKey='time'
+          domain={[0, MultiplayerMatchTypeDurationMap[match.type]]}
+          interval={0}
+          tickFormatter={timeStr => moment(timeStr).format('m:ss')}
+          type='number'
+        />
+        <YAxis />
+        <CartesianGrid
+          opacity={0.5}
+          stroke='var(--bg-color-4)'
+          strokeDasharray='3 9'
+          vertical={false}
+        />
+        <Legend />
+        <Tooltip
+          content={
+            ({ active, payload }) => {
+              if (active && payload && payload.length) {
+                const payloadObj = payload[0].payload;
 
-              if (payloadObj.time === 0) {
-                return;
+                if (![MatchAction.COMPLETE_LEVEL, MatchAction.SKIP_LEVEL].includes(payloadObj.type as MatchAction)) {
+                  return;
+                }
+
+                return (
+                  <div className={classNames('p-2 border rounded', payloadObj.type === MatchAction.COMPLETE_LEVEL ? 'border-green-500' : 'border-blue-500')} style={{
+                    backgroundColor: 'var(--bg-color)',
+                  }}>
+                    {`${moment(payloadObj.time).format('m:ss')} [${payloadObj[payloadObj.user]}] ${payloadObj.user}`}  <span className='font-bold'>{payloadObj.type === MatchAction.COMPLETE_LEVEL ? 'completed' : 'skipped'}</span> {`${payloadObj.name}`}
+                  </div>
+                );
               }
-
-              return (
-                <div className={classNames('p-2 border rounded', payloadObj.type === MatchAction.COMPLETE_LEVEL ? 'border-green-500' : 'border-blue-500')} style={{
-                  backgroundColor: 'var(--bg-color)',
-                }}>
-                  {`${moment(payloadObj.time).format('m:ss')}`}: {payloadObj.user} <span className='font-bold'>{payloadObj.type === MatchAction.COMPLETE_LEVEL ? 'completed' : 'skipped'}</span> {`Level ${payloadObj.name}`}
-                </div>
-              );
             }
           }
-        }
-        wrapperStyle={{ outline: 'none' }}
-      />
-    </LineChart>
-  </>
+          wrapperStyle={{ outline: 'none' }}
+        />
+      </LineChart>
+    </ResponsiveContainer>
   );
 }
