e9e1e3d9881f69a8fd91fdaa2c7f5364d7a9a4f0
Danny Miller <k2xl.com@gmail.com>
2023-03-24 12:06:25 -0400

    more tests

--- pages/api/level/[id]/prostats/[type].ts
+++ pages/api/level/[id]/prostats/[type].ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import { NextApiResponse } from 'next';
 import ProStatsLevelType from '../../../../../constants/proStatsLevelType';
-import { ValidType } from '../../../../../helpers/apiWrapper';
+import { ValidEnum, ValidType } from '../../../../../helpers/apiWrapper';
 import isPro from '../../../../../helpers/isPro';
 import cleanUser from '../../../../../lib/cleanUser';
 import withAuth, { NextApiRequestWithAuth } from '../../../../../lib/withAuth';
@@ -159,7 +159,7 @@ async function getPlayAttemptsOverTime(levelId: string, userId: string) {
 export default withAuth({
   GET: {
     query: {
-      type: ValidType('string', false, true),
+      type: ValidEnum(Object.values(ProStatsLevelType)),
     }
   }
 }, async (req: NextApiRequestWithAuth, res: NextApiResponse) => {
