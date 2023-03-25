6d8ed09dd65beb39fce75629f23e6d99bd189d44
Danny Miller <k2xl.com@gmail.com>
2023-03-24 21:46:26 -0400

    checkpoint API plus 100% coverage

--- helpers/apiWrapper.ts
+++ helpers/apiWrapper.ts
@@ -65,6 +65,34 @@ export function ValidArray(mustExist = true) {
   };
 }
 
+export function ValidGameState() {
+  return (value?: unknown) => {
+    // should be a object like
+    /* { _id: string, gameState: GameState} */
+    if (typeof value !== 'object') {
+      return false;
+    } else {
+      // keys that it needs are all in interface GameState
+
+      const gameStateKeys = ['actionCount', 'blocks', 'board', 'height', 'moveCount', 'moves', 'pos', 'width'];
+      const valueKeys = Object.keys(value as { [key: string]: unknown });
+
+      for (const key of gameStateKeys) {
+        if (!valueKeys.includes(key)) {
+          return false;
+        }
+      }
+
+      if (valueKeys.length !== gameStateKeys.length) {
+        return false;
+      }
+    }
+
+    // make sure there wasn't any extra keys
+    return true;
+  };
+}
+
 export function ValidNumber(mustExist = true, min?: number, max?: number, incrementAllowed?: number) {
   return (value?: unknown) => {
     if (!mustExist && !value) {
