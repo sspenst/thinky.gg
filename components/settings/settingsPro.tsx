57c479f89ce945699453a27a7fe0a31632e7f1fa
Spencer Spenst <spencerspenst@gmail.com>
2023-03-28 16:37:52 -0700

    tweaks

--- components/settings/settingsPro.tsx
+++ components/settings/settingsPro.tsx
@@ -155,35 +155,31 @@ export default function SettingsPro({ stripePaymentLink }: SettingsProProps) {
             title='Checkpoints'
           />
           <ProFeature
-            description='View all solves on levels and analyze your play time'
+            description='View all solves and analyze your play time'
             icon={
-              <div className='rounded-full bg-green-500 border' style={{
-                borderColor: 'var(--bg-color-4)',
-              }}>
-                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6'>
-                  <path strokeLinecap='round' strokeLinejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12' />
-                </svg>
-              </div>
+              <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='rgb(34 197 94)' className='w-6 h-6'>
+                <path strokeLinecap='round' strokeLinejoin='round' d='M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z' />
+              </svg>
             }
-            title='Extra level stats'
+            title='Extra Level Stats'
           />
           <ProFeature
-            description='Search by block type, find unattempted levels, and more'
+            description='Search by block type, unattempted levels, and more'
             icon={
               <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor' strokeWidth={2}>
                 <path strokeLinecap='round' strokeLinejoin='round' d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z' />
               </svg>
             }
-            title='Advanced search filters'
+            title='Advanced Search Filters'
           />
           <ProFeature
-            description='Details on completed levels per user and per creator'
+            description='Details on levels completed per user and per creator'
             icon={
               <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='rgb(234 179 8)' className='w-6 h-6'>
                 <path strokeLinecap='round' strokeLinejoin='round' d='M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18' />
               </svg>
             }
-            title='User insights'
+            title='User Insights'
           />
         </div>
         {!userLoading && !isPro(user) && <>
