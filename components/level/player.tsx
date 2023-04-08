2dc05ef45334465e771e22bfbd4aa5d0bfbcd96d
Danny Miller <k2xl.com@gmail.com>
2023-04-07 23:56:56 -0400

    monkey theme

--- components/level/player.tsx
+++ components/level/player.tsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import React, { useState } from 'react';
 import LevelDataType from '../../constants/levelDataType';
-import Theme from '../../constants/theme';
+import Theme, { ICON_MAP } from '../../constants/theme';
 import isTheme from '../../helpers/isTheme';
 import Position from '../../models/position';
 import { GameState } from './game';
@@ -25,18 +25,65 @@ export default function Player({ borderWidth, gameState, leastMoves, size }: Pla
   const fontSizeRatio = text.length <= 3 ? 2 : (1 + (text.length - 1) / 2);
   const fontSize = innerSize / fontSizeRatio;
 
+  const parentStyle = {
+    backgroundColor: 'var(--bg-color)',
+    height: size,
+    left: size * initPos.x + (classic ? 2 * borderWidth : borderWidth),
+    top: size * initPos.y + (classic ? 0 : borderWidth),
+    transform: `translate(${(gameState.pos.x - initPos.x) * size}px, ${(gameState.pos.y - initPos.y) * size}px)`,
+    transition: 'transform 0.1s',
+    width: size,
+  };
+  const icon = ICON_MAP[Theme.Monkey]?.[LevelDataType.Start];
+
+  if (icon) {
+    const overstepped = leastMoves !== 0 && gameState.moveCount > leastMoves;
+
+    return <div
+      className='absolute'
+      style={parentStyle}
+    >
+      <div
+        className={classNames(
+          'cursor-default select-none z-20',
+          overstepped ? styles.extra : undefined,
+          !atEnd ? undefined : leastMoves !== 0 && gameState.moveCount > leastMoves ? styles.lose :
+            classic ? styles['win-classic'] : styles.win,
+          classic ? styles.classic : undefined,
+        )}
+        id='player'
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          position: 'relative',
+          backgroundColor: 'var(--level-player)',
+          borderColor: 'var(--level-player-extra)',
+          borderWidth: classic ? 1 : 0,
+          boxShadow: classic ?
+            `-${2 * borderWidth}px ${2 * borderWidth}px 0 0 var(--bg-color)` :
+            `0 0 0 ${borderWidth}px var(--bg-color)`,
+          color: 'var(--level-player-text)',
+          fontSize: fontSize,
+          height: innerSize,
+          lineHeight: innerSize + 'px',
+          textAlign: 'center',
+          verticalAlign: 'middle',
+          width: innerSize,
+        }}
+      >
+
+        <span className='theme-monkey-player' style={{ position: 'absolute', zIndex: 0, bottom: 0 }}>{icon(innerSize, text, leastMoves, overstepped )}</span>
+
+      </div>
+
+    </div>;
+  }
+
   return (
     <div
       className='absolute'
-      style={{
-        backgroundColor: 'var(--bg-color)',
-        height: size,
-        left: size * initPos.x + (classic ? 2 * borderWidth : borderWidth),
-        top: size * initPos.y + (classic ? 0 : borderWidth),
-        transform: `translate(${(gameState.pos.x - initPos.x) * size}px, ${(gameState.pos.y - initPos.y) * size}px)`,
-        transition: 'transform 0.1s',
-        width: size,
-      }}
+      style={parentStyle}
     >
       <div
         className={classNames(
--- constants/theme.ts
+++ constants/theme.ts
@@ -1,5 +1,8 @@
+import { MONKEY_THEME_ICONS } from '@root/components/theme/monkey';
+
 const Theme: {[theme: string]: string} = {
   'Modern': 'theme-modern',
+  'Monkey': 'theme-monkey',
   'Classic': 'theme-classic',
   'Light': 'theme-light',
   'Dark': 'theme-dark',
@@ -8,5 +11,9 @@ const Theme: {[theme: string]: string} = {
   'Winter': 'theme-winter',
   'Halloween': 'theme-halloween',
 };
+
+export const ICON_MAP = {
+  [Theme.Monkey]: MONKEY_THEME_ICONS
+} as any;
 
 export default Theme;
