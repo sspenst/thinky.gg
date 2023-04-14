df88e33add29a34e97ade0cadd6f35e6ee7bda3d
Danny Miller <k2xl.com@gmail.com>
2023-04-14 18:12:30 -0400

    separate to step files

---
+++ components/tour/steps/CHAPTER_1.ts
@@ -0,0 +1,10 @@
+import { Step } from 'react-joyride';
+
+export const TOUR_STEPS_CHAPTER_1: Step[] = [
+  {
+    disableBeacon: true,
+    target: '#level-selectcard-0',
+    content: 'This is the first level',
+    // navigate to first chapter
+  },
+];
