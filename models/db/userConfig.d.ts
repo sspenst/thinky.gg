fed5ec88bcfd8e42f35611adc5d0d712902b2bb9
Danny Miller <k2xl.com@gmail.com>
2023-04-14 21:38:20 -0400

    move to hooks folder

--- models/db/userConfig.d.ts
+++ models/db/userConfig.d.ts
@@ -1,4 +1,4 @@
-import { TourTypes } from '@root/components/tour/tourData';
+import { TourTypes } from '@root/hooks/useTour';
 import { Types } from 'mongoose';
 import EmailDigestSettingTypes from '../../constants/emailDigest';
 import User from './user';
--- pages/chapter1/index.tsx
+++ pages/chapter1/index.tsx
@@ -1,5 +1,5 @@
-import { useTour } from '@root/components/tour/tourData';
 import { AppContext } from '@root/contexts/appContext';
+import { useTour } from '@root/hooks/useTour';
 import { ReqUser } from '@root/models/db/user';
 import { GetServerSidePropsContext, NextApiRequest } from 'next';
 import Link from 'next/link';
