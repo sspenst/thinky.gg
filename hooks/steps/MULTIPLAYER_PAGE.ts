efe6e0b95048e40ebc5af66e5e432b0d19932f75
Danny Miller <k2xl.com@gmail.com>
2023-04-17 19:40:29 -0400

    multiplayer page tour file

---
+++ hooks/steps/MULTIPLAYER_PAGE.ts
@@ -0,0 +1,12 @@
+import { Step } from 'react-joyride';
+
+export const TOUR_STEPS_MULTIPLAYER_PAGE: Step[] = [
+  {
+    disableBeacon: true,
+    target: '#create_button_section',
+    title: 'Real time multiplayer!',
+    content: 'Play 5 games and see your Elo rating. While you wait for someone to join you can go back to playing your levels. When someone joins you will be redirected back.',
+    placement: 'auto',
+  },
+
+];
