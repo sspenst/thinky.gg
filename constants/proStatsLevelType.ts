f7704a384dcc7e3186589010488f2ec550d76417
Danny Miller <k2xl.com@gmail.com>
2023-04-20 18:06:31 -0400

    should be working

--- constants/proStatsLevelType.ts
+++ constants/proStatsLevelType.ts
@@ -1,6 +1,7 @@
 enum ProStatsLevelType {
   CommunityStepData = 'community-step-data',
   PlayAttemptsOverTime = 'play-attempts-over-time',
+  CommunityPlayAttemptData = 'community-play-attempt-data',
 }
 
 export default ProStatsLevelType;
