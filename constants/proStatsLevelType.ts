55545254454205413dd4d0c39fccc4e1e1ebd386
Spencer Spenst <spencerspenst@gmail.com>
2023-04-20 17:04:49 -0700

    api update only (#920)

--- constants/proStatsLevelType.ts
+++ constants/proStatsLevelType.ts
@@ -1,4 +1,5 @@
 enum ProStatsLevelType {
+  CommunityPlayAttemptsData = 'community-play-attempts-data',
   CommunityStepData = 'community-step-data',
   PlayAttemptsOverTime = 'play-attempts-over-time',
 }
