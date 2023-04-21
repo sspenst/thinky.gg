69d1b833f1b41d003bbfc67c9ba85ac596917e00
Spencer Spenst <spencerspenst@gmail.com>
2023-04-20 17:07:50 -0700

    fix merge for real

--- constants/proStatsLevelType.ts
+++ constants/proStatsLevelType.ts
@@ -2,7 +2,6 @@ enum ProStatsLevelType {
   CommunityPlayAttemptsData = 'community-play-attempts-data',
   CommunityStepData = 'community-step-data',
   PlayAttemptsOverTime = 'play-attempts-over-time',
-  CommunityPlayAttemptData = 'community-play-attempt-data',
 }
 
 export default ProStatsLevelType;
