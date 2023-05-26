2a86bb40fc294459c4a0543e91b4e245259a43ed
Spencer Spenst <spencerspenst@gmail.com>
2023-05-26 11:08:51 -0700

    Revert "Extra text fix contrast (#933)"
    This reverts commit 2187833899008d120cc3566d3b89bd83bcc4eaed.

--- components/theme/monkey.tsx
+++ components/theme/monkey.tsx
@@ -2,17 +2,17 @@ import TileType from '@root/constants/tileType';
 import React from 'react';
 
 export interface ThemeIconProps {
-  fontSize: number;
-  innerSize: number;
-  leastMoves: number;
-  overstepped: boolean;
-  size: number;
-  text: JSX.Element;
-  tileType: TileType;
+    tileType: TileType;
+    innerSize: number;
+    size: number;
+    fontSize: number;
+    text: JSX.Element;
+    leastMoves: number;
+    overstepped: boolean;
 }
 
 // TOOD figure out how to memoize these functions so they don't get recreated on every render
-function Monkey({ overstepped, size }: ThemeIconProps) {
+function Monkey({ size, overstepped }: ThemeIconProps) {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
