bb00989a269c5e03aa103c68eed4dc4728b4e1a5
Spencer Spenst <spencerspenst@gmail.com>
2023-05-31 14:46:58 -0700

    POST collection transaction

--- pages/api/collection/index.ts
+++ pages/api/collection/index.ts
@@ -1,10 +1,10 @@
 import isFullAccount from '@root/helpers/isFullAccount';
-import { Types } from 'mongoose';
+import Collection from '@root/models/db/collection';
+import mongoose, { Types } from 'mongoose';
 import type { NextApiResponse } from 'next';
 import { ValidType } from '../../../helpers/apiWrapper';
 import { generateCollectionSlug } from '../../../helpers/generateSlug';
 import { logger } from '../../../helpers/logger';
-import dbConnect from '../../../lib/dbConnect';
 import withAuth, { NextApiRequestWithAuth } from '../../../lib/withAuth';
 import { CollectionModel } from '../../../models/mongoose';
 
@@ -21,27 +21,31 @@ export default withAuth({
     });
   }
 
-  try {
-    const { authorNote, name } = req.body;
+  const session = await mongoose.startSession();
+  let collection: Collection | null = null;
 
-    await dbConnect();
-    const trimmedName = name.trim();
-    // TODO: in extremely rare cases there could be a race condition, might need a transaction here
-    const slug = await generateCollectionSlug(req.user.name, trimmedName);
-    const collection = await CollectionModel.create({
-      _id: new Types.ObjectId(),
-      authorNote: authorNote?.trim(),
-      name: trimmedName,
-      slug: slug,
-      userId: req.userId,
+  try {
+    await session.withTransaction(async () => {
+      const { authorNote, name } = req.body;
+      const trimmedName = name.trim();
+      const slug = await generateCollectionSlug(req.user.name, trimmedName, undefined, { session: session });
+
+      collection = (await CollectionModel.create([{
+        _id: new Types.ObjectId(),
+        authorNote: authorNote?.trim(),
+        name: trimmedName,
+        slug: slug,
+        userId: req.userId,
+      }], { session: session }))[0];
     });
 
-    return res.status(200).json(collection);
+    session.endSession();
   } catch (err) /* istanbul ignore next */ {
     logger.error(err);
+    session.endSession();
 
-    return res.status(500).json({
-      error: 'Error creating collection',
-    });
+    return res.status(500).json({ error: 'Error creating collection' });
   }
+
+  return res.status(200).json(collection);
 });
