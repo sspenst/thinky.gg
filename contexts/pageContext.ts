889a024ca5e61bbce7c82b46cd3d6cb88ef88e70
Spencer Spenst <spencerspenst@gmail.com>
2023-07-09 01:14:57 -0700

    better fullscreen mode (fixes #779)

--- contexts/pageContext.ts
+++ contexts/pageContext.ts
@@ -2,10 +2,14 @@ import { createContext } from 'react';
 
 interface PageContextInterface {
   preventKeyDownEvent: boolean;
+  setShowHeader: React.Dispatch<React.SetStateAction<boolean>>;
   setPreventKeyDownEvent: React.Dispatch<React.SetStateAction<boolean>>;
+  showHeader: boolean;
 }
 
 export const PageContext = createContext<PageContextInterface>({
   preventKeyDownEvent: false,
+  setShowHeader: () => { return; },
   setPreventKeyDownEvent: () => { return; },
+  showHeader: true,
 });
