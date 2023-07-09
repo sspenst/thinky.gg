6d1b57de64d517c849d7ae21c64f5530765c0cbd
Spencer Spenst <spencerspenst@gmail.com>
2023-07-08 23:46:18 -0700

    header/main/footer structure

--- components/page.tsx
+++ components/page.tsx
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from 'react';
 import Dimensions from '../constants/dimensions';
 import { PageContext } from '../contexts/pageContext';
 import Footer from './footer';
+import Header from './header';
 import LinkInfo from './linkInfo';
-import Menu from './menu';
 
 interface PageProps {
   children: JSX.Element;
@@ -40,30 +40,23 @@ export default function Page({
   }, [isFullScreen]);
 
   return (
-    <>
-      <div className={classNames({ 'fixed inset-0 overflow-hidden': isFullScreen })} style={{
-        color: 'var(--color)',
-      }}>
-        <PageContext.Provider value={{
-          preventKeyDownEvent: preventKeyDownEvent,
-          setPreventKeyDownEvent: setPreventKeyDownEvent,
+    <PageContext.Provider value={{
+      preventKeyDownEvent: preventKeyDownEvent,
+      setPreventKeyDownEvent: setPreventKeyDownEvent,
+    }}>
+      <div className={classNames('flex flex-col', { 'fixed inset-0 overflow-hidden': isFullScreen })}>
+        <Header
+          folders={folders}
+          subtitle={subtitle ? new LinkInfo(subtitle, subtitleHref) : undefined}
+          title={title ? new LinkInfo(title, titleHref) : undefined}
+        />
+        <main className='grow z-10' style={{
+          height: `calc(100% - ${Dimensions.MenuHeight}px)`,
         }}>
-          <div className='flex flex-col h-full'>
-            <Menu
-              folders={folders}
-              subtitle={subtitle ? new LinkInfo(subtitle, subtitleHref) : undefined}
-              title={title ? new LinkInfo(title, titleHref) : undefined}
-            />
-            <div className='grow z-10' style={{
-              backgroundColor: 'var(--bg-color)',
-              height: `calc(100% - ${Dimensions.MenuHeight}px)`,
-            }}>
-              {children}
-            </div>
-          </div>
-        </PageContext.Provider>
-        {!isFullScreen && <Footer />}
+          {children}
+        </main>
       </div>
-    </>
+      {!isFullScreen && <Footer />}
+    </PageContext.Provider>
   );
 }
