2b8913e6192b4aa4fbb1bf35ec73a5ca8522ed93
Danny Miller <k2xl.com@gmail.com>
2023-07-11 14:07:19 -0400

    pro feature in settings description for checkpoint

--- components/settings/settingsPro.tsx
+++ components/settings/settingsPro.tsx
@@ -120,7 +120,7 @@ export default function SettingsPro({ stripeCustomerPortalLink, stripePaymentLin
             title='Pro Badge'
           />
           <ProFeature
-            description='Save multiple game states for each level'
+            description='Save multiple game states for each level. Have your best level performance automatically saved.'
             icon={
               <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='rgb(239 68 68)' className='w-6 h-6'>
                 <path strokeLinecap='round' strokeLinejoin='round' d='M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5' />
