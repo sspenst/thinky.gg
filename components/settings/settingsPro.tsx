c1848ad3aef09240d11fac4996afea922a309f16
Danny Miller <k2xl.com@gmail.com>
2023-07-11 14:06:45 -0400

    pro feature in settings description

--- components/settings/settingsPro.tsx
+++ components/settings/settingsPro.tsx
@@ -120,6 +120,14 @@ export default function SettingsPro({ stripeCustomerPortalLink, stripePaymentLin
             title='Pro Badge'
           />
           <ProFeature
+            description='Redo your moves in the game.'
+            icon={<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='bi bi-arrow-clockwise' viewBox='0 0 16 16'>
+              <path fillRule='evenodd' d='M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z' />
+              <path d='M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z' />
+            </svg>}
+            title='Redo'
+          />
+          <ProFeature
             description='Save multiple game states for each level'
             icon={
               <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='rgb(239 68 68)' className='w-6 h-6'>
