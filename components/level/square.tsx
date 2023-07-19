aff8e698333a874d92d5a2baa650eeef227b6e20
Spencer Spenst <spencerspenst@gmail.com>
2023-07-19 00:06:45 -0700

    refactor block/player/square into Tile component

--- components/level/square.tsx
+++ components/level/square.tsx
@@ -4,10 +4,12 @@ import classNames from 'classnames';
 import React, { useCallback, useContext } from 'react';
 import Theme, { getIconFromTheme } from '../../constants/theme';
 import TileType from '../../constants/tileType';
+import styles from './Square.module.css';
 
 interface SquareProps {
   borderWidth: number;
   handleClick?: (rightClick: boolean) => void;
+  inHole?: boolean;
   leastMoves: number;
   size: number;
   text?: number;
@@ -17,6 +19,7 @@ interface SquareProps {
 export default function Square({
   borderWidth,
   handleClick,
+  inHole,
   leastMoves,
   size,
   text,
@@ -34,7 +37,7 @@ export default function Square({
   const { theme } = useContext(AppContext);
   const classic = theme === Theme.Classic;
   const innerSize = size - 2 * borderWidth;
-  const innerBorderWidth = Math.round(innerSize / 4.5);
+  const innerBorderWidth = Math.round(innerSize / 5);
   const fontSizeRatio = text === undefined || String(text).length <= 3 ?
     2 : (1 + (String(text).length - 1) / 2);
   const fontSize = innerSize / fontSizeRatio * (classic ? 1.5 : 1);
@@ -73,8 +76,8 @@ export default function Square({
     borderTopWidth: tileType === TileType.Hole || TileTypeHelper.canMoveDown(tileType) ? innerBorderWidth : 0,
     boxShadow: !classic ? `0 0 0 ${borderWidth}px var(--bg-color)` :
       tileType === TileType.Wall ||
-    tileType === TileType.Start ||
-    TileTypeHelper.canMove(tileType) ?
+      tileType === TileType.Start ||
+      TileTypeHelper.canMove(tileType) ?
         `-${2 * borderWidth}px ${2 * borderWidth}px 0 0 var(--bg-color)` :
         `${2 * borderWidth}px -${2 * borderWidth}px 0 0 var(--bg-color)`,
     fontSize: fontSize,
@@ -98,12 +101,9 @@ export default function Square({
       <span className={'theme-' + theme + '-' + tileType} style={{ position: 'absolute', zIndex: 0, }}>
         {icon({
           fontSize: fontSize,
-          innerSize: innerSize / 1.5,
-          leastMoves: leastMoves,
           overstepped: overStepped,
           size: size,
           text: <>{text}</>,
-          tileType: tileType,
         })}
       </span>
     );
@@ -115,6 +115,7 @@ export default function Square({
         `select-none block_type_${tileType} text-center align-middle`,
         { 'square-movable': TileTypeHelper.canMove(tileType) },
         { 'square-hole': tileType === TileType.Hole },
+        inHole ? styles['in-hole'] : undefined,
       )}
       onClick={onClick}
       onContextMenu={onClick}
