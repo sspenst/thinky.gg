9dedcf8f4ac3537a7f53fb37b35dfe0eb094bed8
Danny Miller <k2xl.com@gmail.com>
2023-08-01 14:24:31 -0400

    compare

--- components/profile/proifleMultiplayer.tsx
+++ components/profile/proifleMultiplayer.tsx
@@ -1,10 +1,12 @@
 import useSWRHelper from '@root/hooks/useSWRHelper';
 import MultiplayerMatch from '@root/models/db/multiplayerMatch';
-import { UserWithMultiplayerProfile } from '@root/models/db/user';
+import User, { UserWithMultiplayerProfile } from '@root/models/db/user';
 import { MultiplayerMatchType } from '@root/models/MultiplayerEnums';
-import React from 'react';
+import Link from 'next/link';
+import React, { useState } from 'react';
 import MatchResults from '../matchResults';
 import { getProfileRatingDisplay } from '../matchStatus';
+import MultiSelectUser from '../multiSelectUser';
 
 interface ProfileMultiplayerProps {
   user: UserWithMultiplayerProfile;
@@ -18,8 +20,10 @@ interface ProfileMultiplayerProps {
 
 export default function ProfileMultiplayer({ user, records }: ProfileMultiplayerProps) {
   // return a list of multiplayer games
+  const qs = user._id.toString();
+  const [compare, setCompare] = useState<User>();
   const { data: multiplayerGames } = useSWRHelper<MultiplayerMatch[]>(
-    '/api/match/search?limit=100&players=' + user._id.toString()
+    '/api/match/search?limit=100&players=' + qs + (compare ? ',' + compare?._id.toString() : ''),
   );
 
   if (!multiplayerGames) {
@@ -29,8 +33,16 @@ export default function ProfileMultiplayer({ user, records }: ProfileMultiplayer
   return (
     <div className='flex flex-col gap-4 text-center justify-center items-center'>
       <h2 className='text-2xl font-bold break-words max-w-full'>
-        Multiplayer Games
+        {user.name}&apos;s Multiplayer History
       </h2>
+      <div className='flex flex-row gap-2  text-center justify-center items-center'>
+        <MultiSelectUser defaultValue={compare} onSelect={(selected: User) => {
+          setCompare(selected);
+        }} />
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+          <Link href='/multiplayer'>Multiplayer Lobby</Link>
+        </button>
+      </div>
       <div className='flex flex-col gap-4'>
         <div className='flex flex-row gap-4 text-center justify-center items-center'>
           {getProfileRatingDisplay({
