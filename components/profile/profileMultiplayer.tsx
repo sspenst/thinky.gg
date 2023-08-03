94bb7ae7e5f0f8d9e2614aa7697a3d865202de59
Danny Miller <k2xl.com@gmail.com>
2023-08-02 21:54:34 -0400

    pagination

--- components/profile/profileMultiplayer.tsx
+++ components/profile/profileMultiplayer.tsx
@@ -20,25 +20,73 @@ export interface MultiplayerRecord {
 
 export type MultiplayerRecords = {
   [T in MultiplayerMatchType]: MultiplayerRecord;
-}
+};
 
 export default function ProfileMultiplayer({ user }: ProfileMultiplayerProps) {
   // return a list of multiplayer games
   const qs = user._id.toString();
   const [compare, setCompare] = useState<User>();
+  const [page, setPage] = useState<number>(0);
+  const offset = page * 5;
   const { data: multiplayerGames } = useSWRHelper<MultiplayerMatch[]>(
-    '/api/match/search?limit=100&players=' + qs + (compare ? ',' + compare._id.toString() : ''),
+    '/api/match/search?limit=5&offset=' +
+      offset +
+      '&players=' +
+      qs +
+      (compare ? ',' + compare._id.toString() : '')
   );
   const { data: records } = useSWRHelper<MultiplayerRecords>(
-    '/api/match/record?' + (compare && compare._id.toString().length > 0 ? 'compareUser=' + compare._id.toString() + '&' : '') + 'player=' + qs,
+    '/api/match/record?' +
+      (compare && compare._id.toString().length > 0
+        ? 'compareUser=' + compare._id.toString() + '&'
+        : '') +
+      'player=' +
+      qs
   );
 
-  if (!multiplayerGames) {
-    return (
-      <div className='text-center'>
-        <span>Loading...</span>
-      </div>
-    );
+  let displayGames = [
+    <div key='loadingDisplay' className='text-center'>
+      <span>Loading...</span>
+    </div>,
+  ];
+
+  if (multiplayerGames) {
+    displayGames = [
+      <>
+        {multiplayerGames.map((game) => (
+          <MatchResults
+            showViewLink={true}
+            match={game}
+            key={game._id.toString()}
+          />
+        ))}
+
+        <div className='flex flex-row gap-4 text-center justify-center items-center'>
+          <button
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+            id='prevPage'
+            style={{
+              visibility: page === 0 ? 'hidden' : 'visible',
+            }}
+            onClick={() => setPage(page - 1)}
+          >
+              Prev
+          </button>
+
+          <button
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+            id='nextPage'
+            style={{
+              visibility: multiplayerGames.length < 5 ? 'hidden' : 'visible',
+            }}
+            onClick={() => setPage(page + 1)}
+          >
+              Next
+          </button>
+        </div>
+
+      </>,
+    ];
   }
 
   return (
@@ -47,9 +95,12 @@ export default function ProfileMultiplayer({ user }: ProfileMultiplayerProps) {
         {user.name}&apos;s Multiplayer History
       </h2>
       <div className='flex flex-row gap-2  text-center justify-center items-center'>
-        <MultiSelectUser defaultValue={compare} onSelect={(selected: User) => {
-          setCompare(selected);
-        }} />
+        <MultiSelectUser
+          defaultValue={compare}
+          onSelect={(selected: User) => {
+            setCompare(selected);
+          }}
+        />
         <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
           <Link href='/multiplayer'>Multiplayer Lobby</Link>
         </button>
@@ -78,13 +129,7 @@ export default function ProfileMultiplayer({ user }: ProfileMultiplayerProps) {
           />
         </div>
         <div className='flex flex-col gap-2 text-center justify-center items-center'>
-          {multiplayerGames.map((game) => (
-            <MatchResults
-              showViewLink={true}
-              match={game}
-              key={game._id.toString()}
-            />
-          ))}
+          {displayGames}
         </div>
       </div>
     </div>
