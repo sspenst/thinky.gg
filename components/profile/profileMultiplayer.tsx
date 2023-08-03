8d60f0ec5041ea8714e1d5bbbb77982a4f8041bd
Danny Miller <k2xl.com@gmail.com>
2023-08-02 22:18:32 -0400

    win loss draw filter

--- components/profile/profileMultiplayer.tsx
+++ components/profile/profileMultiplayer.tsx
@@ -4,6 +4,7 @@ import User, { UserWithMultiplayerProfile } from '@root/models/db/user';
 import { MultiplayerMatchType } from '@root/models/MultiplayerEnums';
 import Link from 'next/link';
 import React, { useState } from 'react';
+import FilterButton from '../filterButton';
 import MatchResults from '../matchResults';
 import MultiplayerRating from '../multiplayerRating';
 import MultiSelectUser from '../multiSelectUser';
@@ -21,15 +22,22 @@ export interface MultiplayerRecord {
 export type MultiplayerRecords = {
   [T in MultiplayerMatchType]: MultiplayerRecord;
 };
+export enum MultiplayerMatchHistoryFilters {
+  All = 'all',
+  Wins = 'wins',
+  Losses = 'losses',
+  Draws = 'draws',
+}
 
 export default function ProfileMultiplayer({ user }: ProfileMultiplayerProps) {
   // return a list of multiplayer games
   const qs = user._id.toString();
   const [compare, setCompare] = useState<User>();
   const [page, setPage] = useState<number>(0);
+  const [filter, setFilter] = useState<MultiplayerMatchHistoryFilters>(MultiplayerMatchHistoryFilters.All);
   const offset = page * 5;
   const { data: multiplayerGames } = useSWRHelper<MultiplayerMatch[]>(
-    '/api/match/search?limit=5&offset=' +
+    '/api/match/search?filter=' + filter + '&limit=5&offset=' +
       offset +
       '&players=' +
       qs +
@@ -52,7 +60,7 @@ export default function ProfileMultiplayer({ user }: ProfileMultiplayerProps) {
 
   if (multiplayerGames) {
     displayGames = [
-      <>
+      <div key='displayGames' className='flex flex-col gap-2'>
         {multiplayerGames.map((game) => (
           <MatchResults
             showViewLink={true}
@@ -85,7 +93,7 @@ export default function ProfileMultiplayer({ user }: ProfileMultiplayerProps) {
           </button>
         </div>
 
-      </>,
+      </div>,
     ];
   }
 
@@ -128,6 +136,23 @@ export default function ProfileMultiplayer({ user }: ProfileMultiplayerProps) {
             type={MultiplayerMatchType.RushClassical}
           />
         </div>
+        <div className='flex flex-row gap-2 text-center justify-center items-center'>
+          <FilterButton selected={filter === MultiplayerMatchHistoryFilters.Wins} value='Wins' first last onClick={() => {
+            if (filter === MultiplayerMatchHistoryFilters.Wins) setFilter(MultiplayerMatchHistoryFilters.All);
+            else setFilter(MultiplayerMatchHistoryFilters.Wins);
+          }} element={<span className='text-sm'>Wins</span>} />
+          <FilterButton selected={filter === MultiplayerMatchHistoryFilters.Losses} value='Losses' first last onClick={() => {
+            if (filter === MultiplayerMatchHistoryFilters.Losses) setFilter(MultiplayerMatchHistoryFilters.All);
+            else setFilter(MultiplayerMatchHistoryFilters.Losses);
+          }
+          } element={<span className='text-sm'>Losses</span>} />
+          <FilterButton selected={filter === MultiplayerMatchHistoryFilters.Draws} value='Draws' first last onClick={() => {
+            if (filter === MultiplayerMatchHistoryFilters.Draws) setFilter(MultiplayerMatchHistoryFilters.All);
+            else setFilter(MultiplayerMatchHistoryFilters.Draws);
+          }
+          } element={<span className='text-sm'>Draws</span>} />
+        </div>
+        <div />
         <div className='flex flex-col gap-2 text-center justify-center items-center'>
           {displayGames}
         </div>
