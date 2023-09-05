57e2267c3f51a59d6249f7f8413bf9c30152b796
Danny Miller <k2xl.com@gmail.com>
2023-09-05 16:28:38 -0400

    achievements page formatting

--- components/profile/profileAchievements.tsx
+++ components/profile/profileAchievements.tsx
@@ -1,54 +1,46 @@
-import { AchievementRulesCombined, AchievementRulesTableCreator, AchievementRulesTableLevelCompletion, AchievementRulesTableUser } from '@root/constants/achievementInfo';
+import { AchievementRulesTableCreator, AchievementRulesTableLevelCompletion, AchievementRulesTableUser, IAchievementInfo } from '@root/constants/achievementInfo';
 import Achievement from '@root/models/db/achievement';
 import React from 'react';
 import FormattedAchievement from '../formatted/formattedAchievement';
 
 export function ProfileAchievments({ achievements }: { achievements: Achievement[] }) {
-  return (<div className='flex flex-row gap-2 justify-center'>
-    <div className='flex flex-col gap-1'>
-      <h1 className='text-2xl font-bold text-center'>Creator</h1>
-      <div className='flex flex-col justify-center text-center p-1' >
-        {Object.keys(AchievementRulesTableCreator).map(achievementType => {
-          const achievement = achievements.find(achievement => achievement.type === achievementType);
+  function getAchievementsOfType(rule: any) {
+    return Object.keys(rule).map(achievementType => {
+      const achievement = achievements.find(achievement => achievement.type === achievementType);
 
-          if (!achievement) {
-            return null;
-          }
+      if (!achievement) {
+        return null;
+      }
 
-          return <div key={`achievement-${achievement._id}`} className='p-3'><FormattedAchievement achievement={achievement} /></div>;
-        })}
-      </div>
-    </div>
-    <div className='flex flex-col gap-1'>
-      <h1 className='text-2xl font-bold text-center'>Progress</h1>
-      <div className='flex flex-col justify-center text-center' >
-        {Object.keys(AchievementRulesTableUser).map(achievementType => {
-          const achievement = achievements.find(achievement => achievement.type === achievementType);
+      return <div key={`achievement-${achievement._id}`} className='p-3'><FormattedAchievement achievement={achievement} /></div>;
+    }).filter(achievement => achievement !== null);
+  }
 
-          if (!achievement) {
-            return null;
-          }
+  const creatorAchievements = getAchievementsOfType(AchievementRulesTableCreator);
+  const progressAchievements = getAchievementsOfType(AchievementRulesTableLevelCompletion);
+  const skillAchievements = getAchievementsOfType(AchievementRulesTableUser);
 
-          return <div key={`achievement-${achievement._id}`} className='p-3'><FormattedAchievement achievement={achievement} /></div>;
-        })}
-      </div>
-    </div>
-
+  return (<div className='flex flex-row gap-8 justify-center'>
     <div className='flex flex-col gap-1'>
       <h1 className='text-2xl font-bold text-center'>Skill</h1>
       <div className='flex flex-col justify-center text-center' >
-        {Object.keys(AchievementRulesTableLevelCompletion).map(achievementType => {
-          const achievement = achievements.find(achievement => achievement.type === achievementType);
+        {skillAchievements.length > 0 ? skillAchievements : <span className='text-center'>No achievements yet</span>}
+      </div>
+    </div>
+    <div className='flex flex-col gap-1'>
+      <h1 className='text-2xl font-bold text-center'>Creator</h1>
+      <div className='flex flex-col justify-center text-center' >
 
-          if (!achievement) {
-            return null;
-          }
-
-          return <div key={`achievement-${achievement._id}`} className='p-3'><FormattedAchievement achievement={achievement} /></div>;
-        })}
+        {creatorAchievements.length > 0 ? creatorAchievements : <span className='text-center'>No achievements yet</span>}
       </div>
     </div>
 
+    <div className='flex flex-col gap-1'>
+      <h1 className='text-2xl font-bold text-center'>Progress</h1>
+      <div className='flex flex-col justify-center text-center' >
+        {progressAchievements.length > 0 ? progressAchievements : <span className='text-center'>No achievements yet</span>}
+      </div>
+    </div>
   </div>
 
   );
