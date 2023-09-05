c7a06a6b47af3e59bdccefdda53ea7b1617b2289
Spencer Spenst <spencerspenst@gmail.com>
2023-09-05 15:56:04 -0700

    admin tweaks

--- pages/api/admin/index.ts
+++ pages/api/admin/index.ts
@@ -9,10 +9,11 @@ import { NextApiResponse } from 'next';
 import { refreshAchievements } from '../stats';
 
 enum AdminCommand {
-    refreshAchievements = 'refreshAchievements',
-    refreshIndexCalcs = 'refreshIndexCalcs',
-    refreshPlayAttempts = 'calcPlayAttempts',
+  refreshAchievements = 'refreshAchievements',
+  refreshIndexCalcs = 'refreshIndexCalcs',
+  refreshPlayAttempts = 'calcPlayAttempts',
 }
+
 export default withAuth({ POST: {
   body: {
     targetId: ValidObjectId(),
@@ -27,27 +28,21 @@ export default withAuth({ POST: {
 
   const { targetId, command } = req.body;
 
-  try {
-    switch (command) {
-    case AdminCommand.refreshAchievements:
-      //clear achievements first to make this idempotent
-      await AchievementModel.deleteMany({ userId: targetId });
-      await refreshAchievements(new Types.ObjectId(targetId as string), Object.values(AchievementCategory));
-      break;
-    case AdminCommand.refreshIndexCalcs:
-      await refreshIndexCalcs(new Types.ObjectId(targetId as string));
-      break;
-    case AdminCommand.refreshPlayAttempts:
-      await calcPlayAttempts(new Types.ObjectId(targetId as string));
-      break;
-    default:
-      return res.status(400).json({
-        error: command + ' is an invalid command'
-      });
-    }
-  } catch (e: any) {
-    return res.status(500).json({
-      error: e.message
+  switch (command) {
+  case AdminCommand.refreshAchievements:
+    // clear achievements first to make this idempotent
+    await AchievementModel.deleteMany({ userId: targetId });
+    await refreshAchievements(new Types.ObjectId(targetId as string), Object.values(AchievementCategory));
+    break;
+  case AdminCommand.refreshIndexCalcs:
+    await refreshIndexCalcs(new Types.ObjectId(targetId as string));
+    break;
+  case AdminCommand.refreshPlayAttempts:
+    await calcPlayAttempts(new Types.ObjectId(targetId as string));
+    break;
+  default:
+    return res.status(400).json({
+      error: command + ' is an invalid command'
     });
   }
 
