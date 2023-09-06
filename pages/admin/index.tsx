f6bb9b503a4e4d53e52fb96a8d33db0d078a36fb
Danny Miller <k2xl.com@gmail.com>
2023-09-05 21:59:39 -0400

    delete achievements as separate command in admin

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -28,76 +28,83 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 }
 
 export default function AdminPage() {
-  const [selectedCommand, setSelectedCommand] = useState('Select Command');
+  const [selectedCommand, setSelectedCommand] = useState<{ label: string; command: string; confirm?: boolean } | null>(null);
   const [selectedUser, setSelectedUser] = useState<User>();
   const [runningCommand, setRunningCommand] = useState(false);
+
+  const commands = [
+    { label: 'Refresh Achievements', command: 'refreshAchievements' },
+    { label: 'Delete Achievements', command: 'deleteAchievements', confirm: true },
+    { label: 'Refresh Play Attempts', command: 'calcPlayAttempts' },
+    { label: 'Refresh Index Calculations', command: 'refreshIndexCalcs' },
+
+    // Add more commands here
+  ];
+
+  async function runCommand() {
+    if (selectedCommand?.confirm && !window.confirm('Are you sure you want to proceed?')) return;
+
+    setRunningCommand(true);
+    toast.dismiss();
+    toast.loading('Running command...');
+    const resp = await fetch('/api/admin', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        command: selectedCommand?.command,
+        targetId: selectedUser?._id,
+      }),
+    });
+
+    setRunningCommand(false);
+    const json = await resp.json();
+
+    if (json.error) {
+      toast.error(json.error);
+    } else {
+      toast.success('Command ran successfully');
+    }
+  }
 
   return (
     <Page title='Admin Page'>
       <div className='p-2'>
-        <h1 className='flex flex-col items-center justify-center  text-2xl'>Admin Page</h1>
+        <h1 className='flex flex-col items-center justify-center text-2xl'>Admin Page</h1>
         <div className='flex flex-row items-center justify-center p-2 gap-2'>
           <p className='text-xl'>Run command on user:</p>
           <MultiSelectUser defaultValue={selectedUser} onSelect={(selected: User) => setSelectedUser(selected)} />
           <Menu as='div' className='relative inline-block text-left'>
             <div>
               <Menu.Button className='border border-gray-300 bg-gray rounded-md shadow-sm px-4 py-2 text-sm flex flex-row items-center justify-center gap-2'>
-                {selectedCommand}
+                {selectedCommand?.label || 'Select Command'}
               </Menu.Button>
             </div>
             <Menu.Items className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5'>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    onClick={() => setSelectedCommand('refreshAchievements')}
-                    className={`${active ? 'bg-blue-600 text-white' : 'text-gray-900'} block px-4 py-2 text-sm`}
-                  >
-                    refreshAchievements
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    onClick={() => setSelectedCommand('calcPlayAttempts')}
-                    className={`${active ? 'bg-blue-600 text-white' : 'text-gray-900'} block px-4 py-2 text-sm`}
-                  >
-                    refreshPlayAttempts
-                  </a>
-                )}
-              </Menu.Item>
+              {commands.map((cmd) => (
+                <Menu.Item key={cmd.command}>
+                  {({ active }) => (
+                    <a
+                      onClick={() => setSelectedCommand(cmd)}
+                      className={`${active ? 'bg-blue-600 text-white' : 'text-gray-900'} block px-4 py-2 text-sm`}
+                    >
+                      {cmd.label}
+                    </a>
+                  )}
+                </Menu.Item>
+              ))}
             </Menu.Items>
           </Menu>
-          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+          <button
+            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${runningCommand ? 'bg-gray-500 cursor-not-allowed' : ''}`}
             disabled={runningCommand}
-            onClick={async () => {
-              setRunningCommand(true);
-              toast.dismiss();
-              toast.loading('Running command...');
-              const resp = await fetch('/api/admin', {
-                method: 'POST',
-                credentials: 'include',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                  command: selectedCommand,
-                  targetId: selectedUser?._id
-                }),
-              });
-
-              setRunningCommand(false);
-              const json = await resp.json();
-
-              if (json.error) {
-                toast.error(json.error);
-              } else {
-                toast.success('Command ran successfully');
-              }
-            }}
+            onClick={runCommand}
           >
-            Run
+  Run
           </button>
+
         </div>
         <div className='flex flex-row items-center justify-center p-2 gap-2'>
           {selectedUser && <FormattedUser user={selectedUser} />}
