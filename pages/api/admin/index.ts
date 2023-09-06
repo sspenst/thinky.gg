8dcee3a2779c324b43c0a4840621b752f24b260b
Danny Miller <k2xl.com@gmail.com>
2023-09-05 20:56:30 -0400

    fix test

--- pages/api/admin/index.ts
+++ pages/api/admin/index.ts
@@ -9,11 +9,10 @@ import { NextApiResponse } from 'next';
 import { refreshAchievements } from '../stats';
 
 enum AdminCommand {
-  refreshAchievements = 'refreshAchievements',
-  refreshIndexCalcs = 'refreshIndexCalcs',
-  refreshPlayAttempts = 'calcPlayAttempts',
+    refreshAchievements = 'refreshAchievements',
+    refreshIndexCalcs = 'refreshIndexCalcs',
+    refreshPlayAttempts = 'calcPlayAttempts',
 }
-
 export default withAuth({ POST: {
   body: {
     targetId: ValidObjectId(),
@@ -28,21 +27,25 @@ export default withAuth({ POST: {
 
   const { targetId, command } = req.body;
 
-  switch (command) {
-  case AdminCommand.refreshAchievements:
-    // clear achievements first to make this idempotent
-    await AchievementModel.deleteMany({ userId: targetId });
-    await refreshAchievements(new Types.ObjectId(targetId as string), Object.values(AchievementCategory));
-    break;
-  case AdminCommand.refreshIndexCalcs:
-    await refreshIndexCalcs(new Types.ObjectId(targetId as string));
-    break;
-  case AdminCommand.refreshPlayAttempts:
-    await calcPlayAttempts(new Types.ObjectId(targetId as string));
-    break;
-  default:
-    return res.status(400).json({
-      error: command + ' is an invalid command'
+  try {
+    switch (command) {
+    case AdminCommand.refreshAchievements:
+      await refreshAchievements(new Types.ObjectId(targetId as string), Object.values(AchievementCategory));
+      break;
+    case AdminCommand.refreshIndexCalcs:
+      await refreshIndexCalcs(new Types.ObjectId(targetId as string));
+      break;
+    case AdminCommand.refreshPlayAttempts:
+      await calcPlayAttempts(new Types.ObjectId(targetId as string));
+      break;
+    default:
+      return res.status(400).json({
+        error: command + ' is an invalid command'
+      });
+    }
+  } catch (e: any) {
+    return res.status(500).json({
+      error: e.message
     });
   }
 
