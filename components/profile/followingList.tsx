c2200b4bba16fa26abe20b67bbaef6b35f9c0618
Danny Miller <k2xl.com@gmail.com>
2023-09-06 22:41:54 -0400

    trying to get following list in a data table... works but doesnt paginate

--- components/profile/followingList.tsx
+++ components/profile/followingList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import User from '../../models/db/user';
 import FollowButton from '../buttons/followButton';
 import FormattedUser from '../formatted/formattedUser';
+import DataTable from '../tables/dataTable';
 
 interface FollowingListProps {
   users: User[];
@@ -10,16 +11,32 @@ interface FollowingListProps {
 export default function FollowingList({ users }: FollowingListProps) {
   return (
     <div className='flex flex-col items-center justify-center max-w-md'>
-      <div>
-        {users.map((user) => (
-          <div className='grid gap-6 p-2' key={'following-list-row-' + user._id} style={{
-            gridTemplateColumns: '1fr auto'
-          }}>
-            <FormattedUser user={user} />
-            <FollowButton isFollowing={true} user={user} />
-          </div>
-        ))}
-      </div>
+      <DataTable
+        onChangePage={() => {}}
+        onSort={() => {}}
+        page={1}
+        sortBy='user'
+        sortDir='asc'
+        totalItems={users.length}
+
+        columns={[
+          {
+            id: 'user',
+            name: 'User',
+            selector: (row) => <FormattedUser user={row as User} />,
+            sortable: false,
+          },
+          {
+            id: 'follow',
+            name: '',
+            selector: (row) => <FollowButton isFollowing={true} user={row as User} />,
+            sortable: false,
+          },
+        ]}
+        itemsPerPage={10} // TODO - this doesn't seem to work... this data table component seems like it only works with server...
+        data={users}
+        noDataComponent={<div className='text-gray-500 dark:text-gray-400'>No users to show</div>}
+      />
     </div>
   );
 }
