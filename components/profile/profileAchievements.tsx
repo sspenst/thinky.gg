137b9405d63f49ec16d49c790bef643692356e65
Danny Miller <k2xl.com@gmail.com>
2023-09-08 10:38:34 -0400

    little bit of padding

--- components/profile/profileAchievements.tsx
+++ components/profile/profileAchievements.tsx
@@ -32,10 +32,10 @@ export function ProfileAchievments({ achievements }: { achievements: Achievement
   } as { [key: string]: JSX.Element[] };
 
   return (
-    <div className='flex md:flex-row md:items-start flex-col gap-6 items-center'>
+    <div className='flex md:flex-row md:items-start flex-col gap-6 items-center p-3'>
       {Object.keys(achievementsByCategory).map((achievementCategory) => (
         <div className='flex flex-col gap-4 w-96 max-w-full' key={achievementCategory}>
-          <h1 className='text-2xl font-medium'>{achievementCategory}</h1>
+          <h1 className='text-2xl font-medium '>{achievementCategory}</h1>
           {achievementsByCategory[achievementCategory]}
         </div>
       ))}
