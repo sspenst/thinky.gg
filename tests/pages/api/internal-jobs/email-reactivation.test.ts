ed9de17f774614bbe7ecde3f60a1cb319a7bfee8
Danny Miller <k2xl.com@gmail.com>
2023-09-18 21:06:28 -0400

    cleanup

--- tests/pages/api/internal-jobs/email-reactivation.test.ts
+++ tests/pages/api/internal-jobs/email-reactivation.test.ts
@@ -132,8 +132,6 @@ describe('Email reactivation', () => {
     });
   }, 10000);
   test('Running it again right away should not send any since it has been less than 90 days (idempotency)', async () => {
-    // setup
-    //sendMailRefMock.ref = throwMock;
     jest.spyOn(logger, 'error').mockImplementation(() => ({} as Logger));
     jest.spyOn(logger, 'info').mockImplementation(() => ({} as Logger));
     jest.spyOn(logger, 'warn').mockImplementation(() => ({} as Logger));
