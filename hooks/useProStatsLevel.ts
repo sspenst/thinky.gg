5b7624a93ed4c47d58c0b60556eeb6b7ac2b4c7c
Danny Miller <k2xl.com@gmail.com>
2023-09-25 14:56:39 -0400

    fix pro stats 401 for non logged in users

--- hooks/useProStatsLevel.ts
+++ hooks/useProStatsLevel.ts
@@ -2,8 +2,8 @@ import { ProStatsLevel as ProStatsLevel } from '../contexts/levelContext';
 import { EnrichedLevel } from '../models/db/level';
 import useSWRHelper from './useSWRHelper';
 
-export default function useProStatsLevel(level: EnrichedLevel, disable: boolean = false) {
-  const { data, error, isLoading, mutate } = useSWRHelper<ProStatsLevel>(`/api/level/${level._id.toString()}/prostats`, undefined, undefined, disable);
+export default function useProStatsLevel(level: EnrichedLevel) {
+  const { data, error, isLoading, mutate } = useSWRHelper<ProStatsLevel>(`/api/level/${level._id.toString()}/prostats`, undefined, undefined);
 
   return {
     error,
