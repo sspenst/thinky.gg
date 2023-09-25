e00d055be8b3a2571af7be5a5f67304a2ec23432
Spencer Spenst <spencerspenst@gmail.com>
2023-09-25 15:11:52 -0700

    fix stripe bugs for invoice.payment_failed

--- pages/api/stripe-webhook/index.ts
+++ pages/api/stripe-webhook/index.ts
@@ -60,13 +60,13 @@ async function subscriptionDeleted(userToDowngrade: User) {
   }
 }
 
-async function checkoutSessionComplete(userToUpgrade: User, properties: Stripe.Checkout.Session) {
+async function checkoutSessionComplete(userToUpgrade: User, properties: Stripe.Checkout.Session): Promise<string | undefined> {
   logger.info(`checkoutSessionComplete - ${userToUpgrade.name} (${userToUpgrade._id.toString()})`);
 
   const customerId = properties.customer;
 
   // otherwise... let's upgrade the user?
-  let error;
+  let error: string | undefined;
 
   // if the user is already a pro subscriber, we don't want to do anything
   if (isPro(userToUpgrade)) {
@@ -119,7 +119,7 @@ async function checkoutSessionComplete(userToUpgrade: User, properties: Stripe.C
   return error;
 }
 
-async function splitPaymentIntent(paymentIntentId: string) {
+async function splitPaymentIntent(paymentIntentId: string): Promise<string | undefined> {
   // https://stripe.com/docs/expand/use-cases#stripe-fee-for-payment
   const paymentIntent = await stripe.paymentIntents.retrieve(
     paymentIntentId,
@@ -232,7 +232,14 @@ export default apiWrapper({
       ]);
 
       if (userConfigAgg.length === 0) {
-        error = `UserConfig with customer id ${customerId} does not exist`;
+        if (event.type === 'customer.subscription.deleted') {
+          // there must be a matching userconfig in this case, so we need to return an error here
+          error = `${event.type} - UserConfig with customer id ${customerId} does not exist`;
+        } else {
+          // failed payments without a userconfig imply the user's payment has failed before subscribing
+          // this is a valid case and should not cause an error
+          logger.info(`${event.type} - UserConfig with customer id ${customerId} does not exist`);
+        }
       } else {
         error = await subscriptionDeleted(userConfigAgg[0].userId as User);
       }
