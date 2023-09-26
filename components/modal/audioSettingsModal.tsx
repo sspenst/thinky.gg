b8f87bf133071ff15d782606f15761635245e0d6
Danny Miller <k2xl.com@gmail.com>
2023-09-25 22:48:17 -0400

    more tweaks

--- components/modal/audioSettingsModal.tsx
+++ components/modal/audioSettingsModal.tsx
@@ -17,9 +17,30 @@ interface AudioSettingsModalProps {
 export default function AudioSettingsModal({ closeModal, isOpen }: AudioSettingsModalProps) {
   return (
     <Modal title='Audio Settings' isOpen={isOpen} closeModal={closeModal}>
-      <div>
-        <h1>Audio Settings</h1>
-        <AudioPlayer hideSettingsButton={true} />
+      <div className='flex flex-col gap-2'>
+
+        <div className='flex flex-row gap-2'>
+          <div className='flex flex-row gap-2'>
+            <div>Dynamic Music</div>
+            <div>
+              <input type='checkbox' />
+            </div>
+          </div>
+          <div className='flex flex-col word-wrap gap-2 text-sm'>
+            <span>Changes the music depending on what is happening in the game.</span>
+            <span className='text-xs'>For example, if you are in a puzzle, the music will be more ambient.<br />After solving, the music will transition to a more energetic track.</span>
+          </div>
+        </div>
+
+        <div className='flex flex-row gap-2'>
+          <div>Music Volume</div>
+          <div>
+            <input type='range' />
+          </div>
+        </div>
+        <div className='flex flex-row items-center justify-center'>
+          <AudioPlayer hideSettingsButton={true} />
+        </div>
       </div>
     </Modal>
   );
