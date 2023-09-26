475cc8bc3d292a28d3716eae3e262fce614748a8
Danny Miller <k2xl.com@gmail.com>
2023-09-26 10:24:34 -0400

    local storage now works for music settings

--- contexts/audioPlayerContext.tsx
+++ contexts/audioPlayerContext.tsx
@@ -1,11 +1,12 @@
-import React, { createContext, useContext, useState } from 'react';
+import { SongMetaData } from '@root/components/audioPlayer/audioPlayer';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
 export interface AudioPlayerState {
   audioActive?: HTMLAudioElement;
   audioAmbient?: HTMLAudioElement;
   audioContext?: AudioContext;
   currentSongIndex: number;
-  currentTitle: string;
+  currentMetaData: SongMetaData;
   dynamicMusic: boolean;
   isHot: boolean;
   isPlaying: boolean;
@@ -14,7 +15,7 @@ export interface AudioPlayerState {
   setAudioAmbient: (audioAmbient: HTMLAudioElement) => void;
   setAudioContext: (audioContext: AudioContext) => void;
   setCurrentSongIndex: (index: number) => void;
-  setCurrentTitle: (title: string) => void;
+  setCurrentMetaData: (metaData: SongMetaData) => void;
   setDynamicMusic: (dynamicMusic: boolean) => void;
   setIsHot: (isHot: boolean) => void;
   setIsPlaying: (isPlaying: boolean) => void;
@@ -27,7 +28,7 @@ export const AudioPlayerContext = createContext<AudioPlayerState>({
   audioAmbient: undefined,
   audioContext: undefined,
   currentSongIndex: 0,
-  currentTitle: '',
+  currentMetaData: { title: '', artist: '', active: '', ambient: '', website: '' },
   dynamicMusic: false,
   isHot: false,
   isPlaying: false,
@@ -36,7 +37,7 @@ export const AudioPlayerContext = createContext<AudioPlayerState>({
   setAudioAmbient: (audioAmbient: HTMLAudioElement) => {},
   setAudioContext: (audioContext: AudioContext) => {},
   setCurrentSongIndex: (index: number) => {},
-  setCurrentTitle: (title: string) => {},
+  setCurrentMetaData: (metaData: SongMetaData) => {},
   setDynamicMusic: (dynamicMusic: boolean) => {},
   setIsHot: (isHot: boolean) => {},
   setIsPlaying: (isPlaying: boolean) => {},
@@ -48,11 +49,32 @@ export function AudioPlayerContextProvider({ children }: { children: React.React
   const [audioAmbient, setAudioAmbient] = useState<HTMLAudioElement>();
   const [audioContext, setAudioContext] = useState<AudioContext>();
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
-  const [currentTitle, setCurrentTitle] = useState<string>();
+  const [currentMetaData, setCurrentMetaData] = useState<SongMetaData>({ title: '', artist: '', active: '', ambient: '', website: '' });
   const [dynamicMusic, setDynamicMusic] = useState(true);
   const [isHot, setIsHot] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
   const [maxVolume, setMaxVolume] = useState(1);
+
+  useEffect(() => {
+    // load from local storage the currentSongIndex, isHot, dynamicMusic, maxVolume and load it into state
+    const LS_currentSongIndex = localStorage.getItem('audio.currentSongIndex');
+    const LS_isHot = localStorage.getItem('audio.isHot');
+    const LS_dynamicMusic = localStorage.getItem('audio.dynamicMusic');
+    const LS_maxVolume = localStorage.getItem('audio.maxVolume');
+
+    LS_currentSongIndex && setCurrentSongIndex(parseInt(LS_currentSongIndex));
+    LS_isHot && setIsHot(LS_isHot === 'true');
+    LS_dynamicMusic && setDynamicMusic(LS_dynamicMusic === 'true');
+    LS_maxVolume && setMaxVolume(parseFloat(LS_maxVolume));
+  }, []);
+
+  // now a use effect to save the currentSongIndex, isHot, dynamicMusic, maxVolume to local storage
+  useEffect(() => {
+    localStorage.setItem('audio.currentSongIndex', currentSongIndex.toString());
+    localStorage.setItem('audio.isHot', isHot.toString());
+    localStorage.setItem('audio.dynamicMusic', dynamicMusic.toString());
+    localStorage.setItem('audio.maxVolume', maxVolume.toString());
+  }, [currentSongIndex, isHot, dynamicMusic, maxVolume]);
 
   return (
     <AudioPlayerContext.Provider value={{
@@ -60,13 +82,13 @@ export function AudioPlayerContextProvider({ children }: { children: React.React
       audioAmbient: audioAmbient,
       audioContext: audioContext,
       currentSongIndex: currentSongIndex,
-      currentTitle: currentTitle || '',
+      currentMetaData: currentMetaData,
       dynamicMusic: dynamicMusic,
       isHot: isHot,
       isPlaying: isPlaying,
       maxVolume: maxVolume,
       setCurrentSongIndex: setCurrentSongIndex,
-      setCurrentTitle: setCurrentTitle,
+      setCurrentMetaData: setCurrentMetaData,
       setDynamicMusic: setDynamicMusic,
       setIsHot: setIsHot,
       setIsPlaying: setIsPlaying,
