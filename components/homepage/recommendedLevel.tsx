7060c2313b24f9067383e37858cf6287f739d48d
Danny Miller <k2xl.com@gmail.com>
2023-09-26 21:16:13 -0400

    homepage tweaks

--- components/homepage/recommendedLevel.tsx
+++ components/homepage/recommendedLevel.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { renderToStaticMarkup } from 'react-dom/server';
 import Dimensions from '../../constants/dimensions';
 import { EnrichedLevel } from '../../models/db/level';
 import SelectOption from '../../models/selectOption';
 import SelectOptionStats from '../../models/selectOptionStats';
 import LoadingCard from '../cards/loadingCard';
 import SelectCard from '../cards/selectCard';
+import StyledTooltip from '../page/styledTooltip';
 
 interface RecommendedLevelProps {
   id?: string;
   level?: EnrichedLevel | null;
-  title: string
+  title: JSX.Element | string;
+  tooltip?: string;
 }
 
-export default function RecommendedLevel({ id, level, title }: RecommendedLevelProps): JSX.Element {
+export default function RecommendedLevel({ id, level, title, tooltip }: RecommendedLevelProps): JSX.Element {
   return (
     <div className='flex flex-col justify-center rounded-lg border'
       id={id}
@@ -22,7 +25,8 @@ export default function RecommendedLevel({ id, level, title }: RecommendedLevelP
       }}
     >
       <h2 className='self-center px-4 pt-3 text-lg font-bold'>
-        {title}
+        {tooltip && <span className='w-20 text-center hover:underline hover:decoration-dashed cursor-help' data-tooltip-id={id + '-tooltip'} data-tooltip-content={tooltip}>{title}</span>}
+        <StyledTooltip id={id + '-tooltip'} />
       </h2>
       {level === undefined ? <LoadingCard /> :
         !level ?
