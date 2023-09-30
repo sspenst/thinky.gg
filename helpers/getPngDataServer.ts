f27f8e16a6c5d086078ffcd7f1f43ff303c9b48d
k2xl <k2xl.com@gmail.com>
2023-09-30 01:24:04 -0400

    [Snyk] Upgrade pureimage from 0.3.17 to 0.4.7 (#977)
    * fix: upgrade pureimage from 0.3.17 to 0.4.7
    
    Snyk has created this PR to upgrade pureimage from 0.3.17 to 0.4.7.
    
    See this package in npm:
    https://www.npmjs.com/package/pureimage
    
    See this project in Snyk:
    https://app.snyk.io/org/k2xl/project/cf09253a-8e4c-4f7f-b75f-a1763e5c78eb?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * pureimage type declaration
    
    * fix
    
    ---------
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    Co-authored-by: Spencer Spenst <spencerspenst@gmail.com>

--- helpers/getPngDataServer.ts
+++ helpers/getPngDataServer.ts
@@ -1,5 +1,4 @@
 import * as PImage from 'pureimage';
-import { Bitmap } from 'pureimage/types/bitmap';
 import { PassThrough } from 'stream';
 import Dimensions from '../constants/dimensions';
 import Level from '../models/db/level';
@@ -8,7 +7,7 @@ import generateLevelCanvas from './generateLevelCanvas';
 export default async function getPngDataServer(level: Level): Promise<Buffer> {
   let canvas = PImage.make(Dimensions.LevelCanvasWidth, Dimensions.LevelCanvasHeight, {});
 
-  canvas = generateLevelCanvas(canvas, level.data) as Bitmap;
+  canvas = generateLevelCanvas(canvas, level.data) as PImage.Bitmap;
 
   const stream = new PassThrough();
   const chunks: Uint8Array[] = [];
