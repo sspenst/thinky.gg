a27f672788114486382a4231921f80dfe6a1e607
Danny Miller <k2xl.com@gmail.com>
2023-10-03 11:36:27 -0400

    also do ogurl just in case

--- pages/level/[username]/[slugName].tsx
+++ pages/level/[username]/[slugName].tsx
@@ -187,7 +187,7 @@ export default function LevelPage({ _level, reqUser }: LevelProps) {
   // subtitle is only useful when a level is within a collection created by a different user
   const showSubtitle = collection && (collection.userId._id !== level.userId._id);
   const ogImageUrl = `https://pathology.gg/api/level/image/${level._id.toString()}.png${ts ? `?ts=${ts}` : ''}`;
-  const ogUrl = `/level/${level.slug}`;
+  const ogUrl = `https://pathology.gg/level/${level.slug}`;
   const ogFullUrl = `https://pathology.gg${ogUrl}`;
   const authorNote = level.authorNote ? level.authorNote : `${level.name} by ${level.userId.name}`;
 
