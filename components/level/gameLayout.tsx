31786b4ec89969b1dd78dd959d36c5669e388546
Danny Miller <k2xl.com@gmail.com>
2023-10-03 10:38:49 -0400

    mobile buttons update

--- components/level/gameLayout.tsx
+++ components/level/gameLayout.tsx
@@ -53,7 +53,15 @@ export default function GameLayout({ controls, disableCheckpoints, gameState, hi
     <div className='flex flex-row h-full w-full' id='game-layout' style={{
       backgroundColor: 'var(--bg-color)',
     }}>
-      <div className='grow flex flex-col max-w-full'>
+      <div className='grow flex flex-col max-w-full'
+        style={{
+        // don't allow this div to be selectable
+          userSelect: 'none',
+          // seriously don't allow
+          WebkitUserSelect: 'none',
+
+        }}
+      >
         {!matchId && level.userId && !fullScreen && <>
           <div className='flex items-center justify-center py-1 px-2 gap-1 block xl:hidden'>
             <button
