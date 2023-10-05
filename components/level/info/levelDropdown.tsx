9274a836e243bc5e60c36e29c40d5846e05801f3
Spencer Spenst <spencerspenst@gmail.com>
2023-10-04 21:54:50 -0700

    copy level data / link in level dropdown

--- components/level/info/levelDropdown.tsx
+++ components/level/info/levelDropdown.tsx
@@ -8,6 +8,7 @@ import isCurator from '@root/helpers/isCurator';
 import Level from '@root/models/db/level';
 import classNames from 'classnames';
 import React, { Fragment, useContext, useState } from 'react';
+import toast from 'react-hot-toast';
 
 interface LevelDropdownProps {
   level: Level;
@@ -50,26 +51,68 @@ export default function LevelDropdown({ level }: LevelDropdownProps) {
                 <div
                   className='flex w-full items-center rounded-md cursor-pointer px-3 py-2 gap-3 whitespace-nowrap'
                   onClick={() => {
-                    setIsEditLevelOpen(true);
-                    setPreventKeyDownEvent(true);
+                    navigator.clipboard.writeText(window.location.href);
+                    toast.dismiss();
+                    toast.success('Link copied to clipboard');
                   }}
                   style={{
                     backgroundColor: active ? 'var(--bg-color-3)' : undefined,
                   }}
                 >
-                  {canEdit ?
-                    <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-4 h-4' style={{ minWidth: 16, minHeight: 16 }}>
-                      <path strokeLinecap='round' strokeLinejoin='round' d='M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125' />
-                    </svg>
-                    :
-                    <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' height='16' width='16'>
-                      <path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
-                    </svg>
-                  }
-                  {canEdit ? 'Edit' : 'Add to...'}
+                  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-4 h-4'>
+                    <path strokeLinecap='round' strokeLinejoin='round' d='M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244' />
+                  </svg>
+                  Copy link
                 </div>
               )}
             </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <div
+                  className='flex w-full items-center rounded-md cursor-pointer px-3 py-2 gap-3 whitespace-nowrap'
+                  onClick={() => {
+                    navigator.clipboard.writeText(level.data);
+                    toast.dismiss();
+                    toast.success('Level data copied to clipboard');
+                  }}
+                  style={{
+                    backgroundColor: active ? 'var(--bg-color-3)' : undefined,
+                  }}
+                >
+                  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-4 h-4'>
+                    <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z' />
+                  </svg>
+                  Copy level data
+                </div>
+              )}
+            </Menu.Item>
+            {user &&
+              <Menu.Item>
+                {({ active }) => (
+                  <div
+                    className='flex w-full items-center rounded-md cursor-pointer px-3 py-2 gap-3 whitespace-nowrap'
+                    onClick={() => {
+                      setIsEditLevelOpen(true);
+                      setPreventKeyDownEvent(true);
+                    }}
+                    style={{
+                      backgroundColor: active ? 'var(--bg-color-3)' : undefined,
+                    }}
+                  >
+                    {canEdit ?
+                      <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-4 h-4'>
+                        <path strokeLinecap='round' strokeLinejoin='round' d='M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125' />
+                      </svg>
+                      :
+                      <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' height='16' width='16'>
+                        <path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
+                      </svg>
+                    }
+                    {canEdit ? 'Edit' : 'Add to...'}
+                  </div>
+                )}
+              </Menu.Item>
+            }
             {canEdit && <>
               <Menu.Item>
                 {({ active }) => (
@@ -114,14 +157,16 @@ export default function LevelDropdown({ level }: LevelDropdownProps) {
         </Menu.Items>
       </Transition>
     </Menu>
-    <EditLevelModal
-      closeModal={() => {
-        setIsEditLevelOpen(false);
-        setPreventKeyDownEvent(false);
-      }}
-      isOpen={isEditLevelOpen}
-      level={level}
-    />
+    {user &&
+      <EditLevelModal
+        closeModal={() => {
+          setIsEditLevelOpen(false);
+          setPreventKeyDownEvent(false);
+        }}
+        isOpen={isEditLevelOpen}
+        level={level}
+      />
+    }
     {canEdit && <>
       <ArchiveLevelModal
         closeModal={() => {
