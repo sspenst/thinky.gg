c9f7b26d860c882252e37020ca6abdc7cf6141f3
Spencer Spenst <spencerspenst@gmail.com>
2023-10-16 15:33:27 -0700

    header button hover

--- components/header/directory.tsx
+++ components/header/directory.tsx
@@ -40,13 +40,13 @@ export default function Directory({ folders, subtitle, title }: DirectoryProps) 
     {/* back button on mobile */}
     <div className='flex gap-2 items-center xl:hidden'>
       <FolderDivider />
-      <button onClick={router.back}>
-        <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={2} stroke='currentColor' className='w-6 h-6'>
+      <button className='hover:opacity-70 mr-1' onClick={router.back}>
+        <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={2} stroke='currentColor' className='w-5 h-5'>
           <path strokeLinecap='round' strokeLinejoin='round' d='M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18' />
         </svg>
       </button>
-      <button onClick={router.forward}>
-        <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={2} stroke='currentColor' className='w-6 h-6'>
+      <button className='hover:opacity-70' onClick={router.forward}>
+        <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={2} stroke='currentColor' className='w-5 h-5'>
           <path strokeLinecap='round' strokeLinejoin='round' d='M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3' />
         </svg>
       </button>
--- components/header/dropdown.tsx
+++ components/header/dropdown.tsx
@@ -64,7 +64,7 @@ export default function Dropdown() {
         {user ?
           <ProfileAvatar user={user} />
           :
-          <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor' strokeWidth={2}>
+          <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5 hover:opacity-70' fill='none' viewBox='0 0 24 24' stroke='currentColor' strokeWidth={2}>
             <path strokeLinecap='round' strokeLinejoin='round' d='M4 6h16M4 12h16M4 18h16' />
           </svg>
         }
