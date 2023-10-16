f128cb392d2510188c457e8edb0f273d872f82a3
Spencer Spenst <spencerspenst@gmail.com>
2023-10-16 15:33:27 -0700

    header button hover

--- components/header/headerControls.tsx
+++ components/header/headerControls.tsx
@@ -25,7 +25,7 @@ function UserHeaderControls() {
   if (!user) {
     return (<>
       <Link
-        className='underline'
+        className='hover:underline'
         href='/login'
         onClick={() => {
           sessionStorage.clear();
@@ -33,7 +33,7 @@ function UserHeaderControls() {
       >
         Log In
       </Link>
-      <Link href='/signup' className='underline'>
+      <Link href='/signup' className='hover:underline'>
         Sign Up
       </Link>
     </>);
@@ -50,7 +50,7 @@ function UserHeaderControls() {
       <StyledTooltip id='levels-solved-header' />
     </div>
     {socket?.connected && connectedPlayersCount > 0 &&
-      <Link id='multiplayerBtn' aria-label={`multiplayer - ${connectedPlayersCount} online`} href='/multiplayer' passHref>
+      <Link className='hover:opacity-70' id='multiplayerBtn' aria-label={`multiplayer - ${connectedPlayersCount} online`} href='/multiplayer' passHref>
         <div className='flex items-start -mr-1' data-tooltip-id='connected-players-count' data-tooltip-content={`${connectedPlayersCount} player${connectedPlayersCount !== 1 ? 's' : ''} online${matches.length > 0 ? `, ${matches.length} current multiplayer match${matches.length === 1 ? '' : 'es'}` : ''}`}>
           <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-5 h-6'>
             <path strokeLinecap='round' strokeLinejoin='round' d='M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z' />
@@ -84,7 +84,7 @@ function UserHeaderControls() {
 export default function HeaderControls() {
   return (<>
     <UserHeaderControls />
-    <Link id='searchBtn' aria-label='search' className='hidden sm:block' href={'/search'} passHref prefetch={false}>
+    <Link id='searchBtn' aria-label='search' className='hidden sm:block hover:opacity-70' href={'/search'} passHref prefetch={false}>
       <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor' strokeWidth={2}>
         <path strokeLinecap='round' strokeLinejoin='round' d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z' />
       </svg>
