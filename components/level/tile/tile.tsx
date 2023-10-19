d91e913f1905086254d34df929c17f51ae1c8172
Spencer Spenst <spencerspenst@gmail.com>
2023-10-19 14:26:41 -0700

    revert tile.tsx changes

--- components/level/tile/tile.tsx
+++ components/level/tile/tile.tsx
@@ -3,8 +3,7 @@ import { AppContext } from '@root/contexts/appContext';
 import { GridContext } from '@root/contexts/gridContext';
 import Position from '@root/models/position';
 import classNames from 'classnames';
-import { gsap } from 'gsap';
-import React, { useContext, useEffect, useMemo, useRef, useState } from 'react';
+import React, { useContext, useMemo, useState } from 'react';
 import TileType from '../../../constants/tileType';
 import Block from './block';
 import Player from './player';
@@ -82,22 +81,8 @@ export default function Tile({
     );
   }, [atEnd, inHole, text, tileType]);
 
-  const tileRef = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    if (tileRef.current) {
-      gsap.to(tileRef.current.style, {
-        duration: .1, // duration in seconds, equivalent to 0.1s in your CSS transition
-        transform: `translate(${(pos.x - initPos.x) * tileSize}px, ${(pos.y - initPos.y) * tileSize}px)`,
-        // don't cancel the animation on subsequent renders
-        overwrite: 'auto',
-      });
-    }
-  }, [pos, initPos, tileSize]);
-
   return (
     <div
-      ref={tileRef}
       className={classNames(`absolute tile-type-${tileType}`, className)}
       onClick={onClick}
       onContextMenu={onClick}
@@ -107,9 +92,8 @@ export default function Tile({
         height: classic ? tileSize : innerTileSize,
         left: tileSize * initPos.x + (classic ? 0 : borderWidth),
         top: tileSize * initPos.y + (classic ? 0 : borderWidth),
-        //  transform: `translate(${(pos.x - initPos.x + adjustment) * tileSize}px, ${(pos.y - initPos.y + adjustment) * tileSize}px)`,
-        // add support for safari
-        //  transition: 'transform 0.1s',
+        transform: `translate(${(pos.x - initPos.x) * tileSize}px, ${(pos.y - initPos.y) * tileSize}px)`,
+        transition: 'transform 0.1s',
         width: classic ? tileSize : innerTileSize,
       }}
     >
