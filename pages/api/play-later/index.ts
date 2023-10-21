6c973f32a7d40f89447f8b2a7cf1bb0d97008f6b
Danny Miller <k2xl.com@gmail.com>
2023-10-21 16:14:30 -0400

    play later api

---
+++ pages/api/play-later/index.ts
@@ -0,0 +1,132 @@
+import { ValidEnum, ValidObjectId } from '@root/helpers/apiWrapper';
+import { getEnrichLevelsPipelineSteps } from '@root/helpers/enrich';
+import { generateCollectionSlug } from '@root/helpers/generateSlug';
+import withAuth, { NextApiRequestWithAuth } from '@root/lib/withAuth';
+import { CollectionType } from '@root/models/CollectionEnums';
+import Collection from '@root/models/db/collection';
+import { CollectionModel, LevelModel, UserModel } from '@root/models/mongoose';
+import { LEVEL_DEFAULT_PROJECTION } from '@root/models/schemas/levelSchema';
+import { PipelineStage } from 'mongoose';
+import { NextApiResponse } from 'next';
+
+export const MAX_LEVELS_IN_PlayLater = 500;
+export default withAuth(
+  {
+    POST: {
+      body: {
+        id: ValidObjectId(),
+      },
+
+    },
+    GET: {
+    },
+    DELETE: {
+      body: {
+        id: ValidObjectId(),
+      },
+    },
+  }, async (req: NextApiRequestWithAuth, res: NextApiResponse) => {
+    if (req.method === 'GET') {
+      const { populate } = req.query;
+     
+      // grab the PlayLater
+      const PlayLater = await CollectionModel.aggregate([
+        {
+          $match: {
+            userId: req.user._id,
+            type: CollectionType.PlayLater,
+          }
+        },
+      ]) as Collection[];
+
+      if (PlayLater.length === 0) {
+        return res.status(200).json([]);
+      }
+
+      return res.status(200).json(PlayLater[0]);
+    }
+
+    if (req.method === 'DELETE') {
+      const { id } = req.body;
+
+      await CollectionModel.updateOne(
+        {
+          userId: req.user._id,
+          type: CollectionType.PlayLater,
+        },
+        {
+          $pull: {
+            levels: id,
+          },
+        },
+        {
+          setDefaultsOnInsert: true,
+          runValidators: true,
+        },
+      );
+
+      return res.status(200).json({ success: true });
+    }
+
+    if (req.method === 'POST') {
+      const { id } = req.body;
+
+      const level = await LevelModel.findOne({
+        _id: id,
+        isDraft: false,
+        isDeleted: { $ne: true },
+      }, {
+        _id: 1,
+      }, {
+        lean: true,
+      });
+
+      if (!level) {
+        return res.status(404).json({ error: 'Level not found.' });
+      }
+
+      // grab the PlayLater
+      const PlayLater = await CollectionModel.findOne({
+        userId: req.user._id,
+        type: CollectionType.PlayLater,
+      }, {
+        levels: 1,
+      }, {
+        lean: true,
+      }) as Collection;
+
+      if (PlayLater) {
+        if (PlayLater.levels.length > MAX_LEVELS_IN_PlayLater) {
+          return res.status(400).json({ error: `You can only have ${MAX_LEVELS_IN_PlayLater} levels in your PlayLater. Please remove some levels and try again.` });
+        }
+
+        if (PlayLater.levels.find((level) => level.toString() === id)) {
+          return res.status(400).json({ error: 'This level is already in your PlayLater.' });
+        }
+      }
+
+      await CollectionModel.findOneAndUpdate(
+        {
+          userId: req.user._id,
+          type: CollectionType.PlayLater,
+        },
+        {
+          // add to set the id of the level to add to the PlayLater
+          name: !PlayLater?.name ? 'Play Later' : undefined,
+          $addToSet: {
+            levels: id,
+          },
+          slug: !PlayLater?.slug ? req.user.name + '/play-later' : undefined,
+          type: !PlayLater?.type ? CollectionType.PlayLater : undefined,
+        },
+        {
+          new: true,
+          upsert: true,
+          setDefaultsOnInsert: true,
+          runValidators: true,
+        },
+      );
+
+      return res.status(200).json({ success: true });
+    }
+  });
