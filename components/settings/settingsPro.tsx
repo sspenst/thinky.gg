a147efedc8d5dfbf4676a37a3eb61f0528821903
Spencer Spenst <spencerspenst@gmail.com>
2023-10-24 16:40:59 -0700

    fix collection test

--- components/settings/settingsPro.tsx
+++ components/settings/settingsPro.tsx
@@ -336,7 +336,7 @@ export default function SettingsPro({ stripeCustomerPortalLink, stripePaymentLin
             title='Extra Level Stats'
           />
           <ProFeature
-            description='Create private collections'
+            description='Create collections only viewable by you'
             icon={
               <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6'>
                 <path strokeLinecap='round' strokeLinejoin='round' d='M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z' />
