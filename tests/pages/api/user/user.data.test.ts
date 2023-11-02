9ca93906a832e79059b2570c9d10797fc1144894
Spencer Spenst <spencerspenst@gmail.com>
2023-11-01 22:03:58 -0700

    lint

--- tests/pages/api/user/user.data.test.ts
+++ tests/pages/api/user/user.data.test.ts
@@ -1,18 +1,12 @@
-import { UserModel } from '@root/models/mongoose';
-import { enableFetchMocks } from 'jest-fetch-mock';
-import MockDate from 'mockdate';
-import { testApiHandler } from 'next-test-api-route-handler';
-import { Logger } from 'winston';
-import TestId from '../../../../constants/testId';
-import { TimerUtil } from '../../../../helpers/getTs';
-import { logger } from '../../../../helpers/logger';
-import dbConnect, { dbDisconnect } from '../../../../lib/dbConnect';
-import { getTokenCookieValue } from '../../../../lib/getTokenCookie';
-import withAuth, { NextApiRequestWithAuth } from '../../../../lib/withAuth';
-import modifyUserHandler from '../../../../pages/api/user/[id]/index';
 import { ProfileQueryType } from '@root/constants/profileQueryType';
 import User from '@root/models/db/user';
-import { difficultyList } from '@root/components/formatted/formattedDifficulty';
+import { enableFetchMocks } from 'jest-fetch-mock';
+import { testApiHandler } from 'next-test-api-route-handler';
+import TestId from '../../../../constants/testId';
+import dbConnect, { dbDisconnect } from '../../../../lib/dbConnect';
+import { getTokenCookieValue } from '../../../../lib/getTokenCookie';
+import { NextApiRequestWithAuth } from '../../../../lib/withAuth';
+import modifyUserHandler from '../../../../pages/api/user/[id]/index';
 
 afterEach(() => {
   jest.restoreAllMocks();
@@ -54,7 +48,7 @@ describe('pages/api/user/[id]/index.ts', () => {
         expect(response).toBeDefined();
         expect(
           response[ProfileQueryType.LevelsSolvedByDifficulty]
-        ).toStrictEqual({ "-1": 2 }); // -1 stands for pending
+        ).toStrictEqual({ '-1': 2 }); // -1 stands for pending
         expect(((response[ProfileQueryType.User]) as User).score).toBe(2);
       },
     });
