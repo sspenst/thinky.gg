738730c17c5f1bcc6c039403cad2fec6aebf32a8
k2xl <k2xl.com@gmail.com>
2023-11-21 22:07:05 -0500

    Paginate collection view (#1048)
    * paginate wip
    
    * pagination wip... scroll
    
    * wip
    
    * collectionscroll list
    
    * seems to be working
    
    * fix issue
    
    * i think this works
    
    * kind of a mess... but it works i think

--- pages/api/collection-by-id/[id].ts
+++ pages/api/collection-by-id/[id].ts
@@ -1,28 +1,32 @@
 import cleanUser from '@root/lib/cleanUser';
-import { LEVEL_DEFAULT_PROJECTION } from '@root/models/schemas/levelSchema';
+import { LEVEL_DEFAULT_PROJECTION, LEVEL_SEARCH_DEFAULT_PROJECTION } from '@root/models/schemas/levelSchema';
 import { USER_DEFAULT_PROJECTION } from '@root/models/schemas/userSchema';
 import { FilterQuery, PipelineStage, Types } from 'mongoose';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import apiWrapper, { ValidObjectId } from '../../../helpers/apiWrapper';
+import apiWrapper, { ValidEnum, ValidObjectId } from '../../../helpers/apiWrapper';
 import { getEnrichLevelsPipelineSteps } from '../../../helpers/enrich';
 import { getUserFromToken } from '../../../lib/withAuth';
-import Collection from '../../../models/db/collection';
+import Collection, { EnrichedCollection } from '../../../models/db/collection';
 import User from '../../../models/db/user';
 import { CollectionModel, LevelModel, StatModel, UserModel } from '../../../models/mongoose';
 
 export default apiWrapper({
   GET: {
     query: {
-      id: ValidObjectId(true)
+      id: ValidObjectId(true),
+      populateCursor: ValidObjectId(false),
+      populateDirection: ValidEnum(['before', 'after', 'around'], false),
     }
   } }, async (req: NextApiRequest, res: NextApiResponse) => {
-  const { id } = req.query;
+  const { id, populateCursor, populateDirection } = req.query;
   const token = req.cookies?.token;
   const reqUser = token ? await getUserFromToken(token, req) : null;
   const collection = await getCollection({
     matchQuery: { _id: new Types.ObjectId(id as string) },
     reqUser,
     populateLevels: true,
+    ...(populateCursor ? { populateLevelCursor: new Types.ObjectId(populateCursor as string) } : {}),
+    ...(populateDirection ? { populateLevelDirection: populateDirection as 'before' | 'after' | 'around' } : {}),
   });
 
   if (!collection) {
@@ -40,6 +44,8 @@ interface GetCollectionProps {
   reqUser: User | null,
   includeDraft?: boolean,
   populateLevels?: boolean,
+  populateLevelCursor?: Types.ObjectId | string, // target level
+  populateLevelDirection?: 'before' | 'after' | 'around',
 }
 
 /**
@@ -56,12 +62,15 @@ export async function getCollection(props: GetCollectionProps): Promise<Collecti
   return collections[0] as Collection;
 }
 
+// Create interface for partial collection where we have the fields targetLevelIndex and levelCount added
+
 /**
  * Query collections with optionally populated levels and stats.
  * Private collections are filtered out unless they were created by the reqUser.
  */
-export async function getCollections({ matchQuery, reqUser, includeDraft, populateLevels }: GetCollectionProps): Promise<Collection[]> {
-  const collectionAgg = await CollectionModel.aggregate<Collection>(([
+export async function getCollections({ matchQuery, reqUser, includeDraft, populateLevels, populateLevelCursor, populateLevelDirection }: GetCollectionProps): Promise<Collection[]> {
+  const pageSize = 5;
+  const collectionAgg = await CollectionModel.aggregate<EnrichedCollection>(([
     {
       $match: {
         // filter out private collections unless the userId matches reqUser
@@ -108,6 +117,7 @@ export async function getCollections({ matchQuery, reqUser, includeDraft, popula
         }
       }
     },
+
     {
       $lookup: {
         from: LevelModel.collection.name,
@@ -118,6 +128,7 @@ export async function getCollections({ matchQuery, reqUser, includeDraft, popula
         pipeline: [
           {
             $match: {
+              // only match levels that are in the orderedIds array
               ...(!includeDraft ? {
                 isDraft: {
                   $ne: true
@@ -140,46 +151,20 @@ export async function getCollections({ matchQuery, reqUser, includeDraft, popula
               sort: 1
             }
           },
+
           {
             $project: {
-              leastMoves: 1,
               ...(includeDraft ? {
                 isDraft: 1
               } : {}),
               ...(populateLevels ? {
                 ...LEVEL_DEFAULT_PROJECTION
-              } : {})
+              } : {
+                ...LEVEL_SEARCH_DEFAULT_PROJECTION
+              })
             },
           },
-          ...(populateLevels ? getEnrichLevelsPipelineSteps(reqUser, '_id', '') : [{
-            $lookup: {
-              from: StatModel.collection.name,
-              localField: '_id',
-              foreignField: 'levelId',
-              as: 'stats',
-              pipeline: [
-                {
-                  $match: {
-                    userId: reqUser?._id,
-                    complete: true
-                  },
-                },
-                {
-                  $project: {
-                    _id: 0,
-                    // project complete to 1 if it exists, otherwise 0
-                    complete: {
-                      $cond: {
-                        if: { $eq: ['$complete', true] },
-                        then: 1,
-                        else: 0
-                      }
-                    }
-                  }
-                }
-              ]
-            },
-          }]),
+          ...getEnrichLevelsPipelineSteps(reqUser, '_id', '') as PipelineStage[],
           {
             $unwind: {
               path: '$stats',
@@ -210,6 +195,7 @@ export async function getCollections({ matchQuery, reqUser, includeDraft, popula
     {
       $unset: 'levelsWithSort'
     },
+
     {
       $addFields: {
         levelCount: {
@@ -220,7 +206,50 @@ export async function getCollections({ matchQuery, reqUser, includeDraft, popula
         }
       }
     },
-    ...(!populateLevels ? [{ $unset: 'levels' }] : []),
+    ...populateLevelCursor ? [{
+      // slice the levels array to include 10 before and 10 after the target level.. keep in mind that there may not be 10 levels before or after
+
+      $addFields: {
+        targetLevelIndex: {
+          $indexOfArray: ['$levels._id', populateLevelCursor]
+        },
+      },
+    }, {
+      $addFields: {
+        levels: {
+          $cond: {
+            if: { $eq: [populateLevelDirection, 'before'] },
+            then: {
+              $slice: [
+                '$levels',
+                { $max: [0, { $subtract: ['$targetLevelIndex', pageSize] }] },
+                { $min: [pageSize + 1, { $add: ['$targetLevelIndex', 1] }] }
+              ]
+            },
+            else: {
+              $cond: {
+                if: { $eq: [populateLevelDirection, 'after'] },
+                then: {
+                  $slice: [
+                    '$levels',
+                    '$targetLevelIndex',
+                    { $min: [pageSize + 1, { $subtract: [{ $size: '$levels' }, '$targetLevelIndex'] }] }
+                  ]
+                },
+                else: { // 'around'
+                  $slice: [
+                    '$levels',
+                    { $max: [0, { $subtract: ['$targetLevelIndex', pageSize] }] },
+                    pageSize * 2 + 1
+                  ]
+                }
+              }
+            }
+          }
+        }
+      }
+
+    }] : [],
   ] as PipelineStage[]));
 
   collectionAgg.forEach(collection => {
