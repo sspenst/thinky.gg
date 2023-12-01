438fb26dd8fe8b6174ae80cb25d766f2e2a44ac1
Danny Miller <k2xl.com@gmail.com>
2023-11-30 20:55:52 -0500

    dyk

--- components/page/didYouKnowTip.tsx
+++ components/page/didYouKnowTip.tsx
@@ -1,20 +1,22 @@
+import { AppContext } from '@root/contexts/appContext';
 import User from '@root/models/db/user';
 import Link from 'next/link';
-import React, { useRef } from 'react';
+import React, { useContext, useRef } from 'react';
 
 interface DidYouKnowTipProps {
   reqUser: User | null;
 }
 
 export default function DidYouKnowTip({ reqUser }: DidYouKnowTipProps) {
+  const { game } = useContext(AppContext);
   const tips = [
-    <>Every level in Pathology was created in the level editor! You can start building <Link className='underline' href='/create'>here</Link>.</>,
+    <>Every level in {game.displayName} was created in the level editor! You can start building <Link className='underline' href='/create'>here</Link>.</>,
     <>Use <Link className='underline' href='/search'>advanced search</Link> to find levels that suit your taste.</>,
     <>Compete in <Link className='underline' href='/multiplayer'>real-time multiplayer</Link>. Race to solve puzzles against others!</>,
     <>Join our active <Link className='underline' href='https://discord.gg/kpfdRBt43v'>Discord</Link> to connect with other players and level creators.</>,
     <>Every level in the game has a review rating. Learn how these are calculated <Link className='underline' href='https://github.com/sspenst/pathology/wiki/Level-Review-Score'>here</Link>.</>,
     <>Level difficulties are calculated automatically! Learn more about it <Link className='underline' href='https://github.com/sspenst/pathology/wiki/Level-Difficulty-System'>here</Link>.</>,
-    <>Pathology has a long history and its own lingo / terminology (pipes, locks, etc...). You learn about them in the Pathology <Link className='underline' href='https://github.com/sspenst/pathology/wiki/Glossary'>glossary</Link>.</>,
+    <>{game.displayName} has a long history and its own lingo / terminology (pipes, locks, etc...). You learn about them in the {game.displayName} <Link className='underline' href='https://github.com/sspenst/pathology/wiki/Glossary'>glossary</Link>.</>,
     <>We have an iOS and Android app! Check it out: <Link className='underline' href='https://apps.apple.com/app/pathology-block-pushing-game/id1668925562'>iOS</Link> and <Link className='underline' href='https://play.google.com/store/apps/details?id=com.pathology.gg'>Android</Link>.</>,
     <>Follow level creators by clicking their username to visit their profile.</>,
     <>Go Pro for features like Checkpoints, Redo, and community time comparisons. <Link className='underline' href='/settings/pro'>Learn more</Link>.</>,
