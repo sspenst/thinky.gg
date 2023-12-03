e4447d429ef368977973eba4460c7b0bf958c548
Danny Miller <k2xl.com@gmail.com>
2023-12-03 13:03:58 -0500

    getGameIdFromReq improve

--- helpers/getGameIdFromReq.ts
+++ helpers/getGameIdFromReq.ts
@@ -4,7 +4,7 @@ import type { NextApiRequest } from 'next';
 import { DEFAULT_GAME_ID, GameId } from '../constants/GameId';
 
 export function getGameIdFromReq(req?: NextApiRequest | IncomingMessage): GameId {
-  const subdomain = req?.headers?.referer?.split('://')[1].split('.')[0];
+  const subdomain = req?.headers?.host?.split('.')[0];
 
   return Games[subdomain as GameId]?.id || DEFAULT_GAME_ID;
 }
