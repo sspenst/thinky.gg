f8e045ee7df459a01b025e570c39cb472a336c62
Spencer Spenst <spencerspenst@gmail.com>
2023-12-04 16:22:09 -0800

    discord notif for GM+ achievements

--- components/profile/profileInsightsMostSolves.tsx
+++ components/profile/profileInsightsMostSolves.tsx
@@ -16,41 +16,43 @@ export default function ProfileInsightsMostSolves({ user }: {user: User}) {
     return <span>Loading...</span>;
   }
 
-  return (<div className='flex flex-col gap-1'>
-    <h2 className='text-xl font-bold break-words max-w-full'>Most Solves of {user.name}&apos;s Levels</h2>
-    <div className='w-full max-w-md'>
-      <DataTable
-        columns={[
-          {
-            name: 'User',
-            cell: (row: UserAndSum) => <FormattedUser id='solves' size={Dimensions.AvatarSizeSmall} user={row.user} />,
-            grow: 2,
-          },
-          {
-            name: 'Levels Solved',
-            selector: (row) => row.sum,
-          },
-        ]}
-        conditionalRowStyles={[{
-          when: row => row.user._id === reqUser?._id,
-          style: {
-            backgroundColor: 'var(--bg-color-4)',
-          },
-        }]}
-        customStyles={DATA_TABLE_CUSTOM_STYLES}
-        data={proStatsUser[ProStatsUserType.MostSolvesForUserLevels] as UserAndSum[]}
-        dense
-        noDataComponent={
-          <div className='p-3'>
+  return (
+    <div className='flex flex-col gap-1'>
+      <h2 className='text-xl font-bold break-words max-w-full'>Most Solves of {user.name}&apos;s Levels</h2>
+      <div className='w-full max-w-md'>
+        <DataTable
+          columns={[
+            {
+              name: 'User',
+              cell: (row: UserAndSum) => <FormattedUser id='solves' size={Dimensions.AvatarSizeSmall} user={row.user} />,
+              grow: 2,
+            },
+            {
+              name: 'Levels Solved',
+              selector: (row) => row.sum,
+            },
+          ]}
+          conditionalRowStyles={[{
+            when: row => row.user._id === reqUser?._id,
+            style: {
+              backgroundColor: 'var(--bg-color-4)',
+            },
+          }]}
+          customStyles={DATA_TABLE_CUSTOM_STYLES}
+          data={proStatsUser[ProStatsUserType.MostSolvesForUserLevels] as UserAndSum[]}
+          dense
+          noDataComponent={
+            <div className='p-3'>
             Nothing to display...
-          </div>
-        }
-        pagination={true}
-        paginationComponentOptions={{
-          noRowsPerPage: true,
-        }}
-        striped
-      />
+            </div>
+          }
+          pagination={true}
+          paginationComponentOptions={{
+            noRowsPerPage: true,
+          }}
+          striped
+        />
+      </div>
     </div>
-  </div>);
+  );
 }
