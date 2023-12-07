a4e48ebdf88faeb4657497c0e93e54f3ab81cdf9
Danny Miller <k2xl.com@gmail.com>
2023-12-06 22:08:32 -0500

    no longer need to kill indicies in the migration script

--- server/scripts/migrateUserCalcsToConfig.ts
+++ server/scripts/migrateUserCalcsToConfig.ts
@@ -20,37 +20,6 @@ async function migrate() {
 
   progressBar.start(allUsers.length, 0);
 
-  // kill indices for the platform migration
-  // In UserConfig schema we need to remove the unique index on userId
-  try {
-    console.log('Dropping unique index on userId in UserConfigModel');
-    const res = await UserConfigModel.collection.dropIndex('userId_1');
-
-    console.log('Dropped unique index on userId in UserConfigModel ', res.ok);
-  } catch (err) {
-    console.log('Could not drop unique index on userId in UserConfigModel (maybe already deleted?');
-  }
-
-  try {
-  // next drop on multiplayermatchprofile userId
-    console.log('Dropping unique index on userId in MultiplayerMatchProfileModel');
-    const res2 = await MultiplayerProfileModel.collection.dropIndex('userId_1');
-
-    console.log('Dropped unique index on userId in MultiplayerMatchProfileModel ', res2.ok);
-  } catch (err) {
-    console.log('Could not drop unique index on userId in MultiplayerMatchProfileModel (maybe already deleted?');
-  }
-
-  try {
-  // Drop unique on slug in LevelModel
-    console.log('Dropping unique index on slug in LevelModel');
-    const res3 = await MultiplayerMatchModel.collection.dropIndex('slug_index');
-
-    console.log('Dropped unique index on slug in LevelModel ', res3.ok);
-  } catch (err) {
-    console.log('Could not drop unique index on slug in LevelModel (maybe already deleted?');
-  }
-
   // loop through all userConfigs and update UserModel.emailConfirmed to equal it
   for (let i = 0; i < allUsers.length; i++) {
     const user = await UserModel.findById<User>(allUsers[i]._id, { 'calcRankedSolves': 1, 'calc_levels_created_count': 1, 'calc_records': 1, 'chapterUnlocked': 1, 'score': 1 }).lean();
