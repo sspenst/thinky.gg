e93b56b52d1123d19e178a771dd6cdd919d09e7e
Spencer Spenst <spencerspenst@gmail.com>
2023-12-08 01:48:28 -0800

    minor css update

--- components/header/gameMenu.tsx
+++ components/header/gameMenu.tsx
@@ -17,7 +17,7 @@ export default function GameMenu() {
   return (
     <Menu>
       <Menu.Button>
-        <svg className='h-5 w-5' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' aria-hidden='true'>
+        <svg className='h-5 w-5 hover:opacity-70' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' aria-hidden='true'>
           <path fillRule='evenodd' d='M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z' clipRule='evenodd' />
         </svg>
       </Menu.Button>
@@ -43,7 +43,7 @@ export default function GameMenu() {
               {({ active }) => (
                 <a href={getUrl(link.subdomain)}>
                   <div
-                    className='flex w-full items-center rounded-md cursor-pointer px-3 py-2 gap-3'
+                    className='flex w-full items-center cursor-pointer px-3 py-2 gap-3'
                     style={{
                       backgroundColor: active ? 'var(--bg-color-3)' : undefined,
                     }}
