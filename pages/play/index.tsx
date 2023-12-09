f0137752ed1b2ca38d2da907e3751b48c700116b
Spencer Spenst <spencerspenst@gmail.com>
2023-12-08 21:08:05 -0800

    improve new user tour experience

--- pages/play/index.tsx
+++ pages/play/index.tsx
@@ -1,9 +1,5 @@
-import PagePath from '@root/constants/pagePath';
-import { useTour } from '@root/hooks/useTour';
 import { GetServerSidePropsContext, NextApiRequest } from 'next';
-import { useRouter } from 'next/router';
-import React, { useCallback } from 'react';
-import { CallBackProps } from 'react-joyride';
+import React from 'react';
 import ChapterSelectCard from '../../components/cards/chapterSelectCard';
 import Page from '../../components/page/page';
 import { getUserFromToken } from '../../lib/withAuth';
@@ -36,29 +32,18 @@ interface PlayPageProps {
 /* istanbul ignore next */
 export default function PlayPage({ reqUser }: PlayPageProps) {
   const chapterUnlocked = reqUser.chapterUnlocked ?? 1;
-  const router = useRouter();
-  const memoizedCallback = useCallback((data: CallBackProps) => {
-    if (data.action === 'next' && data.index === 0) {
-      router.push('/chapter1');
-    }
-  }, [router]);
-
-  const tour = useTour(PagePath.PLAY, memoizedCallback);
 
   return (
     <Page title={'Chapter Select'}>
-      <>
-        {tour}
-        <div className='flex flex-col items-center gap-8 p-4'>
-          <div className='font-bold text-3xl text-center' id='title'>
-            Pathology Official Campaign
-          </div>
-          <ChapterSelectCard chapter={1} chapterUnlocked={chapterUnlocked} />
-          <ChapterSelectCard chapter={2} chapterUnlocked={chapterUnlocked} />
-          <ChapterSelectCard chapter={3} chapterUnlocked={chapterUnlocked} />
-          {chapterUnlocked >= 3 && <ChapterSelectCard chapter={4} chapterUnlocked={chapterUnlocked} />}
+      <div className='flex flex-col items-center gap-8 p-4'>
+        <div className='font-bold text-3xl text-center' id='title'>
+          Pathology Official Campaign
         </div>
-      </>
+        <ChapterSelectCard chapter={1} chapterUnlocked={chapterUnlocked} />
+        <ChapterSelectCard chapter={2} chapterUnlocked={chapterUnlocked} />
+        <ChapterSelectCard chapter={3} chapterUnlocked={chapterUnlocked} />
+        {chapterUnlocked >= 3 && <ChapterSelectCard chapter={4} chapterUnlocked={chapterUnlocked} />}
+      </div>
     </Page>
   );
 }
