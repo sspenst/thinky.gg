4a3e45b7ffaa310fb50fb2616fae26203aa5d6b2
Spencer Spenst <spencerspenst@gmail.com>
2023-12-15 11:58:28 -0800

    NavGameMenu

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -43,7 +43,7 @@ export default function Header({
         <GameMenu />
         {/* TODO: show only on isFullScreen / on mobile */}
         <div className='pl-4 pr-2'>
-          <Link className={'font-bold text-3xl'} href={!userLoading && !user ? '/' : '/home'}>
+          <Link className='font-bold text-3xl' href={!userLoading && !user ? '/' : '/home'}>
             <Image alt='logo' src={game.logo} width='24' height='24' className='h-6 w-6' style={{ minWidth: 24, minHeight: 24 }} />
           </Link>
         </div>
