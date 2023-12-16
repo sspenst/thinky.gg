657f0172e81979b693de4e25b0374e74a1eff37a
Danny Miller <k2xl.com@gmail.com>
2023-12-16 14:45:45 -0500

    nav play link tweak

--- components/nav.tsx
+++ components/nav.tsx
@@ -285,7 +285,7 @@ export default function Nav({ isDropdown }: NavProps) {
     label='Discord'
   />;
 
-  const playNavLink = <NavLink
+  const playNavLink = !game.disableCampaign && <NavLink
     href='/play'
     icon={
       <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-5 h-5'>
