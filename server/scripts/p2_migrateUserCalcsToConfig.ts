514b8829345f6f37b44283ee0c251cde773ffcb9
Danny Miller <k2xl.com@gmail.com>
2023-12-16 14:45:45 -0500

    nav play link tweak

--- server/scripts/p2_migrateUserCalcsToConfig.ts
+++ server/scripts/p2_migrateUserCalcsToConfig.ts
@@ -1,11 +1,8 @@
 // run with
 // ts-node -r tsconfig-paths/register --files server/scripts/p2_migrateUserCalcsToConfig.ts
 // import dotenv
-import User from '@root/models/db/user';
 import { UserConfigModel, UserModel } from '@root/models/mongoose';
-import cliProgress from 'cli-progress';
 import dotenv from 'dotenv';
-import dbConnect, { dbDisconnect } from '../../lib/dbConnect';
 import { migrateFields } from './migrateFields';
 
 dotenv.config();
