16ac55385c24d09e1751d57293951d9c72951e3d
Danny Miller <k2xl.com@gmail.com>
2023-12-16 14:25:19 -0500

    generalized migration

--- server/scripts/p2_migrateEmailDigestToUser.ts
+++ server/scripts/p2_migrateEmailDigestToUser.ts
@@ -1,61 +1,16 @@
-// ts-node -r tsconfig-paths/register --files server/scripts/p2_migrateEmailDigestToUser.ts
 import { UserConfigModel, UserModel } from '@root/models/mongoose';
 import dotenv from 'dotenv';
-import dbConnect, { dbDisconnect } from '../../lib/dbConnect';
+import { migrateFields } from './migrateFields';
 
 dotenv.config();
 
-async function migrate() {
-  console.log('Starting the database connection...');
-  await dbConnect();
-  console.log('Database connected.');
+// Example usage
+const sourceModel = UserConfigModel; // Replace with your source model
+const targetModel = UserModel; // Replace with your target model
+const fieldsToMigrate = {
+  emailDigest: '$emailDigest',
+  disallowedEmailNotifications: '$disallowedEmailNotifications',
+  disallowedPushNotifications: '$disallowedPushNotifications'
+};
 
-  console.log('Starting the aggregation pipeline...');
-  const s = Date.now();
-
-  await UserConfigModel.aggregate([
-    {
-      $match: {} // Match all documents or apply any filters if necessary
-    },
-    {
-      $lookup: {
-        from: UserModel.collection.name, // The collection name of UserModel in MongoDB
-        localField: 'userId',
-        foreignField: '_id',
-        as: 'user'
-      }
-    },
-    {
-      $unwind: '$user'
-    },
-    {
-      $match: { 'user.emailDigest': { $exists: false } } // Only match users where emailDigest doesn't exist
-    },
-    {
-      $project: {
-        _id: '$user._id',
-        emailDigest: '$emailDigest',
-        disallowedEmailNotifications: '$disallowedEmailNotifications',
-        disallowedPushNotifications: '$disallowedPushNotifications'
-      }
-    },
-    {
-      $merge: {
-        into: UserModel.collection.name, // The collection name of UserModel in MongoDB
-        whenMatched: 'merge'
-      }
-    }
-  ]);
-  const timeTaken = Date.now() - s;
-  const timeTakenSeconds = (timeTaken / 1000).toFixed(2);
-
-  console.log('Aggregation pipeline completed in ' + timeTakenSeconds + ' seconds.');
-
-  console.log('Closing the database connection...');
-  await dbDisconnect();
-  console.log('Database disconnected.');
-
-  console.log('Migration completed successfully.');
-}
-
-migrate();
+migrateFields(sourceModel, targetModel, fieldsToMigrate, 'userId', '_id', { 'target.emailDigest': { $exists: false } });
