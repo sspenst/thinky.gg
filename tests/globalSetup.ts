3c1b2497e6ddbfc4fd5bbd11eb0c2fa9034be8ac
Danny Miller <k2xl.com@gmail.com>
2023-12-26 16:10:31 -0500

    cleanup

--- tests/globalSetup.ts
+++ tests/globalSetup.ts
@@ -3,8 +3,6 @@
 import { MongoMemoryReplSet } from 'mongodb-memory-server';
 
 module.exports = async () => {
-  // TODO: figure out how to have this work... we get strange errors when we try to use it
-
   const mongoServer = await MongoMemoryReplSet.create({
     replSet: { count: 1 },
   });
@@ -12,7 +10,7 @@ module.exports = async () => {
 
   // Set the Mongo URI for the tests to use
   process.env.MONGODB_TEST_URI = mongoUri;
-  console.log('MONGODB_TEST_URI', process.env.MONGODB_TEST_URI);
+  console.log('\nMONGODB_TEST_URI', process.env.MONGODB_TEST_URI);
 
   // Add mongoServer to the global config so we can tear it down later
   global.__MONGOSERVER__ = mongoServer;
