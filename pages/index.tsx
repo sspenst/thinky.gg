0cd1250c20829a296b51598c8c4b24df9b09bdef
Danny Miller <k2xl.com@gmail.com>
2024-01-05 17:34:57 -0500

    get rid of parallax

--- pages/index.tsx
+++ pages/index.tsx
@@ -100,45 +100,49 @@ export default function ThinkyHomePage() {
       theme: Theme.Classic
     },
   };
+  const ThinkyH1Text = (
+    <h1 className='text-6xl md:text-8xl text-white font-thin p-3 rounded-lg mt-10 text-center'>
+          Thinky.gg
+    </h1>
+  );
+  const headlineComp = <div className='justify-center flex flex-col md:flex-row items-center'>
+    <div className='flex flex-col justify-center'>
+      {ThinkyH1Text}
+      <div className='flex flex-row justify-center p-4 gap-1'>
+        {Object.values(Games).map(game => {
+          if (game.id === GameId.THINKY) {
+            return null;
+          }
+
+          return (
+            <div className='flex flex-col items-center gap-4' key={`game-${game.id}`}>
+              <GameCard game={game} />
+              <div className='flex h-40 w-40'>
+                <AnimatedGrid leastMoves={gameInstr[game.id].leastMoves} animationInstructions={gameInstr[game.id].instructions} game={game} theme={gameInstr[game.id].theme} id={'level-preview-' + game.id} levelData={gameInstr[game.id].data} />
+              </div>
+              <div className='p-2  text-center text-md fadeIn trun'>
+                {game.shortDescription}
+              </div>
+              <a
+                className='text-2xl font-bold px-4 py-3 border-2 border-color-3 rounded-lg hover-bg-3 hover:scale-110 transition'
+                href={userConfig?.tutorialCompletedAt ? getUrl(game.id, '/play') : getUrl(game.id, '/tutorial')}
+                role='button'
+              >
+            Play Now
+              </a>
+            </div>
+          );
+        } )}
+      </div>
+    </div>
+  </div>;
   const headline: BannerLayer = {
     translateY: ['0%', '20%'],
     scale: [1, 1.05, 'easeOutCubic'],
     shouldAlwaysCompleteAnimation: true,
     expanded: false,
     children: (
-      <div className='justify-center flex flex-col md:flex-row items-center'>
-        <h1 className='text-6xl md:text-8xl text-white font-thin p-3 rounded-lg mt-10'>
-          Thinky.gg
-        </h1>
-        <div className='flex justify-center'>
-          <div className='flex flex-row justify-center p-10 md:mt-20 gap-1'>
-            {Object.values(Games).map(game => {
-              if (game.id === GameId.THINKY) {
-                return null;
-              }
-
-              return (
-                <div className='flex flex-col items-center gap-4' key={`game-${game.id}`}>
-                  <GameCard game={game} />
-                  <div className='flex h-40 w-40'>
-                    <AnimatedGrid leastMoves={gameInstr[game.id].leastMoves} animationInstructions={gameInstr[game.id].instructions} game={game} theme={gameInstr[game.id].theme} id={'level-preview-' + game.id} levelData={gameInstr[game.id].data} />
-                  </div>
-                  <div className='p-2 h-20 text-center text-md fadeIn trun'>
-                    {game.shortDescription}
-                  </div>
-                  <a
-                    className='text-2xl font-bold px-4 py-3 border-2 border-color-3 rounded-lg hover-bg-3 hover:scale-110 transition'
-                    href={userConfig?.tutorialCompletedAt ? getUrl(game.id, '/play') : getUrl(game.id, '/tutorial')}
-                    role='button'
-                  >
-                  Play Now
-                  </a>
-                </div>
-              );
-            } )}
-          </div>
-        </div>
-      </div>
+      headlineComp
     ),
   };
 
@@ -182,36 +186,26 @@ export default function ThinkyHomePage() {
   }, []);
 
   function FeatureCardComp({ title, description, index, video }: {title: string, description: string | JSX.Element, index: number, video?: string}) {
-    const targetElement = targetElements[index] ?? targetElements[index];
+  //  const targetElement = targetElements[index] ?? targetElements[index];
 
-    return <Parallax
-
-      translateX={['-200%', '0']}
-      targetElement={targetElement}
-      //startScroll={650 + index * 350}
-      //endScroll={1000 + index * 350}
-    >
-      <div className='feature-line' style={{
-        position: 'absolute',
-        top: -400,
-        left: 50,
-        width: 10,
-        height: 100,
-        zIndex: 100,
-        backgroundColor: 'var(--color-3)',
-
-      }} />
-      <div className='feature-card md:w-80 rounded-xl p-3 m-4 border border-color-4 bg-gray-800' style={{}}>
-        <div className='font-bold text-lg'>{title}</div>
-        <div className='text-sm'>
-          {description}
-          <div className='justify-center flex p-2'>
-            <video autoPlay loop muted className='rounded-lg h-60 text-center' src={video} />
-          </div>
+    return <div className='feature-card md:w-80 rounded-xl p-3 m-4 border border-color-4 bg-gray-800' style={{}}>
+      <div className='font-bold text-lg'>{title}</div>
+      <div className='text-sm'>
+        {description}
+        <div className='justify-center flex p-2'>
+          <video autoPlay loop muted className='rounded-lg h-60 text-center' src={video} />
         </div>
       </div>
-    </Parallax>;
+    </div>;
   }
+
+  const middleThingBanner = <ParallaxBanner
+    layers={[background, foreground, gradientOverlay, subtext]}
+    className='middle-section bg-gray-900' style={{
+      height: '50vh',
+
+    }}
+  />;
 
   return (
     <Page title='Thinky.gg'
@@ -231,80 +225,104 @@ export default function ThinkyHomePage() {
 
       }}
     >
-      <div className='flex flex-col justify-center items-center h-full '>
+      <div className='flex flex-col justify-center items-center  '>
         <ParallaxProvider>
-          <ParallaxBanner layers={[headline]} className='bg-gray-900' style={{
-            height: '820px',
-          }} />
-          <ParallaxBanner
-            layers={[background, foreground, gradientOverlay, subtext]}
-            className='middle-section bg-gray-900' style={{
-              height: '50vh',
+          <div className='justify-center flex flex-col md:flex-row items-center'>
+            <div className='flex flex-col justify-center'>
+              {ThinkyH1Text}
+              <div className='flex flex-row justify-center p-4 gap-1'>
+                {Object.values(Games).map(game => {
+                  if (game.id === GameId.THINKY) {
+                    return null;
+                  }
 
-            }}
-          />
-          <div className='flex flex-col gap-3 feature-area' />
-          {FeatureCardComp({
-            title: 'Level Editor',
-            index: 0,
-            description: (
-              <div className='text-sm'>
+                  return (
+                    <div className='flex flex-col items-center gap-4' key={`game-${game.id}`}>
+                      <GameCard game={game} />
+                      <div className='flex h-40 w-40'>
+                        <AnimatedGrid leastMoves={gameInstr[game.id].leastMoves} animationInstructions={gameInstr[game.id].instructions} game={game} theme={gameInstr[game.id].theme} id={'level-preview-' + game.id} levelData={gameInstr[game.id].data} />
+                      </div>
+                      <div className='p-2  text-center text-md fadeIn trun'>
+                        {game.shortDescription}
+                      </div>
+                      <a
+                        className='text-2xl font-bold px-4 py-3 border-2 border-color-3 rounded-lg hover-bg-3 hover:scale-110 transition'
+                        href={userConfig?.tutorialCompletedAt ? getUrl(game.id, '/play') : getUrl(game.id, '/tutorial')}
+                        role='button'
+                      >
+            Play Now
+                      </a>
+                    </div>
+                  );
+                } )}
+              </div>
+
+            </div>
+          </div>
+          <div className='flex flex-wrap md:ml-40 md:mr-40 justify-center' style={{
+          }}>
+            {FeatureCardComp({
+              title: 'Level Editor',
+              index: 0,
+              description: (
+                <div className='text-sm'>
               Create your <span className='font-bold'>own</span> levels and share them with the world.
-              </div>),
-            video: 'https://i.imgur.com/uc6ndtx.mp4',
-          })}
-          {
-            FeatureCardComp({
-              title: 'Leaderboards',
-              index: 1,
+                </div>),
+              video: 'https://i.imgur.com/uc6ndtx.mp4',
+            })}
+            {
+              FeatureCardComp({
+                title: 'Leaderboards',
+                index: 1,
 
-              description: (<div className='text-sm'>
+                description: (<div className='text-sm'>
                 Compete with others in challenges.
-              </div>),
-              video: 'https://i.imgur.com/xWwF1XK.mp4',
-            })
-          }
-          {
-            FeatureCardComp({
-              title: 'Reviews',
-              index: 2,
+                </div>),
+                video: 'https://i.imgur.com/xWwF1XK.mp4',
+              })
+            }
+            {
+              FeatureCardComp({
+                title: 'Reviews',
+                index: 2,
 
-              description: (<div className='text-sm'>
+                description: (<div className='text-sm'>
                 Rate and review levels.
-              </div>),
-              video: 'https://i.imgur.com/kbIOV9w.mp4',
-            })
-          }
-          {FeatureCardComp({
-            title: 'Multiplayer',
-            index: 3,
-            description: (<div className='text-sm'>
+                </div>),
+                video: 'https://i.imgur.com/kbIOV9w.mp4',
+              })
+            }
+            {FeatureCardComp({
+              title: 'Multiplayer',
+              index: 3,
+              description: (<div className='text-sm'>
             Play with friends in real-time.
-            </div>),
-          })}
-          {FeatureCardComp({
-            title: 'Advanced Search',
-            video: 'https://i.imgur.com/GqCi1vV.mp4',
-            index: 4,
-            description: (<div className='text-sm'>
+              </div>),
+            })}
+            {FeatureCardComp({
+              title: 'Advanced Search',
+              video: 'https://i.imgur.com/GqCi1vV.mp4',
+              index: 4,
+              description: (<div className='text-sm'>
             Search for levels by name, creator, or tags.
-            </div>),
-          })}
-          {FeatureCardComp({
-            title: 'Automatic difficulty',
-            index: 5,
-            description: (<div className='text-sm'>
+              </div>),
+            })}
+            {FeatureCardComp({
+              title: 'Automatic difficulty',
+              index: 5,
+              description: (<div className='text-sm'>
               Levels are automatically rated by difficulty.
-            </div>),
-            video: 'https://i.imgur.com/y5M6nGk.mp4'
-          })}
-          {FeatureCardComp({
-            title: 'Pro',
-            index: 6,
-            description: (<div className='text-sm'>
+              </div>),
+              video: 'https://i.imgur.com/y5M6nGk.mp4'
+            })}
+            {FeatureCardComp({
+              title: 'Pro',
+              index: 6,
+              description: (<div className='text-sm'>
             Unlock <span className='font-bold'>advanced</span> analytics, checkpoint saving, and tons more.
-            </div>),
-          })}
+              </div>),
+            })}
+          </div>
           <Footer />
         </ParallaxProvider>
 
