28669633f5eae0fe82ac40453cf242640b6e6d5c
Spencer Spenst <spencerspenst@gmail.com>
2024-01-04 19:06:06 -0800

    fix getDifficultyCompletionEstimate

--- helpers/getDifficultyEstimate.ts
+++ helpers/getDifficultyEstimate.ts
@@ -33,12 +33,29 @@ export function getDifficultyCompletionEstimate(
   level: Level | EnrichedLevel | Partial<Level>,
   uniqueUsersCount: number,
 ) {
-  if (!level || uniqueUsersCount < 10 || level.calc_playattempts_duration_before_stat_sum === undefined) {
+  // NB: for completion games, the level author always has a 0 second solve since they published the level having completed it
+  // uniqueUsersCount always includes the author, so we need to subtract 1 to account for this
+  if (!level || (uniqueUsersCount - 1) < 10 || level.calc_playattempts_duration_before_stat_sum === undefined) {
     return -1;
   }
 
-  // when we have 10 unique users, we want to return a non-zero value
-  const completedCount = !level.calc_stats_completed_count ? 1 : level.calc_stats_completed_count;
+  function getCompletedCount() {
+    // need to make sure we return a non-zero value
+    if (!level.calc_stats_completed_count) {
+      return 1;
+    }
+
+    // similarly to above, we need to subtract 1 from calc_stats_completed_count to account for the author which has 0 playtime
+    const completedCount = level.calc_stats_completed_count - 1;
+
+    if (completedCount < 1) {
+      return 1;
+    }
+
+    return completedCount;
+  }
+
+  const completedCount = getCompletedCount();
 
   return level.calc_playattempts_duration_before_stat_sum / completedCount * getSolveCountFactor(completedCount);
 }
