617b4533fd857549065571a2e13032f873a54899
Spencer Spenst <spencerspenst@gmail.com>
2024-01-07 14:16:19 -0800

    fix font error

--- helpers/getFont.ts
+++ helpers/getFont.ts
@@ -4,7 +4,7 @@ import { Nunito_Sans, Onest, Rubik, Teko } from 'next/font/google';
 export const rubik = Rubik({ display: 'swap', subsets: ['latin'] });
 export const teko = Teko({ display: 'swap', subsets: ['latin'], weight: '500' });
 export const nunitoSans = Nunito_Sans({ display: 'swap', subsets: ['latin'] });
-export const onest = Onest({ display: 'swap', subsets: ['latin'] });
+export const onest = Onest({ adjustFontFallback: false, display: 'swap', subsets: ['latin'] });
 
 export default function getFontFromGameId(gameId: GameId) {
   switch (gameId) {
