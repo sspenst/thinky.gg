0cc57e84acdfb05edc9ea14c6258fd3243cf5134
Danny Miller <k2xl.com@gmail.com>
2024-01-07 13:48:22 -0500

    use a pool of mongomemoryservers to make tests less flaky

--- tests/globalSetup.ts
+++ tests/globalSetup.ts
@@ -3,15 +3,21 @@
 import { MongoMemoryReplSet } from 'mongodb-memory-server';
 
 module.exports = async () => {
-  const mongoServer = await MongoMemoryReplSet.create({
-    replSet: { count: 1 },
-  });
-  const mongoUri = mongoServer.getUri();
+  const mongoServerPool = [];
+  const mongoServerUris = [];
+  const num = 4;
 
-  // Set the Mongo URI for the tests to use
-  process.env.MONGODB_TEST_URI = mongoUri;
-  console.log('\nMONGODB_TEST_URI', process.env.MONGODB_TEST_URI);
+  process.env.MONGODB_TEST_URI_COUNT = '3';
+
+  for (let i = 0; i < num; i++) {
+    mongoServerPool.push(await MongoMemoryReplSet.create({
+      replSet: { count: 1 },
+    }));
+    mongoServerUris.push(mongoServerPool[i].getUri());
+    process.env[`MONGODB_TEST_URI_${i}`] = mongoServerUris[i];
+    console.log(`MONGODB_TEST_URI_${i}: ${mongoServerUris[i]}`);
+  }
 
   // Add mongoServer to the global config so we can tear it down later
-  global.__MONGOSERVER__ = mongoServer;
+  global.__MONGOSERVER__ = mongoServerPool;
 };
