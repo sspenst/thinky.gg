b71824705a33b02db7182008d7e5b0605647e6ea
Spencer Spenst <spencerspenst@gmail.com>
2024-01-07 22:50:56 -0800

    consistent font for now - can update later if we want

--- helpers/getFont.ts
+++ helpers/getFont.ts
@@ -1,18 +1,19 @@
 import { GameId } from '@root/constants/GameId';
-import { Nunito_Sans, Onest, Rubik, Teko } from 'next/font/google';
+import { Rubik, Teko } from 'next/font/google';
 
 export const rubik = Rubik({ display: 'swap', subsets: ['latin'] });
 export const teko = Teko({ display: 'swap', subsets: ['latin'], weight: '500' });
-export const nunitoSans = Nunito_Sans({ display: 'swap', subsets: ['latin'] });
-export const onest = Onest({ adjustFontFallback: false, display: 'swap', subsets: ['latin'] });
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function getFontFromGameId(gameId: GameId) {
-  switch (gameId) {
-  case GameId.PATHOLOGY:
-    return rubik.className;
-  case GameId.SOKOBAN:
-    return nunitoSans.className;
-  default:
-    return onest.className;
-  }
+  return rubik.className;
+
+  // switch (gameId) {
+  // case GameId.PATHOLOGY:
+  //   return rubik.className;
+  // case GameId.SOKOBAN:
+  //   return nunitoSans.className;
+  // default:
+  //   return onest.className;
+  // }
 }
