1a4684eee234ef7a57de9290180c558b97f65265
Danny Miller <k2xl.com@gmail.com>
2024-01-08 23:01:19 -0500

    maybe get more error info

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -4,7 +4,7 @@ import { AppContext } from '@root/contexts/appContext';
 import { ErrorProps } from 'next/error';
 import React, { useContext } from 'react';
 
-export default function CustomError({ statusCode }: ErrorProps) {
+export default function CustomError({ statusCode, title }: ErrorProps) {
   const { game } = useContext(AppContext);
 
   return (
@@ -16,6 +16,9 @@ export default function CustomError({ statusCode }: ErrorProps) {
         <span>
           {`A ${statusCode ? 'server' : 'client'}-side error occurred.`}
         </span>
+        {title && <span>
+          {title}
+        </span>}
         <span>
           Please report the issue on <a className='hover:underline text-blue-500' href='https://discord.com/channels/971585343956590623/975382809642430504' rel='noreferrer' target='_blank'>Discord</a>, create an issue on <a className='hover:underline text-blue-500' href='https://github.com/sspenst/pathology/issues' rel='noreferrer' target='_blank'>GitHub</a>, or contact <a className='hover:underline text-blue-500' href='mailto:help@thinky.gg' rel='noreferrer' target='_blank'>help@thinky.gg</a>.
         </span>
