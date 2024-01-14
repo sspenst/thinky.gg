8349dfed94a02408f71d5906433c5b6bff40388b
Danny Miller <k2xl.com@gmail.com>
2024-01-13 21:48:11 -0500

    level-of-the-day page

---
+++ pages/[subdomain]/level-of-the-day/index.tsx
@@ -0,0 +1,19 @@
+import { getGameIdFromReq } from '@root/helpers/getGameIdFromReq';
+import { getLevelOfDay } from '@root/pages/api/level-of-day';
+import { GetServerSidePropsContext } from 'next';
+import { getServerSideProps as getServerSidePropsFromLevel } from '../level/[username]/[slugName]';
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  // query the current level of the day
+  const gameId = getGameIdFromReq(context.req);
+  const levelOfDay = await getLevelOfDay(gameId);
+
+  context.params = {
+    username: levelOfDay?.userId.name,
+    slugName: levelOfDay?.slug.split('/').pop(),
+  };
+
+  return getServerSidePropsFromLevel(context);
+}
+
+export { default } from '../level/[username]/[slugName]';
