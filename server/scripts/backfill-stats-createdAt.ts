bd248702229673d37d37f3ee45b60ba517a90528
Danny Miller <k2xl.com@gmail.com>
2024-01-14 14:54:29 -0500

    backfill stats created ts script

---
+++ server/scripts/backfill-stats-createdAt.ts
@@ -0,0 +1,39 @@
+// ts-node -r tsconfig-paths/register --files server/scripts/backfill-stats-createdAt.ts
+import dbConnect from '@root/lib/dbConnect';
+import { StatModel } from '@root/models/mongoose';
+import cliProgress from 'cli-progress';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+
+async function Go() {
+  await dbConnect();
+  // grab all stats where createdAt is undefined
+  const statsWithUndefinedCreatedAt = await StatModel.countDocuments({ createdAt: { $exists: false } }, { _id: 1 });
+
+  const start = Date.now();
+
+  console.log('Number to migrate', statsWithUndefinedCreatedAt);
+  progressBar.start(statsWithUndefinedCreatedAt, 0);
+  const perBatch = Math.ceil(statsWithUndefinedCreatedAt / 50);
+
+  for (let i = 0; i < 50; i++) {
+    await StatModel.aggregate([
+      { $match: { createdAt: { $exists: false } } },
+      { $limit: perBatch },
+      { $addFields: { createdAt: { $toDate: '$_id' } } },
+      { $merge: { into: StatModel.collection.name, whenMatched: 'merge' } }
+    ]);
+    progressBar.increment(perBatch);
+  }
+
+  progressBar.update(30);
+  progressBar.stop();
+  const durationM = (Date.now() - start) / 1000 / 60;
+
+  console.log(`\nDone! Duration: ${durationM.toFixed(2)} minutes`);
+}
+
+console.log('Starting backfill stats createdAt script');
+Go();
