cc253a7a386961d62cc5744522f8015e59d79915
Danny Miller <k2xl.com@gmail.com>
2024-01-17 22:24:15 -0500

    gating

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -79,17 +79,17 @@ export default function Home({
 
   const buttonsSection = <div className='flex flex-col items-center gap-2 max-w-full'>
     <div className='flex flex-row gap-2 w-fit'>
-      <Link passHref href='/ranked' className={buttonClassNames}>
+      { !game.disableRanked && <Link passHref href='/ranked' className={buttonClassNames}>
         <span className='w-5 h-5 flex justify-center items-center text-xl'>🏅</span>
         <span className='text-lg font-bold'>Ranked</span>
-      </Link>
+      </Link>}
       <Link passHref href='/drafts' className={buttonClassNames}>
         <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='2 2 20 20' strokeWidth={1.5} stroke='currentColor' className='w-5 h-5'>
           <path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
         </svg>
         <span className='text-lg font-bold'>Create</span>
       </Link>
-      <Link passHref href='/multiplayer' className={buttonClassNames}>
+      { !game.disableMultiplayer && <Link passHref href='/multiplayer' className={buttonClassNames}>
         <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-5 h-5'>
           <path strokeLinecap='round' strokeLinejoin='round' d='M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z' />
         </svg>
@@ -109,16 +109,18 @@ export default function Home({
           }
         </div>
       </Link>
+      }
     </div>
   </div>;
 
   const buttonsSections2 = <div className='flex justify-center m-6'>
     <div className='gap-x-4 gap-y-2 flex flex-wrap flex-col md:flex-row justify-center'>
-      <Link id='communityCampaignsBtn' passHref href='/campaigns' className={buttonClassNames}>
+      {!game.disableCommunityCampaigns && <Link id='communityCampaignsBtn' passHref href='/campaigns' className={buttonClassNames}>
         <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='bi bi-book' viewBox='0 0 16 16'>
           <path d='M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z' />
         </svg>Community Campaigns
       </Link>
+      }
       <Link passHref href='/leaderboards' className={buttonClassNames}>
         <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='bi bi-trophy' viewBox='0 0 16 16'>
           <path d='M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935zM3.504 1c.007.517.026 1.006.056 1.469.13 2.028.457 3.546.87 4.667C5.294 9.48 6.484 10 7 10a.5.5 0 0 1 .5.5v2.61a1 1 0 0 1-.757.97l-1.426.356a.5.5 0 0 0-.179.085L4.5 15h7l-.638-.479a.501.501 0 0 0-.18-.085l-1.425-.356a1 1 0 0 1-.757-.97V10.5A.5.5 0 0 1 9 10c.516 0 1.706-.52 2.57-2.864.413-1.12.74-2.64.87-4.667.03-.463.049-.952.056-1.469H3.504z' />
@@ -153,11 +155,9 @@ export default function Home({
     }
     <div className='flex justify-center m-6'>
       <div className='flex flex-col items-center gap-3 w-full max-w-screen-2xl'>
-        { user &&
-        <><FormattedUser className='text-2xl' id='home' size={40} user={user} />
-          {buttonsSection}</>
-        }
+        { user && <FormattedUser className='text-2xl' id='home' size={40} user={user} />}
         {getSuggestedAction()}
+        { user && buttonsSection }
         <div className='flex flex-wrap justify-center gap-6 max-w-full'>
           <LevelCardWithTitle
             id='level-of-day'
