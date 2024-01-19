8af6c8a635727ad0334859964d133313ab506087
Spencer Spenst <spencerspenst@gmail.com>
2024-01-19 14:54:12 -0800

    revert gameId to 'sokoban' so we don't have to datafix

--- constants/GameId.ts
+++ constants/GameId.ts
@@ -1,7 +1,7 @@
 export enum GameId {
   THINKY = 'thinky',
   PATHOLOGY = 'pathology',
-  SOKOPATH = 'sokopath',
+  SOKOPATH = 'sokoban',
 }
 
 export const DEFAULT_GAME_ID = process.env.NODE_ENV !== 'test' ? GameId.THINKY : GameId.PATHOLOGY;
