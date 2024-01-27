f2b702d1623f23c5689d27f01d4b7bf3e809897a
Danny Miller <k2xl.com@gmail.com>
2024-01-27 14:44:43 -0500

    fix bug

--- helpers/validLevelBackgroundImageUrl.ts
+++ helpers/validLevelBackgroundImageUrl.ts
@@ -1,5 +1,5 @@
 export function validBackgroundImageUrl(url: string): boolean {
-  const match = url.match(/^(https?:\/\/)?(www\.)?(i\.)?imgur\.com\/[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?$/);
+  const match = !url || url.match(/^(https?:\/\/)?(www\.)?(i\.)?imgur\.com\/[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?$/);
 
   return !!match;
 }
