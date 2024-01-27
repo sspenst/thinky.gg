349a9e28bf5682a9a3c2d566b9461695aa921437
Danny Miller <k2xl.com@gmail.com>
2024-01-27 11:12:11 -0500

    Refresh button on error page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -19,9 +19,15 @@ export default function CustomError({ statusCode, title }: ErrorProps) {
         {title && <span>
           {title}
         </span>}
-        <span>
-          Please report the issue on <a className='hover:underline text-blue-500' href='https://discord.com/channels/971585343956590623/975382809642430504' rel='noreferrer' target='_blank'>Discord</a>, create an issue on <a className='hover:underline text-blue-500' href='https://github.com/sspenst/pathology/issues' rel='noreferrer' target='_blank'>GitHub</a>, or contact <a className='hover:underline text-blue-500' href='mailto:help@thinky.gg' rel='noreferrer' target='_blank'>help@thinky.gg</a>.
-        </span>
+        <div className='flex flex-col gap-2 justify-center items-center'>
+          <span>Please report the issue on <a className='hover:underline text-blue-500' href='https://discord.com/channels/971585343956590623/975382809642430504' rel='noreferrer' target='_blank'>Discord</a>, create an issue on <a className='hover:underline text-blue-500' href='https://github.com/sspenst/pathology/issues' rel='noreferrer' target='_blank'>GitHub</a>, or contact <a className='hover:underline text-blue-500' href='mailto:help@thinky.gg' rel='noreferrer' target='_blank'>help@thinky.gg</a>.</span>
+          <button
+            className='bg-blue-500 hover:bg-blue-400 transition px-3 py-1 rounded-lg w-fit'
+            onClick={() => window.location.reload()}
+          >
+            Refresh page
+          </button>
+        </div>
       </div>
     </Page>
   );
