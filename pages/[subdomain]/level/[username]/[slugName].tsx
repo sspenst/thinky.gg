137f37331f991a5e93ebd26e6d0f4a0addb0eae0
Spencer Spenst <spencerspenst@gmail.com>
2024-03-13 00:31:22 -0700

    fix cid BSONError

--- pages/[subdomain]/level/[username]/[slugName].tsx
+++ pages/[subdomain]/level/[username]/[slugName].tsx
@@ -38,13 +38,14 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   const reqUser = token ? await getUserFromToken(token, context.req as NextApiRequest) : null;
   const gameId = getGameIdFromReq(context.req);
   const cid = context.query?.cid as string | undefined;
+  const cObjectId = cid && Types.ObjectId.isValid(cid) ? new Types.ObjectId(cid) : null;
 
   // TODO: technically we could get the level from the collection instead of separately querying for it
   // If cid doesn't exist then we can just call getLevelsByUrlPath...
   const [level, collection] = await Promise.all([
     getLevelByUrlPath(gameId, username, slugName, reqUser),
-    (cid) ? getCollection({
-      matchQuery: { _id: new Types.ObjectId(cid), gameId: gameId },
+    cObjectId ? getCollection({
+      matchQuery: { _id: cObjectId, gameId: gameId },
       populateAroundSlug: username + '/' + slugName,
       populateLevelDirection: 'around',
       reqUser,
