3ad0a50a8045e6393988297abf68fbf386ec14e0
Spencer Spenst <spencerspenst@gmail.com>
2024-03-20 19:47:59 -0700

    fix long level title overflow in modal

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -74,7 +74,7 @@ export default function Modal({
             leaveTo='opacity-0 scale-95'
           >
             <form
-              className='flex justify-center'
+              className='flex justify-center overflow-hidden'
               onSubmit={(e) => {
                 e.preventDefault();
 
@@ -87,12 +87,7 @@ export default function Modal({
                 }
               }}
             >
-              <Dialog.Panel
-                className={classNames('py-3 px-4 my-8 text-left align-middle transition-all transform shadow-xl rounded-xl flex flex-col gap-4 border bg-1 border-color-3', getFontFromGameId(game.id))}
-                style={{
-                  maxWidth: '100vw',
-                }}
-              >
+              <Dialog.Panel className={classNames('py-3 px-4 my-8 text-left align-middle transition-all transform shadow-xl rounded-xl flex flex-col gap-4 border bg-1 border-color-3 overflow-hidden', getFontFromGameId(game.id))}>
                 <Dialog.Title as='div' className='flex gap-4 text-center'>
                   <span className='w-6' />
                   <span className='grow text-xl font-bold truncate'>{title}</span>
