8cbb9d7104caf31fbe544e16687d6530c846f277
Spencer Spenst <spencerspenst@gmail.com>
2024-04-11 21:36:22 -0700

    update github repo name

--- components/page/footer.tsx
+++ components/page/footer.tsx
@@ -60,10 +60,10 @@ export default function Footer() {
           <ExternalLink href='mailto:help@thinky.gg'>
             Contact
           </ExternalLink>
-          <ExternalLink href='https://github.com/sspenst/pathology'>
+          <ExternalLink href='https://github.com/sspenst/thinky.gg'>
             GitHub
           </ExternalLink>
-          <ExternalLink href='https://github.com/sspenst/pathology/wiki'>
+          <ExternalLink href='https://github.com/sspenst/thinky.gg/wiki'>
             Wiki
           </ExternalLink>
           <ExternalLink href='https://docs.google.com/document/d/e/2PACX-1vSNgV3NVKlsgSOEsnUltswQgE8atWe1WCLUY5fQUVjEdu_JZcVlRkZcpbTOewwe3oBNa4l7IJlOnUIB/pub'>
