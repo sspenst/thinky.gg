d9c40373ef372ae494f54669c5bc4e8cf730d8a8
Spencer Spenst <spencerspenst@gmail.com>
2024-04-16 11:16:00 -0700

    remove react-data-table-component-sspenst (#1090)
    * update /users data table
    
    * ProfileInsightsLevelPlayLog wip
    
    * remove react-data-table-component-sspenst

--- components/profile/profileInsightsRecords.tsx
+++ components/profile/profileInsightsRecords.tsx
@@ -1,12 +1,11 @@
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 import React from 'react';
-import DataTable from 'react-data-table-component-sspenst';
-import { DATA_TABLE_CUSTOM_STYLES } from '../../helpers/dataTableCustomStyles';
 import useProStatsUser, { ProStatsUserType } from '../../hooks/useProStatsUser';
 import User from '../../models/db/user';
 import FormattedDate from '../formatted/formattedDate';
 import FormattedLevelLink from '../formatted/formattedLevelLink';
+import { DataTableOffline } from '../tables/dataTable';
 
 dayjs.extend(duration);
 
@@ -17,59 +16,38 @@ export default function ProfileInsightsRecords({ user }: {user: User}) {
     return <span>Loading...</span>;
   }
 
+  const data = proStatsUser[ProStatsUserType.Records].sort((a, b) => {
+    return b.records[0].ts - a.records[0].ts;
+  });
+
   return (<>
     <h2 className='text-xl font-bold break-words max-w-full'>Level Records by {user.name}</h2>
     <div className='w-full max-w-lg'>
-      <DataTable
-        defaultSortFieldId={'date'}
-        defaultSortAsc={false}
+      <DataTableOffline
         columns={[
           {
+            id: 'level',
             name: 'Level',
-            sortable: true,
-            selector: (row) => row.name,
-            cell: (row, index) => <FormattedLevelLink id={`play-log-${index}`} level={row} />,
-            grow: 2,
+            selector: (row) => <FormattedLevelLink id={`play-log-${row._id}-${row.ts}`} level={row} />,
           },
-
           {
             id: 'date',
-            grow: 2,
             name: 'Achieved',
-            sortable: true,
-            cell: (row) => <FormattedDate ts={row.records[0].ts} />,
-            selector: (row) => (row.records[0].ts),
-
+            selector: (row) => <FormattedDate ts={row.records[0].ts} />,
           },
           {
-            grow: 3,
+            id: 'timeSinceCreation',
             name: 'Time Since Creation',
-            sortable: true,
-            cell: (row) => row.records[0].ts > row.ts ? dayjs.duration(((row.records[0].ts - row.ts) * 1000)).humanize() : 'N/A',
-
-            sortFunction: (a, b) => {
-              const aDelta = a.records[0].ts - a.ts;
-              const bDelta = b.records[0].ts - b.ts;
-
-              return aDelta - bDelta;
-            }
-
+            selector: (row) => row.records[0].ts > row.ts ? dayjs.duration(((row.records[0].ts - row.ts) * 1000)).humanize() : 'N/A',
           },
         ]}
-
-        customStyles={DATA_TABLE_CUSTOM_STYLES}
-        data={proStatsUser[ProStatsUserType.Records]}
-        dense
+        data={data}
+        itemsPerPage={10}
         noDataComponent={
           <div className='p-3'>
             Nothing to display...
           </div>
         }
-        pagination={true}
-        paginationComponentOptions={{
-          noRowsPerPage: true,
-        }}
-        striped
       />
     </div>
   </>);
