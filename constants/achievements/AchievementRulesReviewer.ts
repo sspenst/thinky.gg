22301f2a7619c7480e72b7adc74f00f23c9fc824
Spencer Spenst <spencerspenst@gmail.com>
2024-05-14 17:12:44 -0700

    optimize AchievementCategory.REVIEWER

--- constants/achievements/AchievementRulesReviewer.ts
+++ constants/achievements/AchievementRulesReviewer.ts
@@ -1,9 +1,8 @@
-import Review from '@root/models/db/review';
 import { IAchievementInfo } from './achievementInfo';
 import AchievementType from './achievementType';
 
-interface IAchievementInfoReviewer extends IAchievementInfo{
-  unlocked: ({ reviewsCreated }: {reviewsCreated: Review[]}) => boolean;
+interface IAchievementInfoReviewer extends IAchievementInfo {
+  unlocked: ({ reviewsCreatedCount }: {reviewsCreatedCount: number}) => boolean;
 }
 
 const AchievementRulesReviewer: { [achievementType: string]: IAchievementInfoReviewer; } = {
@@ -12,40 +11,40 @@ const AchievementRulesReviewer: { [achievementType: string]: IAchievementInfoRev
     emoji: '👑',
     getDescription: () => 'Reviewed 3000 levels',
     discordNotification: true,
-    unlocked: ({ reviewsCreated }) => reviewsCreated.length >= 3000,
+    unlocked: ({ reviewsCreatedCount }) => reviewsCreatedCount >= 3000,
   },
   [AchievementType.REVIEWED_1000_LEVELS]: {
     name: 'Master Critic',
     emoji: '🎭',
     getDescription: () => 'Reviewed 1000 levels',
     discordNotification: true,
-    unlocked: ({ reviewsCreated }) => reviewsCreated.length >= 1000,
+    unlocked: ({ reviewsCreatedCount }) => reviewsCreatedCount >= 1000,
   },
   [AchievementType.REVIEWED_500_LEVELS]: {
     name: 'Critic',
     emoji: '🍿',
     getDescription: () => 'Reviewed 500 levels',
     discordNotification: true,
-    unlocked: ({ reviewsCreated }) => reviewsCreated.length >= 500,
+    unlocked: ({ reviewsCreatedCount }) => reviewsCreatedCount >= 500,
   },
   [AchievementType.REVIEWED_100_LEVELS]: {
     name: 'Reviewer',
     emoji: '📝',
     getDescription: () => 'Reviewed 100 levels',
     discordNotification: true,
-    unlocked: ({ reviewsCreated }) => reviewsCreated.length >= 100,
+    unlocked: ({ reviewsCreatedCount }) => reviewsCreatedCount >= 100,
   },
   [AchievementType.REVIEWED_25_LEVELS]: {
     name: 'Commentator',
     emoji: '💬',
     getDescription: () => 'Reviewed 25 levels',
-    unlocked: ({ reviewsCreated }) => reviewsCreated.length >= 25,
+    unlocked: ({ reviewsCreatedCount }) => reviewsCreatedCount >= 25,
   },
   [AchievementType.REVIEWED_1_LEVEL]: {
     name: 'Scribbler',
     emoji: '🖍️',
     getDescription: () => 'Reviewed a level',
-    unlocked: ({ reviewsCreated }) => reviewsCreated.length >= 1,
+    unlocked: ({ reviewsCreatedCount }) => reviewsCreatedCount >= 1,
   },
 };
 
