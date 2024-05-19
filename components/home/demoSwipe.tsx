8ca73f81402b97b32937ce3c4136f99c0d0a8d22
Danny Miller <k2xl.com@gmail.com>
2024-05-19 14:25:10 -0400

    check file in

---
+++ components/home/demoSwipe.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function DemoSwipe() {
+  return <div className='flex flex-col gap-4'>
+    <div className=' '>
+      <video autoPlay loop muted playsInline className='w-80 h-40' src='https://i.imgur.com/7sYpgCt.mp4' style={{
+      }} />
+    </div>
+  </div>;
+}
