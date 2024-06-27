81e78e4f8985eb29fcad1b4f268359882d4c37c8
Danny Miller <k2xl.com@gmail.com>
2024-06-26 22:37:54 -0400

    enter button for search

--- helpers/className.ts
+++ helpers/className.ts
@@ -1,1 +1,1 @@
-export const blueButton = 'bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-3 rounded';
+export const blueButton = 'bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-3 rounded disabled:bg-gray-400 disabled:cursor-not-allowed';
