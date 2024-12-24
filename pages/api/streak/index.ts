0da2813553fe86b8ffff062e77d899fdb49a9e0d
Danny Miller <k2xl.com@gmail.com>
2024-12-24 12:25:55 -0500

    streak fixes

--- pages/api/streak/index.ts
+++ pages/api/streak/index.ts
@@ -12,71 +12,90 @@ export async function getStreaks(userId: Types.ObjectId) {
     },
     {
       $group: {
-        // $startTime is a timestamp (not a date)
         _id: {
           $dateToString: {
             format: '%Y-%m-%d',
             date: {
-              $toDate: {
-                $multiply: [
-                  '$startTime',
-                  1000
-                ]
+              $convert: {
+                input: { $multiply: ['$startTime', 1000] },
+                to: 'date',
+                onError: null
               }
-            }
+            },
+            timezone: 'UTC'
           }
         },
         value: {
-          // sum endTime - startTime
           $sum: {
-            $subtract: [
-              '$endTime',
-              '$startTime'
-            ]
+            $subtract: ['$endTime', '$startTime']
           }
         }
       }
     },
-    // filter out zero values
     {
       $match: {
         value: {
           $gt: 0
+        },
+        _id: {
+          $ne: null
         }
       }
     },
     {
-      // sort by date
       $sort: {
         _id: -1
       }
     },
     {
-      // change _id to date
       $project: {
         date: '$_id',
         value: 1,
         _id: 0,
       }
-
     }
   ]);
+
   let streak = 0;
-  const currentDate = new Date();
+  const today = new Date(Date.now());
+  today.setHours(0, 0, 0, 0);
 
-  for (let i = 0; i < playAttemptAgg.length; i++) {
-    const playAttempt = playAttemptAgg[i];
-    const date = new Date(playAttempt.date);
-    const dateDiff = Math.floor((currentDate.getTime() - date.getTime()) / (1000 * 3600 * 24));
+  if (playAttemptAgg.length === 0) {
+    return {
+      currentStreak: 0,
+      calendar: []
+    };
+  }
 
-    // if the date diff is more than 24h, then the streak is broken
-    if (dateDiff > 1) {
-      break;
-    } else {
+  const mostRecentPlay = new Date(playAttemptAgg[0].date);
+  mostRecentPlay.setHours(0, 0, 0, 0);
+  
+  const daysSinceLastPlay = Math.floor((today.getTime() - mostRecentPlay.getTime()) / (1000 * 3600 * 24));
+  
+  if (daysSinceLastPlay > 1) {
+    return {
+      currentStreak: 0, 
+      calendar: playAttemptAgg
+    };
+  }
+
+  let currentDate = new Date(playAttemptAgg[0].date);
+  currentDate.setHours(0, 0, 0, 0);
+  
+  streak = 1;
+
+  for (let i = 1; i < playAttemptAgg.length; i++) {
+    const prevDate = new Date(playAttemptAgg[i].date);
+    prevDate.setHours(0, 0, 0, 0);
+
+    const dayDiff = Math.floor((currentDate.getTime() - prevDate.getTime()) / (1000 * 3600 * 24));
+    
+    if (dayDiff === 1) {
       streak++;
+      currentDate = prevDate;
+    } else {
+      break;
     }
-
-    currentDate.setDate(date.getDate());
   }
 
   return {
