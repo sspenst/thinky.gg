19a3be2f5f647787700f21287339939e56ef8c9e
Danny Miller <k2xl.com@gmail.com>
2025-04-12 13:05:12 -0400

    rename search to Browse Levels

--- components/nav.tsx
+++ components/nav.tsx
@@ -306,7 +306,7 @@ export default function Nav({ isDropdown }: NavProps) {
           d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z' />
       </svg>
     }
-    label='Search'
+    label='Browse Levels'
   />;
 
   const homeNavLink = <NavLink
