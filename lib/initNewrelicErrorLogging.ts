9a5369f7aaf818d796b6412b642dda5ef9f04eaa
Danny Miller <k2xl.com@gmail.com>
2025-06-12 23:01:30 -0400

    newrelic logging improvements

--- lib/initNewrelicErrorLogging.ts
+++ lib/initNewrelicErrorLogging.ts
@@ -1,7 +1,7 @@
-import { NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 import isLocal from './isLocal';
 
-export default async function initNewrelicErrorLogging(res: NextApiResponse) {
+export default async function initNewrelicErrorLogging(req: NextApiRequest, res: NextApiResponse) {
   // dynamically import newrelic
   const newrelic = process.env.NODE_ENV === 'test' ? undefined : await import('newrelic');
   // overwrite the res.json function to log errors
@@ -12,8 +12,10 @@ export default async function initNewrelicErrorLogging(res: NextApiResponse) {
     if (data && data.error && process.env.NODE_ENV !== 'test') {
       if (!isLocal()) {
         newrelic?.addCustomAttribute && newrelic.addCustomAttribute('jsonError', data.error);
+        newrelic?.addCustomAttribute && newrelic.addCustomAttribute('url', req.url ?? '');
+        newrelic?.addCustomAttribute && newrelic.addCustomAttribute('method', req.method ?? '');
       } else {
-        console.error('Error response:', data.error);
+        console.error('Error response:', data.error, 'URL:', req.url, 'Method:', req.method);
       }
     }
 
