38020d1730b82260aea7eb2316d91525f649f392
Danny Miller <k2xl.com@gmail.com>
2025-07-24 13:01:07 -0400

    tests

--- tests/pages/api/user/[id]/prostats/prostats.user.test.ts
+++ tests/pages/api/user/[id]/prostats/prostats.user.test.ts
@@ -20,11 +20,12 @@ afterEach(() => {
 });
 enableFetchMocks();
 
-async function query({ userId, type, expectedError, expectedStatus, additionalAssertions }: {
+async function query({ userId, type, expectedError, expectedStatus, additionalAssertions, timeFilter }: {
     userId?: string,
     type: ProStatsUserType,
     expectedError?: string,
     expectedStatus: number,
+    timeFilter?: string,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     additionalAssertions?: (response: any) => Promise<void>,
     }) {
@@ -34,7 +35,8 @@ async function query({ userId, type, expectedError, expectedStatus, additionalAs
         method: 'GET',
         query: {
           id: userId || TestId.USER,
-          type: type
+          type: type,
+          ...(timeFilter && { timeFilter })
         },
         cookies: {
           token: getTokenCookieValue(TestId.USER)
@@ -185,6 +187,121 @@ describe('api/user/[id]/prostats/[type]', () => {
         expect(response[ProStatsUserType.PlayLogForUserCreatedLevels]).toBeUndefined();
         expect(response[ProStatsUserType.Records]).toBeDefined();
         expect(response[ProStatsUserType.Records].length).toBe(1);
+      }
+    });
+  });
+
+  test('should be able to get FollowerActivityPatterns with Pro', async () => {
+    await UserModel.findByIdAndUpdate(TestId.USER, {
+      $addToSet: {
+        roles: Role.PRO
+      }
+    });
+
+    await query({
+      userId: TestId.USER,
+      type: ProStatsUserType.FollowerActivityPatterns,
+      expectedStatus: 200,
+      additionalAssertions: async response => {
+        expect(response[ProStatsUserType.ScoreHistory]).toBeUndefined();
+        expect(response[ProStatsUserType.MostSolvesForUserLevels]).toBeUndefined();
+        expect(response[ProStatsUserType.DifficultyLevelsComparisons]).toBeUndefined();
+        expect(response[ProStatsUserType.PlayLogForUserCreatedLevels]).toBeUndefined();
+        expect(response[ProStatsUserType.Records]).toBeUndefined();
+        expect(response[ProStatsUserType.FollowerActivityPatterns]).toBeDefined();
+        expect(response[ProStatsUserType.FollowerActivityPatterns]).toHaveProperty('followerCount');
+        expect(response[ProStatsUserType.FollowerActivityPatterns]).toHaveProperty('activeFollowerCount');
+        expect(response[ProStatsUserType.FollowerActivityPatterns]).toHaveProperty('hasDiscordConnected');
+      }
+    });
+  });
+
+  test('should not be able to get FollowerActivityPatterns for another user', async () => {
+    await UserModel.findByIdAndUpdate(TestId.USER, {
+      $addToSet: {
+        roles: Role.PRO
+      }
+    });
+
+    await query({
+      userId: TestId.USER_B,
+      type: ProStatsUserType.FollowerActivityPatterns,
+      expectedStatus: 401,
+      expectedError: 'Not authorized',
+    });
+  });
+
+  test('should be able to get ScoreHistory for another user (public access)', async () => {
+    await UserModel.findByIdAndUpdate(TestId.USER, {
+      $addToSet: {
+        roles: Role.PRO
+      }
+    });
+
+    await query({
+      userId: TestId.USER_B,
+      type: ProStatsUserType.ScoreHistory,
+      expectedStatus: 200,
+      additionalAssertions: async response => {
+        expect(response[ProStatsUserType.ScoreHistory]).toBeDefined();
+        expect(response[ProStatsUserType.DifficultyLevelsComparisons]).toBeUndefined();
+        expect(response[ProStatsUserType.FollowerActivityPatterns]).toBeUndefined();
+      }
+    });
+  });
+
+  test('should be able to get Records for another user (public access)', async () => {
+    await UserModel.findByIdAndUpdate(TestId.USER, {
+      $addToSet: {
+        roles: Role.PRO
+      }
+    });
+
+    await query({
+      userId: TestId.USER_B,
+      type: ProStatsUserType.Records,
+      expectedStatus: 200,
+      additionalAssertions: async response => {
+        expect(response[ProStatsUserType.Records]).toBeDefined();
+        expect(response[ProStatsUserType.DifficultyLevelsComparisons]).toBeUndefined();
+        expect(response[ProStatsUserType.FollowerActivityPatterns]).toBeUndefined();
+      }
+    });
+  });
+
+  test('should support timeFilter parameter for ScoreHistory', async () => {
+    await UserModel.findByIdAndUpdate(TestId.USER, {
+      $addToSet: {
+        roles: Role.PRO
+      }
+    });
+
+    await query({
+      type: ProStatsUserType.ScoreHistory,
+      timeFilter: '30d',
+      expectedStatus: 200,
+      additionalAssertions: async response => {
+        expect(response[ProStatsUserType.ScoreHistory]).toBeDefined();
+        // The filter should still return data (the specific filtering logic is tested in the backend aggregation)
+        expect(Array.isArray(response[ProStatsUserType.ScoreHistory])).toBe(true);
+      }
+    });
+  });
+
+  test('should support timeFilter parameter for DifficultyLevelsComparisons', async () => {
+    await UserModel.findByIdAndUpdate(TestId.USER, {
+      $addToSet: {
+        roles: Role.PRO
+      }
+    });
+
+    await query({
+      type: ProStatsUserType.DifficultyLevelsComparisons,
+      timeFilter: '1y',
+      expectedStatus: 200,
+      additionalAssertions: async response => {
+        expect(response[ProStatsUserType.DifficultyLevelsComparisons]).toBeDefined();
+        expect(Array.isArray(response[ProStatsUserType.DifficultyLevelsComparisons])).toBe(true);
       }
     });
   });
