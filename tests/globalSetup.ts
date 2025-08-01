1dcb7efb20a0a32fa8e35fcfd4d9227302a09b3d
Danny Miller <k2xl.com@gmail.com>
2025-07-31 20:39:24 -0400

    promise all

--- tests/globalSetup.ts
+++ tests/globalSetup.ts
@@ -9,19 +9,27 @@ module.exports = async () => {
 
   process.env.MONGODB_TEST_URI_COUNT = num.toString();
 
-  for (let i = 0; i < num; i++) {
-    mongoServerPool.push(await MongoMemoryReplSet.create({
-      binary: {
-        version: '6.0.14',
-      },
-      replSet: {
-        count: 1,
-      },
-    }));
-    mongoServerUris.push(mongoServerPool[i].getUri());
-    process.env[`MONGODB_TEST_URI_${i}`] = mongoServerUris[i];
-    console.log(`MONGODB_TEST_URI_${i}: ${mongoServerUris[i]}`);
-  }
+  const mongoServers = await Promise.all(
+    Array.from({ length: num }).map(() =>
+      MongoMemoryReplSet.create({
+        binary: {
+          version: '6.0.14',
+        },
+        replSet: {
+          count: 1,
+        },
+      })
+    )
+  );
+
+  mongoServers.forEach((server, i) => {
+    mongoServerPool.push(server);
+    const uri = server.getUri();
+
+    mongoServerUris.push(uri);
+    process.env[`MONGODB_TEST_URI_${i}`] = uri;
+    console.log(`MONGODB_TEST_URI_${i}: ${uri}`);
+  });
 
   // Add mongoServer to the global config so we can tear it down later
   global.__MONGOSERVER__ = mongoServerPool;
